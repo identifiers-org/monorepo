ab0078e357f727b16a55c53cb8154a8bae7dedc0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-15 12:28:47 +0000

    project structure

---
+++ src/test/java/org/identifiers/org/cloud/ws/resolver/ResolverApplicationTests.java
@@ -0,0 +1,16 @@
+package org.identifiers.org.cloud.ws.resolver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResolverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
