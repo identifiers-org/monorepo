2f0eece1b49d9a3f4a084f3ab0ca1a450ddfb4b3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-15 12:35:20 +0000

    change in spacing

--- src/main/java/org/identifiers/org/cloud/ws/resolver/controllers/ResolverApi.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/controllers/ResolverApi.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ResolverApi {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Hey!, I'm your resolver!";
