6fa06b80c1125e74493b57664ecb6150a6ea8c11
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-15 12:34:21 +0000

    main resolver method

--- src/main/java/org/identifiers/org/cloud/ws/resolver/controllers/ResolverApi.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/controllers/ResolverApi.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.resolver.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resolver
@@ -7,5 +10,11 @@ package org.identifiers.org.cloud.ws.resolver.controllers;
  * Timestamp: 2018-01-15 12:31
  * ---
  */
+@RestController
 public class ResolverApi {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hey!, I'm your resolver!";
+    }
 }
