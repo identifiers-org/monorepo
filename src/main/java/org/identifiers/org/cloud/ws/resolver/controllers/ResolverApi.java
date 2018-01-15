1d414358042d19ff54b726404fa5ca6ae68cd6eb
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-15 12:33:15 +0000

    resolver API controller, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/controllers/ResolverApi.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.controllers;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.controllers
+ * Timestamp: 2018-01-15 12:31
+ * ---
+ */
+public class ResolverApi {
+}
