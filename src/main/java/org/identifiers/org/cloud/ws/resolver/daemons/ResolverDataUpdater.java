36f7da03e7a5f4388d65b8348a0bc31a2fffa2f3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-16 16:34:31 +0000

    ResolverDataUpdater daemon placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/daemons/ResolverDataUpdater.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.daemons;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.daemons
+ * Timestamp: 2018-01-16 16:34
+ * ---
+ */
+public class ResolverDataUpdater {
+}
