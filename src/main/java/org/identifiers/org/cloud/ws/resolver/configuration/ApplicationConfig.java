729388272805fe61c3f8f177c791db0497eaf31b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 16:25:48 +0000

    configuration and redis repos annotations

--- src/main/java/org/identifiers/org/cloud/ws/resolver/configuration/ApplicationConfig.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/configuration/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.resolver.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resolver
@@ -7,5 +10,7 @@ package org.identifiers.org.cloud.ws.resolver.configuration;
  * Timestamp: 2018-01-17 16:25
  * ---
  */
+@Configuration
+@EnableRedisRepositories
 public class ApplicationConfig {
 }
