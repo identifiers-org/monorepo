4f9df17eac7d50a9e33d0d64aa6413f7de2ffa5f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 20:57:29 +0000

    interface ResourceEntryRepository

--- src/main/java/org/identifiers/org/cloud/ws/resolver/data/repositories/ResourceEntryRepository.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/data/repositories/ResourceEntryRepository.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.resolver.data.repositories;
 
+import org.identifiers.org.cloud.ws.resolver.models.ResourceEntry;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resolver
@@ -7,5 +10,6 @@ package org.identifiers.org.cloud.ws.resolver.data.repositories;
  * Timestamp: 2018-01-17 20:56
  * ---
  */
-public interface ResourceEntryRepository {
+public interface ResourceEntryRepository extends CrudRepository<ResourceEntry, String> {
+    // TODO
 }
