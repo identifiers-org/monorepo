7efc56048dcd3e9988b514981ae1ca333049d209
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 15:19:02 +0000

    pid data repo placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/data/repositories/PidDataRepository.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.data.repositories;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.data.repositories
+ * Timestamp: 2018-01-17 15:18
+ * ---
+ */
+public interface PidDataRepository {
+}
