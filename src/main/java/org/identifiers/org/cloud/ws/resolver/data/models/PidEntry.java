4ff559bfe32345c8557edca21f132880a263f820
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 21:58:47 +0000

    bug fix on pid entry definition

--- src/main/java/org/identifiers/org/cloud/ws/resolver/data/models/PidEntry.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/data/models/PidEntry.java
@@ -20,14 +20,18 @@ public class PidEntry {
     private String definition;
     @Indexed
     private String prefix;
+    private String url;
+    private boolean prefixed;
     private ResourceEntry[] resourceEntries;
 
-    public PidEntry(String id, String name, String pattern, String definition, String prefix, ResourceEntry[] resourceEntries) {
+    public PidEntry(String id, String name, String pattern, String definition, String prefix, String url, boolean prefixed, ResourceEntry[] resourceEntries) {
         this.id = id;
         this.name = name;
         this.pattern = pattern;
         this.definition = definition;
         this.prefix = prefix;
+        this.url = url;
+        this.prefixed = prefixed;
         this.resourceEntries = resourceEntries;
     }
 
@@ -69,6 +73,22 @@ public class PidEntry {
 
     public void setPrefix(String prefix) {
         this.prefix = prefix;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isPrefixed() {
+        return prefixed;
+    }
+
+    public void setPrefixed(boolean prefixed) {
+        this.prefixed = prefixed;
     }
 
     public ResourceEntry[] getResourceEntries() {
