8f02212631daa643054cd74dd686a4e72a9be783
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 16:27:31 +0000

    redis template bean

--- src/main/java/org/identifiers/org/cloud/ws/resolver/configuration/ApplicationConfig.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/configuration/ApplicationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -20,5 +21,11 @@ public class ApplicationConfig {
     @Bean
     public RedisConnectionFactory connectionFactory() {
         return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        return template;
     }
 }
