ef920b189749f6554a51df9cc9ffb61c6c851ded
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 16:23:45 +0000

    new pid entry repo, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/data/repositories/PidEntryRepository.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.data.repositories;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.data.repositories
+ * Timestamp: 2018-01-17 16:23
+ * ---
+ */
+public interface PidEntryRepository {
+}
