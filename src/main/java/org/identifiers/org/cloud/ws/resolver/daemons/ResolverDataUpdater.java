758b1bf1bd260a9026309b73b80939715718d1b1
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 22:05:22 +0000

    autowire repo and save entry

--- src/main/java/org/identifiers/org/cloud/ws/resolver/daemons/ResolverDataUpdater.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/daemons/ResolverDataUpdater.java
@@ -1,8 +1,10 @@
 package org.identifiers.org.cloud.ws.resolver.daemons;
 
 import org.identifiers.org.cloud.ws.resolver.data.models.PidEntry;
+import org.identifiers.org.cloud.ws.resolver.data.repositories.PidEntryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -23,6 +25,9 @@ public class ResolverDataUpdater extends Thread {
 
     private boolean shutdown = false;
 
+    @Autowired
+    private PidEntryRepository pidEntryRepository;
+
     public boolean isShutdown() {
         return shutdown;
     }
@@ -39,6 +44,7 @@ public class ResolverDataUpdater extends Thread {
             // TODO - Do your stuff
             logger.info("---> Creating instance of PidEntry ---");
             PidEntry pidEntry = new PidEntry("MIR:00000002", "ChEBI", "^CHEBI:\\d+$", "Chemical Entities of Biological Interest (ChEBI) is a freely available dictionary of molecular entities focused on 'small' chemical compounds.", "chebi", "http://identifiers.org/chebi", true, null);
+            pidEntryRepository.save(pidEntry);
             // Wait for a predefined period of time before the next announcement
             try {
                 long waitTime = random.nextInt(WAIT_TIME_LIMIT_SECONDS) * 1000;
