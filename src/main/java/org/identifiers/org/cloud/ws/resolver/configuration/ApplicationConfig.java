05ac403a42f87926e999f36824c3a4f8ed75d425
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 16:25:21 +0000

    application configuration

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/configuration/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.configuration;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.configuration
+ * Timestamp: 2018-01-17 16:25
+ * ---
+ */
+public class ApplicationConfig {
+}
