2a543e6c041057c8581497e26d716c9f70ba5785
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 16:15:58 +0000

    resource entry placholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/models/ResourceEntry.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.models
+ * Timestamp: 2018-01-17 16:15
+ * ---
+ */
+public class ResourceEntry {
+}
