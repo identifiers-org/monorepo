5b61c754626dbc07efcf8346d56f435472be16a6
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 16:24:22 +0000

    repo interface inheritance

--- src/main/java/org/identifiers/org/cloud/ws/resolver/data/repositories/PidEntryRepository.java
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/data/repositories/PidEntryRepository.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.resolver.data.repositories;
 
+import org.identifiers.org.cloud.ws.resolver.models.PidEntry;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resolver
@@ -7,5 +10,6 @@ package org.identifiers.org.cloud.ws.resolver.data.repositories;
  * Timestamp: 2018-01-17 16:23
  * ---
  */
-public interface PidEntryRepository {
+public interface PidEntryRepository extends CrudRepository<PidEntry, String> {
+
 }
