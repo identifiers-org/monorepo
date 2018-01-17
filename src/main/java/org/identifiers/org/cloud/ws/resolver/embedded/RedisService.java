69bbff377be06faf71ad77d2b52884585e8ea1e8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-17 11:29:17 +0000

    embedded redis service component, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/resolver/embedded/RedisService.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.resolver.embedded;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.org.cloud.ws.resolver.embedded
+ * Timestamp: 2018-01-17 11:28
+ * ---
+ */
+public class RedisService {
+}
