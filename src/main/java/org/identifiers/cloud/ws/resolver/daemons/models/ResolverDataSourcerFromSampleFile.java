f34e2a6e7878818ea8f4a6f343a8eaf5d538d0bc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-18 10:17:18 +0000

    property value

--- src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcerFromSampleFile.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcerFromSampleFile.java
@@ -3,6 +3,7 @@ package org.identifiers.cloud.ws.resolver.daemons.models;
 import org.identifiers.cloud.ws.resolver.data.models.PidEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +18,14 @@ import java.util.List;
 public class ResolverDataSourcerFromSampleFile implements ResolverDataSourcer {
     private Logger logger = LoggerFactory.getLogger(ResolverDataSourcerFromSampleFile.class);
 
+    @Value("${org.identifiers.cloud.ws.resolver.data.source.local.sample.file.name}")
     private String sampleDataFileName;
 
     @Override
     public List<PidEntry> getResolverData() throws ResolverDataSourcerException {
         ArrayList<PidEntry> result = new ArrayList<>();
         // TODO
+        
         return result;
     }
 }
