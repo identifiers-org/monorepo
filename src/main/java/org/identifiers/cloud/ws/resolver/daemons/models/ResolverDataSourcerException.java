3d76ebf44ef66da7598555d9ed6a83a5b2367554
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-18 09:57:12 +0000

    Resolver data sourcer exception

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcerException.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2018-01-18 9:56
+ * ---
+ */
+public class ResolverDataSourcerException extends RuntimeException {
+    public ResolverDataSourcerException(String message) {
+        super(message);
+    }
+}
