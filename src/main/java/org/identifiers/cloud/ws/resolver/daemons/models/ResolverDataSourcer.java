aa0be3d86d69f9e800045fe78e3a7aaaddffe897
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-18 09:54:46 +0000

    resolver data source interface

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcer.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2018-01-18 9:46
+ * ---
+ */
+public interface ResolverDataSourcer {
+    
+}
