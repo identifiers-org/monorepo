0489d6d9341de6af0442043ec5d4d3e1f21a954f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-18 10:07:36 +0000

    resolver data sourcer from file, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcerFromSampleFile.java
@@ -0,0 +1,19 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+import org.identifiers.cloud.ws.resolver.data.models.PidEntry;
+
+import java.util.List;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2018-01-18 10:06
+ * ---
+ */
+public class ResolverDataSourcerFromSampleFile implements ResolverDataSourcer {
+    @Override
+    public List<PidEntry> getResolverData() throws ResolverDataSourcerException {
+        return null;
+    }
+}
