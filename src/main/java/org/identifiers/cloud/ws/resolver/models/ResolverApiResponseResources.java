cd89e5cc919e54e058e8c621db385d5049ce53e5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 11:38:18 +0000

    ResolverApiResponseResources, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverApiResponseResources.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-26 11:37
+ * ---
+ */
+public class ResolverApiResponseResources {
+}
