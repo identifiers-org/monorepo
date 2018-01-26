6d35cc521e1f4da4b01b838ccc79ebadcc13a697
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 10:56:38 +0000

    ResolverResourceDecider documentation

--- src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverResourceDecider.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverResourceDecider.java
@@ -6,6 +6,14 @@ package org.identifiers.cloud.ws.resolver.models;
  * Package: org.identifiers.cloud.ws.resolver.models
  * Timestamp: 2018-01-26 10:48
  * ---
+ *
+ * This class implements the resolution strategy when it comes to different situations, e.g. choose a resource among
+ * different options regarding a compact ID
+ *
+ * DEVNOTE - This should be an interface and different strategies be implementations of that interface, if things go
+ * south in terms of granularity, there could be specializations where we have splitted all the possible strategies for
+ * all the possible situations as individual pieces. But this is not required right now, just to let you know, future
+ * developer.
  */
 public class ResolverResourceDecider {
 }
