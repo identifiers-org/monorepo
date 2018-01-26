4da0b91da9282a92bc809b8e78fc10b61af2ff70
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 15:11:54 +0000

    unit tests for Compact ID parsing, placeholder

---
+++ src/test/java/org/identifiers/cloud/ws/resolver/models/CompactIdTest.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-26 15:11
+ * ---
+ *
+ * Unit tests for Compact ID parsing
+ */
+public class CompactIdTest {
+
+}
