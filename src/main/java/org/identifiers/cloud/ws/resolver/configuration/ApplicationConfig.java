fdd6a7d1c46a0c4a2f53b91b272e9200646a6134
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 12:19:31 +0000

    resolverDataFetcher bean

--- src/main/java/org/identifiers/cloud/ws/resolver/configuration/ApplicationConfig.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/configuration/ApplicationConfig.java
@@ -2,6 +2,7 @@ package org.identifiers.cloud.ws.resolver.configuration;
 
 import org.identifiers.cloud.ws.resolver.daemons.models.ResolverDataSourcer;
 import org.identifiers.cloud.ws.resolver.daemons.models.ResolverDataSourcerFromSampleFile;
+import org.identifiers.cloud.ws.resolver.models.ResolverDataFetcher;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -45,4 +46,9 @@ public class ApplicationConfig {
     public ResolverDataSourcer resolverDataSourcer() {
         return new ResolverDataSourcerFromSampleFile();
     }
+
+    // Resolver Data Fetcher
+    @Bean
+    public ResolverDataFetcher resolverDataFetcher() {
+        return new ResolverDataFetcher();}
 }
