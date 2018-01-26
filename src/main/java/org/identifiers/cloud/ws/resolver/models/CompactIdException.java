cbdfdfca76a01221e1d40aec9f20cd6d215c4c04
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 12:54:25 +0000

    CompactIdException, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/CompactIdException.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-26 12:54
+ * ---
+ */
+public class CompactIdException {
+}
