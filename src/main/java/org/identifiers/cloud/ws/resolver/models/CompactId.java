0ad20eee0b00c1a6014b769eb85db00e53389873
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 12:27:04 +0000

    CompactId model, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/CompactId.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-26 12:26
+ * ---
+ */
+public class CompactId {
+}
