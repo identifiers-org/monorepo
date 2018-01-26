fa06fd06c0b2e9f394fa1321148cb4ec925b88cf
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-26 10:32:04 +0000

    resolver API model, for resolver API controller

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverApi.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-26 10:31
+ * ---
+ */
+public class ResolverApi {
+}
