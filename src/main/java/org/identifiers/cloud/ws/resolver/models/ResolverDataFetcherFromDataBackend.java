886f5569352532a611ebf5d8c97335f0e1b262d2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-27 09:03:08 +0000

    refactoring the resolver data fetcher, new fetcher

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverDataFetcherFromDataBackend.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-27 9:02
+ * ---
+ */
+public class ResolverDataFetcherFromDataBackend {
+}
