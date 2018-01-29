86ef7380e7a997ba365ea96811b3f6a81d7877de
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-29 10:15:29 +0000

    resolver data fetcher from WS

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverDataFetcherFromProviderWs.java
@@ -0,0 +1,19 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+import org.identifiers.cloud.ws.resolver.data.models.ResourceEntry;
+
+import java.util.List;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-01-29 10:14
+ * ---
+ */
+public class ResolverDataFetcherFromProviderWs implements ResolverDataFetcher {
+    @Override
+    public List<ResourceEntry> findResourcesByPrefix(String prefix) {
+        return null;
+    }
+}
