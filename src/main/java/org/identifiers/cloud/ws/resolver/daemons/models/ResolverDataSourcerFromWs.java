b195ba64dd3fd3ccf8c669dbd635ba8e3266a872
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-29 10:16:39 +0000

    resolver data sourcer from identifiers.org web service, placeholder

--- src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcerFromWs.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDataSourcerFromWs.java
@@ -8,9 +8,12 @@ import java.util.List;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resolver
  * Package: org.identifiers.cloud.ws.resolver.daemons.models
- * Timestamp: 2018-01-18 9:46
+ * Timestamp: 2018-01-29 10:16
  * ---
  */
-public interface ResolverDataSourcer {
-    List<PidEntry> getResolverData() throws ResolverDataSourcerException;
+public class ResolverDataSourcerFromWs implements ResolverDataSourcer {
+    @Override
+    public List<PidEntry> getResolverData() throws ResolverDataSourcerException {
+        return null;
+    }
 }
