9b1eba68fdc16cbd5f50ba76e7561a1612e22ba0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-07 11:29:29 +0000

    metadata main API controller

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/controllers/MetadataApiController.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.metadata.controllers;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.controllers
+ * Timestamp: 2018-02-07 11:29
+ * ---
+ */
+public class MetadataApiController {
+}
