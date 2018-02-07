e77c78967884be6f998afae64996dfb7e0d4d7ad
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-07 11:46:15 +0000

    placeholder for application configuration

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/configuration/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.metadata.configuration;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.configuration
+ * Timestamp: 2018-02-07 11:46
+ * ---
+ */
+public class ApplicationConfig {
+}
