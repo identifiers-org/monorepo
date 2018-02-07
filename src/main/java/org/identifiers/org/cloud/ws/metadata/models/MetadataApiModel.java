2395b221280576ff9115d3e070086dfe5f51e22a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-07 11:31:39 +0000

    model singleton managed by spring, fine by now

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataApiModel.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataApiModel.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: metadata
@@ -7,5 +9,6 @@ package org.identifiers.org.cloud.ws.metadata.models;
  * Timestamp: 2018-02-07 11:29
  * ---
  */
+@Component
 public class MetadataApiModel {
 }
