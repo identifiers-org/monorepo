eb419cb5138b03f2888fe2b1e167a73082aef1a6
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-07 11:37:09 +0000

    placeholder for the metadata API response

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataApiResponse.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-07 11:36
+ * ---
+ */
+public class MetadataApiResponse {
+}
