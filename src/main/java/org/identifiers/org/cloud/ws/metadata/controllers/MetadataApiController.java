cb7cd1bcbd8b2f48256b8aabc595a2dd4891ccf6
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-07 11:32:52 +0000

    rest controller

--- src/main/java/org/identifiers/org/cloud/ws/metadata/controllers/MetadataApiController.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/controllers/MetadataApiController.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.metadata.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: metadata
@@ -7,6 +9,7 @@ package org.identifiers.org.cloud.ws.metadata.controllers;
  * Timestamp: 2018-02-07 11:29
  * ---
  */
+@RestController
 public class MetadataApiController {
 
     private MetadataApiController metadataApiController;
