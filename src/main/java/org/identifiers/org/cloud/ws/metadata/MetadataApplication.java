062cb35474fbf9bd59542fe153492edd7ac3b6aa
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-07 11:28:24 +0000

    project structure

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/MetadataApplication.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataApplication.class, args);
+	}
+}
