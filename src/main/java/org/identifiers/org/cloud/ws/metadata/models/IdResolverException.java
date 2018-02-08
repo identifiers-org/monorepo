10ee4b1f957ec915e35d4de37e5b8c8f01b9e2b3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-08 12:00:43 +0000

    IdResolverException

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverException.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-08 12:00
+ * ---
+ */
+public class IdResolverException extends RuntimeException {
+    public IdResolverException(String message) {
+        super(message);
+    }
+}
