52e23ec3f728872f656cec19c130919618d2f0b0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-08 11:54:07 +0000

    MetadataExtractor interface, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataExtractor.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-08 11:53
+ * ---
+ */
+public interface MetadataExtractor {
+}
