954f6274227227deb4fa8293d34cef644e27724d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 13:04:01 +0000

    re-try pattern, re-try policy definition

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverThroughResolverWebService.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverThroughResolverWebService.java
@@ -2,6 +2,8 @@ package org.identifiers.org.cloud.ws.metadata.models;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 import java.util.List;
@@ -21,6 +23,17 @@ public class IdResolverThroughResolverWebService implements IdResolver {
     public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500;    // 1.5 seconds
 
     private static final RetryTemplate retryTemplate;
+    static {
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(WS_REQUEST_RETRY_MAX_ATTEMPTS);
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(WS_REQUEST_RETRY_BACK_OFF_PERIOD);
+
+        retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+    }
 
     @Override
     public List<ResolverApiResponseResource> resolve(String compactIdParameter) throws IdResolverException {
