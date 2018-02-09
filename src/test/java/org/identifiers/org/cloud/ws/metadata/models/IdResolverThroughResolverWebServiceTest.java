5cf619d6b4e5c1f5d5eaf3165866115a365a7af9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 14:01:40 +0000

    setup test as spring boot test

--- src/test/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverThroughResolverWebServiceTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverThroughResolverWebServiceTest.java
@@ -1,5 +1,9 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: metadata
@@ -7,6 +11,8 @@ package org.identifiers.org.cloud.ws.metadata.models;
  * Timestamp: 2018-02-09 13:42
  * ---
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class IdResolverThroughResolverWebServiceTest {
 
 }
