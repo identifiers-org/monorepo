906fcb323d979b423644f0a8cb8b69a26ec280d7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 14:12:55 +0000

    IdResolver, unit test for valid Compact ID

--- src/test/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverTest.java
@@ -1,9 +1,13 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -19,5 +23,12 @@ public class IdResolverTest {
     @Autowired
     private IdResolver idResolver;
 
-    
+    @Test
+    public void testValidId() {
+        String compactId = "CHEBI:36927";
+        // Maybe I should not check that it comes back non-empty...
+        assertThat(String.format("Valid Compact ID '%s' is resolved", compactId),
+                idResolver.resolve(compactId).isEmpty(),
+                is(false));
+    }
 }
