d9305f229a9e93e69f069f17a08653967913ac06
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 14:05:58 +0000

    refactor, as this unit tests is valid for all implementations of ID resolution

--- src/test/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverTest.java
@@ -13,6 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IdResolverThroughResolverWebServiceTest {
+public class IdResolverTest {
 
 }
