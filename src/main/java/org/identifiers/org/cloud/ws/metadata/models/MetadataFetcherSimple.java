5e4e9a9fa4fa189bf40709fb0ca96862c4cdea2f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 16:32:46 +0000

    placeholder for simple metadata fetcher, taken care by spring

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: metadata
@@ -7,6 +10,8 @@ package org.identifiers.org.cloud.ws.metadata.models;
  * Timestamp: 2018-02-09 16:31
  * ---
  */
+@Component
+@Scope("prototype")
 public class MetadataFetcherSimple implements MetadataFetcher {
     @Override
     public String fetchMetadataFor(String url) throws MetadataFetcherException {
