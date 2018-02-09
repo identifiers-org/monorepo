b0cde11f23fea5270be00099f8e5a4da772039bc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 16:31:55 +0000

    placeholder for simple metadata fetcher

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-09 16:31
+ * ---
+ */
+public class MetadataFetcherSimple implements MetadataFetcher {
+    @Override
+    public String fetchMetadataFor(String url) throws MetadataFetcherException {
+        return null;
+    }
+}
