798f27263756ab186ae38a4f708210e2a40ad748
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-09 13:00:14 +0000

    re-try pattern, pom file dependency

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverThroughResolverWebService.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResolverThroughResolverWebService.java
@@ -14,11 +14,12 @@ import java.util.List;
  */
 public class IdResolverThroughResolverWebService implements IdResolver {
     private static Logger logger = LoggerFactory.getLogger(IdResolverThroughResolverWebService.class);
-    
+
     // Re-try pattern
     public static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500;    // 1.5 seconds
 
+    private static final RetryTemplate
 
     @Override
     public List<ResolverApiResponseResource> resolve(String compactIdParameter) throws IdResolverException {
