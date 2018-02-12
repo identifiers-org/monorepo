b07b01a0476d5bdc3305bc590fe2131a48aab680
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 14:31:19 +0000

    MetadataFetcherWithJavascript with javascript support

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherWithJavascript.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-12 14:31
+ * ---
+ */
+public class MetadataFetcherWithJavascript implements MetadataFetcher {
+    @Override
+    public String fetchMetadataFor(String url) throws MetadataFetcherException {
+        return null;
+    }
+}
