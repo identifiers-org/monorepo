3a028e0a573e6c176a98af34c966fb56f16091a8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 12:45:02 +0000

    grab the content of the given URL

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
@@ -1,7 +1,11 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -16,6 +20,12 @@ public class MetadataFetcherSimple implements MetadataFetcher {
     @Override
     public String fetchMetadataFor(String url) throws MetadataFetcherException {
         // TODO - Fetch URL content
+        Document document = null;
+        try {
+            document = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new MetadataFetcherException(String.format("METADATA FETCH ERROR for URL '%s', there was a problem while fetching its content", url));
+        }
         // TODO - Look for JSON-LD
         // TODO - Check on schema.org context
         return null;
