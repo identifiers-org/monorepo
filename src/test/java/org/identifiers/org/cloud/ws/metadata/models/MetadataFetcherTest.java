64f0d103d68b57cd0213243d28cf84373e71bf47
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 13:41:23 +0000

    unit test for metadata fetchers, testValidMetadata()

--- src/test/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherTest.java
@@ -1,9 +1,13 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -18,4 +22,14 @@ public class MetadataFetcherTest {
 
     @Autowired
     private MetadataFetcher metadataFetcher;
+
+    @Test
+    public void testValidMetadata() {
+        // TODO - Possible future extension here to make it more exhaustive
+        // TODO - This unit test is too dependant on that particular URL to be up, as soon as this is working, I may
+        // TODO - choose to deactivate this test
+        String url = "https://www.omicsdi.org/dataset/arrayexpress-repository/E-GEOD-37196";
+        String metadata = metadataFetcher.fetchMetadataFor(url);
+        assertThat(String.format("URL '%s' contains VALID metadata", url), metadata.isEmpty(), is(false));
+    }
 }
