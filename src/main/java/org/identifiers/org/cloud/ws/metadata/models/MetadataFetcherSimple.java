a1571dc6c60f27b93eb9e229fc14f1b8b8aeb0ea
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 13:02:25 +0000

    catch error were the page has multiple JSON-LD entries

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
@@ -2,6 +2,7 @@ package org.identifiers.org.cloud.ws.metadata.models;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
@@ -32,6 +33,13 @@ public class MetadataFetcherSimple implements MetadataFetcher {
         }
         logger.debug("Retrive content for URL '{}', titled '{}'", url, document.title());
         // TODO - Look for JSON-LD
+        String jsonldSelector = "script[type='application/ld+json'";
+        Elements jsonldElements = document.head().select(jsonldSelector);
+        if (jsonldElements.size() > 1) {
+            String errorMessage = String.format("MULTIPLE JSON-LD entries found in the header of URL '%s', entries: %s", url, jsonldElements.toString());
+            logger.error(errorMessage);
+            throw new MetadataFetcherException(errorMessage);
+        }
         // TODO - Check on schema.org context
         return null;
     }
