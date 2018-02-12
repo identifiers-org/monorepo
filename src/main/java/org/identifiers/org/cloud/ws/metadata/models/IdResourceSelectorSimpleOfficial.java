456e364b7c805239b22372e295826cb6dccda6b8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 11:22:14 +0000

    simple resource selector, skeleton

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResourceSelectorSimpleOfficial.java
@@ -0,0 +1,17 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+import java.util.List;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-12 11:21
+ * ---
+ */
+public class IdResourceSelectorSimpleOfficial implements IdResourceSelector {
+    @Override
+    public ResolverApiResponseResource selectResource(List<ResolverApiResponseResource> resources) throws IdResourceSelectorException {
+        return null;
+    }
+}
