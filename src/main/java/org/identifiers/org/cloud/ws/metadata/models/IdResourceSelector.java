585816c0b0e7af2cb1c8a4a6e302deb147bd4c74
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 11:19:03 +0000

    IdResourceSelector strategy interface

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResourceSelector.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-12 11:18
+ * ---
+ */
+public interface IdResourceSelector {
+    
+}
