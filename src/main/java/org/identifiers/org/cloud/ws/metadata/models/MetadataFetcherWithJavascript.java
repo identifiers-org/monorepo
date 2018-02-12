dd4e84a5de18446b13ccaa1fc65daeff0d9e603f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 16:01:40 +0000

    fetch web page and handle error

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherWithJavascript.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherWithJavascript.java
@@ -1,11 +1,13 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
-import com.ui4j.api.browser.BrowserEngine;
-import com.ui4j.api.browser.BrowserFactory;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -22,7 +24,13 @@ public class MetadataFetcherWithJavascript implements MetadataFetcher {
     @Override
     public String fetchMetadataFor(String url) throws MetadataFetcherException {
         // TODO - Fetch the URL content
-        BrowserEngine browserEngine = BrowserFactory.getWebKit();
+        WebClient webClient = new WebClient();
+        HtmlPage page = null;
+        try {
+            page = webClient.getPage(url);
+        } catch (IOException e) {
+            throw new MetadataFetcherException(String.format("METADATA FETCH ERROR for URL '%s', there was a problem while fetching its content", url));
+        }
         // TODO - Look for JSON-LD
         // TODO - Check on used contexts
         return null;
