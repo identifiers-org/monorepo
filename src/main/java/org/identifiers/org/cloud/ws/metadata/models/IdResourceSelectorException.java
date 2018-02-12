6944180bcd7d37562210ee3e6300ed6a232697bf
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 11:20:37 +0000

    IdResourceSelectorException

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResourceSelectorException.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-02-12 11:20
+ * ---
+ */
+public class IdResourceSelectorException extends RuntimeException {
+    public IdResourceSelectorException(String message) {
+        super(message);
+    }
+}
