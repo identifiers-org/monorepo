6921f1c12f6eb886a24313024c5c6d7213f04ab5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-12 13:28:21 +0000

    log successful metadata parsing

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherSimple.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -49,10 +51,22 @@ public class MetadataFetcherSimple implements MetadataFetcher {
         try {
             metadataRootNode = new ObjectMapper().readTree(metadata);
         } catch (IOException e) {
-            String errorMessage = String.format("JSON-LD PROCESSING ERROR for URL '%s', error '%s'", url, e.getMessage());
+            String errorMessage = String.format("JSON-LD PROCESSING ERROR for URL '%s', " +
+                    "METADATA being parsed '%s', " +
+                    "ERROR '%s'", url, metadata, e.getMessage());
             logger.error(errorMessage);
             throw new MetadataFetcherException(errorMessage);
         }
+        List<JsonNode> contextParents = metadataRootNode.findParents("@context");
+        if (contextParents.isEmpty()) {
+            String errorMessage = String.format("JSON-LD PROCESSING ERROR for URL '%s', " +
+                    "METADATA being parsed '%s', " +
+                    "NO CONTEXT DEFINITION NODES FOUND! INVALID JSON+LD", url, metadata);
+            logger.error(errorMessage);
+            throw new MetadataFetcherException(errorMessage);
+        }
+        String contexts = String.join(",", contextParents.stream().map(JsonNode::asText).collect(Collectors.toList()));
+        logger.info("SUCCESSFUL metadata extraction from URL '{}', METADATA '{}', found contexts '[{}]'", url, metadata, contexts);
         return metadata;
     }
 }
