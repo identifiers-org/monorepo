d7b54f6be51ebefd3fe23fa5cf9431791f4e2337
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-17 20:29:16 +0000

    RequestMetadataForUrl, ignore unknown properties

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/RequestMetadataForUrl.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/RequestMetadataForUrl.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: metadata
@@ -7,6 +9,7 @@ package org.identifiers.org.cloud.ws.metadata.models;
  * Timestamp: 2018-02-17 20:27
  * ---
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestMetadataForUrl {
     private String url;
 
