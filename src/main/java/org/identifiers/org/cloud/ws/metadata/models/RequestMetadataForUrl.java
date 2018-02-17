333b3b6619daba0de9e639222ebd53adeb6c2a3c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-17 20:28:50 +0000

    RequestMetadataForUrl, getters and setters

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/RequestMetadataForUrl.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/RequestMetadataForUrl.java
@@ -9,4 +9,13 @@ package org.identifiers.org.cloud.ws.metadata.models;
  */
 public class RequestMetadataForUrl {
     private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public RequestMetadataForUrl setUrl(String url) {
+        this.url = url;
+        return this;
+    }
 }
