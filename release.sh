f0ec429a5fd54bfedda45aeddc129a13eb1d5133
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-23 14:19:32 +0000

    release helper script, heading comments and defaults

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This script is a helper in the release cycle of the containerized service
+#
+# Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+
+# Defaults
+verb=nothing
+message=""
