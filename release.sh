3e3295a26220df82a5f1be1cfc3e3d363c7a7b47
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-23 14:34:21 +0000

    check if everything went ok

--- release.sh
+++ release.sh
@@ -17,6 +17,8 @@ fi
 
 # Read verb
 verb=$1
+# Everything ok?
+ok=false
 
 # Process release
 if [ "${verb}" == "patch" ]
@@ -25,4 +27,28 @@ then
     echo -e "\tCurrent version '${version}'"
     version=$(./increment_version.sh -p ${version})
     echo -e "\tNew version '${version}'"
+    echo "${version}" > VERSION
+fi
+if [ "${verb}" == "minor" ]
+then
+    echo "<===|DEVOPS|===> [RELEASE] MINOR"
+    echo -e "\tCurrent version '${version}'"
+    version=$(./increment_version.sh -m ${version})
+    echo -e "\tNew version '${version}'"
+    echo "${version}" > VERSION
+fi
+if [ "${verb}" == "major" ]
+then
+    echo "<===|DEVOPS|===> [RELEASE] MAJOR"
+    echo -e "\tCurrent version '${version}'"
+    version=$(./increment_version.sh -M ${version})
+    echo -e "\tNew version '${version}'"
+    echo "${version}" > VERSION
+fi
+
+# Should we publish the changes?
+if $ok ; then
+    echo -e "\tCommit, push and tag version"
+else
+    echo -e "\t--- ABORT --- Something went wrong"
 fi
