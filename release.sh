20908b62353a81a7ac1a3a51cec064bec0af9c1f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-23 14:29:49 +0000

    release patch action, in progress

--- release.sh
+++ release.sh
@@ -7,3 +7,22 @@
 # Defaults
 verb=nothing
 message=""
+version=$(cat VERSION)
+
+if [ $# -lt 1 ]
+then
+  echo "usage: $(basename $0) patch|minor|major major.minor.patch"
+  exit 1
+fi
+
+# Read verb
+verb=$1
+
+# Process release
+if [ "${verb}" == "patch" ]
+then
+    echo "<===|DEVOPS|===> [RELEASE] PATCH"
+    echo -e "\tCurrent version '${version}'"
+    version=$(./increment_version.sh -p ${version})
+    echo -e "\tNew version '${version}'"
+fi
