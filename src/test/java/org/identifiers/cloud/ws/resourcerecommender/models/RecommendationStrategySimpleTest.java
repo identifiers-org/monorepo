3ac6c984d44c4ee6d7b0fd216e50ba2dd7f27ea9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 12:29:45 +0000

    unit test for simple recommendation strategy

---
+++ src/test/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategySimpleTest.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-02-27 12:28
+ * ---
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecommendationStrategySimpleTest {
+
+}
