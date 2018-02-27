c93121cc7ebb16324c5a7c23899cf72522fae4f4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 21:41:17 +0000

    recommender service, preparing re-try pattern

--- src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
@@ -1,7 +1,12 @@
 package org.identifiers.cloud.ws.resolver.models;
 
 import org.identifiers.cloud.ws.resolver.data.models.ResourceEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,6 +21,8 @@ import java.util.List;
 @Component
 @Scope("prototype")
 public class ResourceRecommenderService implements ResourceRecommenderStrategy {
+    public static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
+
     @Override
     public List<RecommendedResource> getRecommendations(List<ResourceEntry> resources) throws ResourceRecommenderStrategyException{
         return null;
