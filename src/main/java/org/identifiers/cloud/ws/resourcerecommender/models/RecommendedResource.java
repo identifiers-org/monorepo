3cd93805610170d3a4061b5597aaee26ea9b4695
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:44:24 +0000

    model for a resource recommendation, placeholder

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendedResource.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendedResource.java
@@ -1,16 +1,11 @@
 package org.identifiers.cloud.ws.resourcerecommender.models;
 
-import org.springframework.http.HttpStatus;
-
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resource-recommender
  * Package: org.identifiers.cloud.ws.resourcerecommender.models
- * Timestamp: 2018-02-27 11:33
+ * Timestamp: 2018-02-27 11:43
  * ---
  */
-public class ResourceRecommenderApiResponse {
-    private String errorMessage;
-    private HttpStatus httpStatus = HttpStatus.OK;
-    // TODO - payload
+public class RecommendedResource {
 }
