e3a183bc124bb9461cd5979c3a49affc1ff3c164
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 12:14:55 +0000

    corner case, no official resource has been provided

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategySimple.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategySimple.java
@@ -2,7 +2,9 @@ package org.identifiers.cloud.ws.resourcerecommender.models;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
@@ -16,11 +18,13 @@ import java.util.stream.Collectors;
 public class RecommendationStrategySimple implements RecommendationStrategy {
     @Override
     public List<RecommendedResource> getRecommendations(List<ResolvedResource> resolvedResources) {
+        AtomicReference<Boolean> thereIsOfficialResource = new AtomicReference<>(false);
         List<RecommendedResource> recommendations = resolvedResources.parallelStream().map(resolvedResource -> {
             RecommendedResource recommendedResource = new RecommendedResource()
                     .setEndPointUrl(resolvedResource.getEndPointUrl())
                     .setId(resolvedResource.getId());
             if (resolvedResource.isOfficial()) {
+                thereIsOfficialResource.set(Boolean.TRUE);
                 return recommendedResource
                         .setRecommendationIndex(99)
                         .setRecommendationExplanation("Official resource in this context");
@@ -33,7 +37,14 @@ public class RecommendationStrategySimple implements RecommendationStrategy {
             recommendations.get(0)
                     .setRecommendationIndex(99)
                     .setRecommendationExplanation("This is the ONLY resource available within this context");
+            return recommendations;
         }
+        // There is no official resource, pick one randomly
+        Collections.shuffle(recommendations);
+        recommendations.get(0)
+                .setRecommendationIndex(99)
+                .setRecommendationExplanation("There are multiple resources for this case, and none of them is official, " +
+                        "so this one has been chosen randomly");
         return recommendations;
     }
 }
