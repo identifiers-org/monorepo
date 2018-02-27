94f707df887b5549e9c1d92b4427ef2394811177
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:01:22 +0000

    sorce code skeleton

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/ResourceRecommenderApplication.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resourcerecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceRecommenderApplication.class, args);
+	}
+}
---
+++ src/test/java/org/identifiers/cloud/ws/resourcerecommender/ResourceRecommenderApplicationTests.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.ws.resourcerecommender;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourceRecommenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
