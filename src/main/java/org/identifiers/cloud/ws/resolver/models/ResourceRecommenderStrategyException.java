d4a805faa6af32a3705308ed4bc674f6c158b4a5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 21:36:33 +0000

    ResourceRecommenderStrategyException

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderStrategyException.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-02-27 21:36
+ * ---
+ */
+public class ResourceRecommenderStrategyException extends RuntimeException {
+    public ResourceRecommenderStrategyException(String message) {
+        super(message);
+    }
+}
