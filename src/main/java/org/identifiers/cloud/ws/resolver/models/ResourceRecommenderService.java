90dc6983195e9ef06038f0d19bb5369ec2b53358
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 21:35:30 +0000

    first and only resource recommender strategy, delegation on Resource Recommender Service

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
@@ -0,0 +1,23 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+import org.identifiers.cloud.ws.resolver.data.models.ResourceEntry;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-02-27 21:34
+ * ---
+ */
+@Component
+@Scope("prototype")
+public class ResourceRecommenderService implements ResourceRecommenderStrategy {
+    @Override
+    public List<RecommendedResource> getRecommendations(List<ResourceEntry> resources) {
+        return null;
+    }
+}
