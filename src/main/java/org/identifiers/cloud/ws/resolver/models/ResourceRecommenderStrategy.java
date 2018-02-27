5ea63849997716e8ce8854a1a24f3547728dbcfc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 21:36:58 +0000

    updated resource recommender strategy interface

--- src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderStrategy.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderStrategy.java
@@ -12,5 +12,5 @@ import java.util.List;
  * ---
  */
 public interface ResourceRecommenderStrategy {
-    List<RecommendedResource> getRecommendations(List<ResourceEntry> resources);
+    List<RecommendedResource> getRecommendations(List<ResourceEntry> resources) throws ResourceRecommenderStrategyException;
 }
