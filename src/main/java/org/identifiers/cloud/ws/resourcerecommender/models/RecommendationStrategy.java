767b5ade851a3b3e74bcd17034141945680f7416
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:56:19 +0000

    Recommendation strategy interface

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategy.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-02-27 11:56
+ * ---
+ */
+public interface RecommendationStrategy {
+}
