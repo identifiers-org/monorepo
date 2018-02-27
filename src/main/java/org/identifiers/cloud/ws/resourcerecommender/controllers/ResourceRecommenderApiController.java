a1d1ce106b6d035e3e74380e43cad9f596697d6a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:25:55 +0000

    liveness probe

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/controllers/ResourceRecommenderApiController.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/controllers/ResourceRecommenderApiController.java
@@ -2,6 +2,7 @@ package org.identifiers.cloud.ws.resourcerecommender.controllers;
 
 import org.identifiers.cloud.ws.resourcerecommender.models.ResourceRecommenderApiModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -13,5 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ResourceRecommenderApiController {
 
     @Autowired
-    private ResourceRecommenderApiModel myModel;
+    private ResourceRecommenderApiModel model;
+
+    // liveness probe
+    @RequestMapping(value = "/liveness_check")
+    public String livenessCheck() {
+        // TODO - This will be refactored out later, it will be the model who will implement the logic to determine
+        // TODO - whether the service should be considered "alive" or not, but this code will live here for testing
+        // TODO - purposes
+        return model.livenessCheck();
+    }
+
 }
