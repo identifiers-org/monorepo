2fdb020032d0f42a205fce31b3bf0537ff431488
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 12:31:32 +0000

    official and unofficial resolved resources, fields

--- src/test/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategySimpleTest.java
+++ src/test/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategySimpleTest.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: resource-recommender
@@ -14,5 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RecommendationStrategySimpleTest {
+    List<ResolvedResource> officialResolvedResources;
+    List<ResolvedResource> unOfficialResolvedResources;
 
+    
 }
