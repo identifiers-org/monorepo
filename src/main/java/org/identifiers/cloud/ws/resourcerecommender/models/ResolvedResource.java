ed8d32895af3634403037eeaef296bfdac69694f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:11:16 +0000

    recommendation request context

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ResolvedResource.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ResolvedResource.java
@@ -8,4 +8,11 @@ package org.identifiers.cloud.ws.resourcerecommender.models;
  * ---
  */
 public class ResolvedResource {
+    // Even if we have access to another service that, given a resource ID, could provide information on that resource,
+    // these particular attributes exist within the context that resource / provider for a particular Compact ID. In the
+    // future, we could include more context information related to the particularities of the current recommendation
+    // request to fine tune the recommendation mechanism
+    private String id;
+    private String endPointUrl;
+    private boolean official;
 }
