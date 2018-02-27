34e580b9cda49cbf5dbe9e8774a11ebb4a5d6e90
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:32:41 +0000

    sortByRecommendationDescending API endpoint placeholder

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/controllers/ResourceRecommenderApiController.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/controllers/ResourceRecommenderApiController.java
@@ -1,8 +1,12 @@
 package org.identifiers.cloud.ws.resourcerecommender.controllers;
 
 import org.identifiers.cloud.ws.resourcerecommender.models.ResourceRecommenderApiModel;
+import org.identifiers.cloud.ws.resourcerecommender.models.ResourceRecommenderRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -15,6 +19,13 @@ public class ResourceRecommenderApiController {
 
     @Autowired
     private ResourceRecommenderApiModel model;
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<?> sortByRecommendationDescending(@RequestBody ResourceRecommenderRequest request) {
+        // The model associated with the controller should handle any possible exception that could happen while running
+        // the business logic, thus, the controller should handle only exceptions within the domain of the controller.
+        return null;
+    }
 
     // liveness probe
     @RequestMapping(value = "/liveness_check")
