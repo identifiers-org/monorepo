f5310d7752716b197fa95a084f5c755c8557713c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 21:26:36 +0000

    RecommenderStrategy interface, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/RecommenderStrategy.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2018-02-27 21:26
+ * ---
+ */
+public interface RecommenderStrategy {
+}
