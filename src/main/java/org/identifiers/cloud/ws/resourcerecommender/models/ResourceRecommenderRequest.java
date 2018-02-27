47e71ce023c696f66c1b0163eb274eb0c238a213
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:18:39 +0000

    modeling the request to the resource recommender service

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ResourceRecommenderRequest.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-02-27 11:18
+ * ---
+ */
+public class ResourceRecommenderRequest {
+}
