a596df9978c4964bbdda8de57f3aa149bf477877
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-27 11:04:40 +0000

    main API controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/controllers/ResourceRecommenderApiController.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resourcerecommender.controllers;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.controllers
+ * Timestamp: 2018-02-27 11:04
+ * ---
+ */
+public class ResourceRecommenderApiController {
+}
