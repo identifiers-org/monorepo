16c360b0c02bdcc0e0077f8668df46588260b23f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-28 13:47:16 +0000

    error handler for the request

--- src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
@@ -5,12 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +48,25 @@ public class ResourceRecommenderService implements ResourceRecommenderStrategy {
 
     private String resourceRecommenderServiceHost;
     private String resourceRecommenderServicePort;
+
+    // Error handler for the request
+    class RestTemplateErrorHandler implements ResponseErrorHandler {
+        ClientHttpResponse clientHttpResponse;
+
+        @Override
+        public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
+            // We're going to say that it has no error so we can't handle this properly
+            return false;
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
+            logger.error("The following error came back from the Recommender Service, HTTP Status #{}, error content '{}'",
+                    clientHttpResponse.getRawStatusCode(),
+                    clientHttpResponse.getStatusText());
+        }
+    }
+
 
     @Override
     public List<RecommendedResource> getRecommendations(List<ResourceEntry> resources) throws ResourceRecommenderStrategyException{
