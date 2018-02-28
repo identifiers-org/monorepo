38588168c7b90601683e699fafe8911ccb507004
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-28 14:25:51 +0000

    Recommendation, getters and setters

--- src/main/java/org/identifiers/cloud/ws/resolver/models/Recommendation.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/Recommendation.java
@@ -11,4 +11,22 @@ public class Recommendation {
     // This models a recommendation attached to a particular resource in the response from this web service
     private int recommendationIndex = 0;
     private String recommendationExplanation = "--- default explanation ---";
+
+    public int getRecommendationIndex() {
+        return recommendationIndex;
+    }
+
+    public Recommendation setRecommendationIndex(int recommendationIndex) {
+        this.recommendationIndex = recommendationIndex;
+        return this;
+    }
+
+    public String getRecommendationExplanation() {
+        return recommendationExplanation;
+    }
+
+    public Recommendation setRecommendationExplanation(String recommendationExplanation) {
+        this.recommendationExplanation = recommendationExplanation;
+        return this;
+    }
 }
