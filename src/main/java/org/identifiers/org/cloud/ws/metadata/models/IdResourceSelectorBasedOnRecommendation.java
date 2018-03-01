3ff7ac430e6c57977f395f855aa490b58b9c3a18
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-01 01:51:54 +0000

    selector algorithm

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResourceSelectorBasedOnRecommendation.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResourceSelectorBasedOnRecommendation.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.metadata.models;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -9,9 +12,11 @@ import java.util.List;
  * Timestamp: 2018-03-01 1:41
  * ---
  */
+@Component
 public class IdResourceSelectorBasedOnRecommendation implements IdResourceSelector {
     @Override
     public ResolverApiResponseResource selectResource(List<ResolverApiResponseResource> resources) throws IdResourceSelectorException {
-        return null;
+        resources.sort(Comparator.comparing(resolverApiResponseResource -> resolverApiResponseResource.getRecommendation().getRecommendationIndex()));
+        return resources.get(resources.size() - 1);
     }
 }
