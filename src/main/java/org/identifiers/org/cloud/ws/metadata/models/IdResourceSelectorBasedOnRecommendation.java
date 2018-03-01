197fc2546bb5fc104002614eea72441d6df22e85
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-01 01:41:50 +0000

    new selection strategy in metadata service, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/IdResourceSelectorBasedOnRecommendation.java
@@ -0,0 +1,17 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+import java.util.List;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-03-01 1:41
+ * ---
+ */
+public class IdResourceSelectorBasedOnRecommendation implements IdResourceSelector {
+    @Override
+    public ResolverApiResponseResource selectResource(List<ResolverApiResponseResource> resources) throws IdResourceSelectorException {
+        return null;
+    }
+}
