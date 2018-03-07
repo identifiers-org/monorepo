2d9aee71549a2658a05da29b39d1456c39824ade
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 07:59:14 +0000

    ServiceResponseResolve

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/api/responses/ServiceResponseResolve.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.ws.resolver.models.api.responses;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.ws.resolver.models.api.responses
+ * Timestamp: 2018-03-07 7:42
+ * ---
+ */
+public class ServiceResponseResolve extends ServiceResponse<ResponseResolvePayload> {
+}
