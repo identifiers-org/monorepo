9ed50595a9cb9a97c15f83ef6a8f36eba8784227
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 13:35:31 +0000

    this is also part of the new API

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/api/requests/ServiceRequestFetchMetadataForUrl.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.metadata.models.api.requests;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models.api.requests
+ * Timestamp: 2018-03-07 13:17
+ * ---
+ */
+public class ServiceRequestFetchMetadataForUrl extends ServiceRequest<RequestFetchMetadataForUrlPayload> {
+}
---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/api/responses/ServiceResponseFetchMetadataForUrl.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.metadata.models.api.responses;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models.api.responses
+ * Timestamp: 2018-03-07 13:18
+ * ---
+ */
+public class ServiceResponseFetchMetadataForUrl extends ServiceResponse<ResponseFetchMetadataForUrlPayload> {
+}
