1b85fae57bb546141bc7df861d738c67cb917fe9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 07:55:33 +0000

    ResponseResolvePayload

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/api/responses/ResponseResolvePayload.java
@@ -0,0 +1,24 @@
+package org.identifiers.cloud.ws.resolver.models.api.responses;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.ws.resolver.models.api.responses
+ * Timestamp: 2018-03-07 7:40
+ * ---
+ */
+public class ResponseResolvePayload implements Serializable {
+    private List<ResolvedResource> resolvedResources;
+
+    public List<ResolvedResource> getResolvedResources() {
+        return resolvedResources;
+    }
+
+    public ResponseResolvePayload setResolvedResources(List<ResolvedResource> resolvedResources) {
+        this.resolvedResources = resolvedResources;
+        return this;
+    }
+}
