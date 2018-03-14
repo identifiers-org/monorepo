518577780811873d5b1167437f03a1878546a74c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-14 13:06:45 +0000

    updated code with latest release of libapi

--- src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResourceRecommenderService.java
@@ -1,6 +1,6 @@
 package org.identifiers.cloud.ws.resolver.models;
 
-import org.identifiers.cloud.libapi.ApiServicesFactory;
+import org.identifiers.cloud.libapi.services.ApiServicesFactory;
 import org.identifiers.cloud.libapi.models.resourcerecommender.ResolvedResource;
 import org.identifiers.cloud.libapi.models.resourcerecommender.ResourceRecommendation;
 import org.identifiers.cloud.ws.resolver.data.models.ResourceEntry;
