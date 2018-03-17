56bcac9561dc7ab55ac421f0f76f72cdc63c1f33
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-17 05:43:09 +0000

    production env configuration, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/configuration/ProductionConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.configuration;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.configuration
+ * Timestamp: 2018-03-17 5:42
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ProductionConfiguration {
+}
