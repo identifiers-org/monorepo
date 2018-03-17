ff643fab5dbfdde0e75b6f9bfea7c6bdfc9ccdd5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-17 05:43:30 +0000

    development env configuration, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/configuration/DevelopmentConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.configuration;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.configuration
+ * Timestamp: 2018-03-17 5:43
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class DevelopmentConfiguration {
+}
