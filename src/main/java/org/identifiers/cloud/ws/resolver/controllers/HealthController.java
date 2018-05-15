83b748897a6b4d814b476c7a283f7252659b4f0e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-15 14:49:16 +0100

    bug fix, missing annotation

--- src/main/java/org/identifiers/cloud/ws/resolver/controllers/HealthController.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/controllers/HealthController.java
@@ -3,6 +3,7 @@ package org.identifiers.cloud.ws.resolver.controllers;
 import org.identifiers.cloud.ws.resolver.models.HealthApiModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Project: resolver
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  * This controller offers the endpoints that provide health checks for the service, e.g. liveness and health check
  */
+@RestController
 public class HealthController {
     @Autowired
     private HealthApiModel model;
