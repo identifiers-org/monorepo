65960d2c7b02c82d9f2ca4d912376e0d144b217e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-15 13:33:13 +0100

    new health controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/controllers/HealthController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.controllers;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.controllers
+ * Timestamp: 2018-05-15 13:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HealthController {
+}
