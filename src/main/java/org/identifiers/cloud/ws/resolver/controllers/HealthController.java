e5ab0a15b2cb9db30eb40d3ad19a02295fa4737c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-15 13:33:49 +0100

    new health controller, documentation

--- src/main/java/org/identifiers/cloud/ws/resolver/controllers/HealthController.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/controllers/HealthController.java
@@ -7,6 +7,8 @@ package org.identifiers.cloud.ws.resolver.controllers;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This controller offers the endpoints that provide health checks for the service, e.g. liveness and health check
  */
 public class HealthController {
 }
