045961a021c8c5ed3cadc11a32fd705d4ddb85f5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-15 13:35:26 +0100

    Health API model, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/HealthApiModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-05-15 13:34
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HealthApiModel {
+}
