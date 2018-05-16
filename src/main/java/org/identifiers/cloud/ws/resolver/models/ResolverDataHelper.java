9336474b90f7c885f6262d38f92814d9749b4c04
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-16 15:40:07 +0100

    ResolverDataHelper placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolverDataHelper.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-05-16 15:39
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResolverDataHelper {
+}
