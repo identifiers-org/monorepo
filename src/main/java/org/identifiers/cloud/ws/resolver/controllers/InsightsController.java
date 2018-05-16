b2fafba5d5392c6fe2ca9b70188f2ad91ce5ddcd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-16 13:17:23 +0100

    Insights Controller placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/controllers/InsightsController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.controllers;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.controllers
+ * Timestamp: 2018-05-16 13:16
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class InsightsController {
+}
