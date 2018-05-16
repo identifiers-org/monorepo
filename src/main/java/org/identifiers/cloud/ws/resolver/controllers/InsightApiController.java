1baae48d029a53ee6e44c365f9de3a3e7acfa1b9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-16 13:57:30 +0100

    autowire model

--- src/main/java/org/identifiers/cloud/ws/resolver/controllers/InsightApiController.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/controllers/InsightApiController.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.ws.resolver.controllers;
 
+import org.identifiers.cloud.ws.resolver.models.InsightApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class InsightApiController {
-
     // TODO
+    @Autowired
+    private InsightApiModel model;
 }
