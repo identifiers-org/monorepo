b800c49d0e873912f44e54f515312281097a71e2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-16 13:50:31 +0100

    InsightApiModel, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/InsightApiModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-05-16 13:50
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class InsightApiModel {
+}
