6a3988e0a5fb2a2909c204e4829786912c742257
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-17 12:35:45 +0100

    API Central management, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ApiCentral.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.models;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.models
+ * Timestamp: 2018-05-17 12:35
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiCentral {
+}
