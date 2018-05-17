e21907258e3f3ccf212c940459f027f543e4359c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-17 14:15:14 +0100

    getAllHomeUrls() first implementation

--- src/main/java/org/identifiers/cloud/ws/resolver/controllers/InsightApiController.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/controllers/InsightApiController.java
@@ -3,7 +3,6 @@ package org.identifiers.cloud.ws.resolver.controllers;
 import org.identifiers.cloud.ws.resolver.models.InsightApiModel;
 import org.identifiers.cloud.ws.resolver.models.api.responses.ServiceResponseResolve;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +31,7 @@ public class InsightApiController {
 
     @RequestMapping(value = "get_all_home_urls")
     public ResponseEntity<?> getAllHomeUrls() {
-        // TODO
-        return new ResponseEntity<>("get_all_home_urls", HttpStatus.OK);
+        ServiceResponseResolve response = model.getAllHomeUrls();
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 }
