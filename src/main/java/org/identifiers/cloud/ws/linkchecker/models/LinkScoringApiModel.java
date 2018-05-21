2868aab2f617a9888ef8c0e7d792733165a111e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-21 10:41:22 +0100

    LinkScoringApiModel, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/models/LinkScoringApiModel.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.ws.linkchecker.models;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.models
+ * Timestamp: 2018-05-21 10:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+@Scope("prototype")
+public class LinkScoringApiModel {
+    // TODO
+}
