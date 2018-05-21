592b6929b5604d61a107c6486c7d67422c13df01
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-21 10:40:25 +0100

    link scoring api controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/controllers/LinkScoringApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.controllers;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.controllers
+ * Timestamp: 2018-05-21 10:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkScoringApiController {
+}
