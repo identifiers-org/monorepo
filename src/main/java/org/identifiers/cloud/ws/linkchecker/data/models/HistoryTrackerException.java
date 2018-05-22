1c1532f5c8f50ea700dba9cddb2722acfdb04eab
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-22 15:56:08 +0100

    HistoryTrackerException

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/HistoryTrackerException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-05-22 15:55
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HistoryTrackerException extends RuntimeException {
+    public HistoryTrackerException(String message) {
+        super(message);
+    }
+}
