f059dc0d1b726acb781a567ef024f53ede2a9841
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-22 10:48:52 +0100

    CheckedUrl entity placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/CheckedUrl.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-05-22 10:48
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class CheckedUrl {
+}
