96a4200aa36da1531aac4666e96ab772f05db7ee
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-22 13:48:50 +0100

    CheckedUrlHistoryStats interface

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/CheckedUrlHistoryStats.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-05-22 13:48
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface CheckedUrlHistoryStats {
+    // TODO
+}
