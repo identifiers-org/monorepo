027591499fe2f26aa2cb145f034093af158d9787
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-22 11:56:27 +0100

    a resolution entity will keep track of resolved CIDs

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/ResolutionEntity.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-05-22 11:55
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResolutionEntity {
+}
