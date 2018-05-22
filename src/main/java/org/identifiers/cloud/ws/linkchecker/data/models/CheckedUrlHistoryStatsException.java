1f6e33bd6b7b101fd51bcc6a04bd8e1f13146b8f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-22 15:13:05 +0100

    history stats exeption

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/CheckedUrlHistoryStatsException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-05-22 15:12
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class CheckedUrlHistoryStatsException extends RuntimeException {
+    public CheckedUrlHistoryStatsException(String message) {
+        super(message);
+    }
+}
