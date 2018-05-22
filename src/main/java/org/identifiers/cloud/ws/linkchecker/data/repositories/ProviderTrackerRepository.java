e783a5691e6f3f4d78c9b5338da0fff67679f51d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-22 16:18:28 +0100

    ProviderTrackerRepository

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/repositories/ProviderTrackerRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.ws.linkchecker.data.repositories;
+
+import org.identifiers.cloud.ws.linkchecker.data.models.ProviderTracker;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.repositories
+ * Timestamp: 2018-05-22 16:17
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ProviderTrackerRepository extends CrudRepository<ProviderTracker, String> {
+    // TODO
+}
