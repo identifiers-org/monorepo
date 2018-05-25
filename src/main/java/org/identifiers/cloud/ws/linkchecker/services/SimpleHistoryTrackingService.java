5509ce34e749ae7e13632c875c470afdddfa6cba
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-25 11:47:34 +0100

    history tracking service component

--- src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
@@ -1,6 +1,7 @@
 package org.identifiers.cloud.ws.linkchecker.services;
 
 import org.identifiers.cloud.ws.linkchecker.models.ProviderTracker;
+import org.springframework.stereotype.Component;
 
 /**
  * Project: link-checker
@@ -10,6 +11,7 @@ import org.identifiers.cloud.ws.linkchecker.models.ProviderTracker;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
 public class SimpleHistoryTrackingService implements HistoryTrackingService {
     @Override
     public ProviderTracker getTrackerForProvider(String providerId) {
