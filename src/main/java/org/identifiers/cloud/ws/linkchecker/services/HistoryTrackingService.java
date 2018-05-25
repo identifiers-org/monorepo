95bf8fc14ba18870a1719c07184405a273297252
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-25 11:40:38 +0100

    History tracking service refactored as interface

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/HistoryTrackingService.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.linkchecker.services;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.services
+ * Timestamp: 2018-05-25 11:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * A history tracking service maintains and provides the tracking information for the requested entity.
+ */
+public interface HistoryTrackingService {
+}
