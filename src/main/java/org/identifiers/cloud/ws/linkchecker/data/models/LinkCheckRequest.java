bca061b1a93d76eb12e2843c240d95cded937f69
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-25 04:36:46 +0100

    link check request model, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckRequest.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-05-25 4:36
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkCheckRequest {
+}
