17abd4978cafbc04d62af132902819cd7d94adab
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-25 11:37:51 +0100

    HistoryTrackingService, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/HistoryTrackingService.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.services;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.services
+ * Timestamp: 2018-05-25 11:37
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HistoryTrackingService {
+}
