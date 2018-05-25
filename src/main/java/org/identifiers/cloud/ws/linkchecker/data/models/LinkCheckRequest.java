e1a36146b58cc7b1838f5e1d6c03efe685f6d71d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-25 04:38:27 +0100

    core fields of a link check request

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckRequest.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckRequest.java
@@ -1,5 +1,8 @@
 package org.identifiers.cloud.ws.linkchecker.data.models;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.data.models
@@ -8,5 +11,34 @@ package org.identifiers.cloud.ws.linkchecker.data.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-public class LinkCheckRequest {
+public class LinkCheckRequest implements Serializable, Comparable<LinkCheckRequest> {
+    // URL that has been checked
+    protected String url;
+    // When it has been checked (UTC)
+    protected Timestamp timestamp;
+    
+
+    public String getUrl() {
+        return url;
+    }
+
+    public LinkCheckRequest setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public LinkCheckRequest setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+        return this;
+    }
+
+    @Override
+    public int compareTo(LinkCheckRequest o) {
+        return this.timestamp.compareTo(o.getTimestamp());
+    }
+
 }
