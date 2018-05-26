a6b91ba889df37990bf7f40a05d9809093260e23
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-27 00:05:11 +0100

    TODO on handling the error

--- src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
@@ -81,7 +81,7 @@ public class SimpleHistoryTrackingService implements HistoryTrackingService {
                 }
             });
         } catch (ExecutionException e) {
-            e.printStackTrace();
+            // TODO
         }
     }
 }
