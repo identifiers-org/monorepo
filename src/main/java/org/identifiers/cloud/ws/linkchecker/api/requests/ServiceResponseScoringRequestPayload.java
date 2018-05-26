d5ccc3303a4b455f01f757dd049ad5c82816f00e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-26 10:24:32 +0100

    service response to a scoring request, building

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/requests/ServiceResponseScoringRequestPayload.java
@@ -0,0 +1,17 @@
+package org.identifiers.cloud.ws.linkchecker.api.requests;
+
+import java.io.Serializable;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.requests
+ * Timestamp: 2018-05-26 10:23
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Response to a Scoring request
+ */
+public class ServiceResponseScoringRequestPayload implements Serializable {
+    
+}
