2c29daaa3d18bee6df0390da2b31b696d139485c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-26 10:34:55 +0100

    PlainScoringRequestPayload, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/requests/PlainScoringRequestPayload.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.ws.linkchecker.api.requests;
+
+import java.io.Serializable;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.requests
+ * Timestamp: 2018-05-26 10:34
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PlainScoringRequestPayload implements Serializable {
+
+}
