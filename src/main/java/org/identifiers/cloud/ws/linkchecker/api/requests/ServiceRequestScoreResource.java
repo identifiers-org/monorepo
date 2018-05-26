dc6fcf2ded73119f831aac5cd6cce4ac92541263
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-26 10:40:15 +0100

    ServiceRequestScoreResource

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/requests/ServiceRequestScoreResource.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.api.requests;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.requests
+ * Timestamp: 2018-05-26 10:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestScoreResource extends ServiceRequest<ScoringRequestWithIdPayload> {
+}
