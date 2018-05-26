a21bdcf07619c31d5e7a2652a436cccca42d5b51
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-26 09:52:29 +0100

    getters and setters

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/TrackedProvider.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/TrackedProvider.java
@@ -34,4 +34,57 @@ public class TrackedProvider implements Serializable {
     @Indexed
     private Timestamp created;
 
+    public String getId() {
+        return id;
+    }
+
+    public TrackedProvider setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TrackedProvider setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public TrackedProvider setInstitution(String institution) {
+        this.institution = institution;
+        return this;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public TrackedProvider setLocation(String location) {
+        this.location = location;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public TrackedProvider setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public TrackedProvider setCreated(Timestamp created) {
+        this.created = created;
+        return this;
+    }
 }
