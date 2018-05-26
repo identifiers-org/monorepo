92fdfeebc9a0cad9448746d9e9e583fcc9ab6489
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-27 00:27:57 +0100

    SimpleHistoryTrackingServiceException

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.ws.linkchecker.services;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.services
+ * Timestamp: 2018-05-27 0:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class SimpleHistoryTrackingServiceException extends HistoryTrackingServiceException {
+    public SimpleHistoryTrackingServiceException(String message) {
+        super(message);
+    }
+}
