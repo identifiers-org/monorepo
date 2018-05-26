8295d20ca90b10deaea0872fba82b254beecce60
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-27 00:04:22 +0100

    building a provider tracker on the fly

--- src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
@@ -1,6 +1,9 @@
 package org.identifiers.cloud.ws.linkchecker.services;
 
-import com.google.common.cache.*;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import org.identifiers.cloud.ws.linkchecker.api.requests.ScoringRequestWithIdPayload;
 import org.identifiers.cloud.ws.linkchecker.data.repositories.TrackedProviderRepository;
 import org.identifiers.cloud.ws.linkchecker.models.ProviderTracker;
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
 import java.util.concurrent.*;
 
 /**
@@ -23,18 +27,30 @@ import java.util.concurrent.*;
 @Component
 public class SimpleHistoryTrackingService implements HistoryTrackingService {
     private final static Logger logger = LoggerFactory.getLogger(SimpleHistoryTrackingService.class);
+    // Cached stats
+    Cache<String, ProviderTracker> providers;
     // Provider trackers by Provider ID
     private ConcurrentMap<String, ProviderTracker> providerTrackers = new ConcurrentHashMap<>();
-
     @Value("${org.identifiers.cloud.ws.linkchecker.backend.data.cache.expiry.seconds}")
     private long cacheExpirySeconds;
-
     @Value("${org.identifiers.cloud.ws.linkchecker.backend.data.cache.size}")
     private long cacheSize;
-
     // Repositories
     @Autowired
     private TrackedProviderRepository trackedProviderRepository;
+
+    public SimpleHistoryTrackingService() {
+        providers = CacheBuilder.newBuilder()
+                .maximumSize(cacheSize)
+                .expireAfterWrite(cacheExpirySeconds, TimeUnit.SECONDS)
+                .removalListener(new RemovalListener<String, ProviderTracker>() {
+                    @Override
+                    public void onRemoval(RemovalNotification<String, ProviderTracker> removalNotification) {
+                        processProviderEviction(removalNotification);
+                    }
+                })
+                .build();
+    }
 
     // Cache Eviction Listener Actions
     private void processProviderEviction(RemovalNotification<String, ProviderTracker> removalNotification) {
@@ -51,30 +67,17 @@ public class SimpleHistoryTrackingService implements HistoryTrackingService {
         });
     }
 
-    // Cached stats
-    Cache<String, ProviderTracker> providers;
-
-    public SimpleHistoryTrackingService() {
-        providers = CacheBuilder.newBuilder()
-                .maximumSize(cacheSize)
-                .expireAfterWrite(cacheExpirySeconds, TimeUnit.SECONDS)
-                .removalListener(new RemovalListener<String, ProviderTracker>() {
-                    @Override
-                    public void onRemoval(RemovalNotification<String, ProviderTracker> removalNotification) {
-                        processProviderEviction(removalNotification);
-                    }
-                })
-                .build();
-    }
-
     @Override
     public ProviderTracker getTrackerForProvider(ScoringRequestWithIdPayload scoringRequestWithIdPayload) {
         try {
             return providers.get(scoringRequestWithIdPayload.getId(), new Callable<ProviderTracker>() {
                 @Override
                 public ProviderTracker call() throws Exception {
-                    // TODO
-                    return null;
+                    ProviderTracker providerTracker = new ProviderTracker();
+                    providerTracker.setId(scoringRequestWithIdPayload.getId())
+                            .setUrl(scoringRequestWithIdPayload.getUrl())
+                            .setCreated(new Timestamp(System.currentTimeMillis()));
+                    return providerTracker;
                 }
             });
         } catch (ExecutionException e) {
