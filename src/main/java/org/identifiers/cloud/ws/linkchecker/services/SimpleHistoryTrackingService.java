da6cf9a644e568682c71485f7d3252aea0ff6ef8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-27 00:07:18 +0100

    load from repo

--- src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/services/SimpleHistoryTrackingService.java
@@ -77,6 +77,7 @@ public class SimpleHistoryTrackingService implements HistoryTrackingService {
             return providers.get(scoringRequestWithIdPayload.getId(), new Callable<ProviderTracker>() {
                 @Override
                 public ProviderTracker call() throws Exception {
+                    // TODO - Load from Repo
                     return providerTracker;
                 }
             });
