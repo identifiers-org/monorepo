25e9ed4f366844caf760e559db7de23e8eac45b4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-26 09:50:42 +0100

    TrackedProvider data model, core attributes

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/TrackedProvider.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/TrackedProvider.java
@@ -9,8 +9,16 @@ import java.io.Serializable;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
- *
+ * <p>
  * This models a provider being tracked by the service.
  */
 public class TrackedProvider implements Serializable {
+    // Provider ID within the context of a namespace or prefix
+    private String id;
+    // A description of this provider within the context of a namespace or prefix
+    private String description;
+    // Institution information
+    private String institution;
+    // Location information on this provider within the context of a namespace or prefix, if available
+    private String location;
 }
