08f797ecac0b66ee300968be6bbcdb04ba975409
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 09:21:57 +0100

    SimpleLinkCheckerTest, for working out how the link checking will work

---
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/strategies/SimpleLinkCheckerTest.java
@@ -0,0 +1,21 @@
+package org.identifiers.cloud.ws.linkchecker.strategies;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.strategies
+ * Timestamp: 2018-05-28 9:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleLinkCheckerTest {
+
+}
