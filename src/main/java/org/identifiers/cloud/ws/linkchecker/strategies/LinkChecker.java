c15e7cb36a8aa64966a8ee87d824dcb616dc276c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 08:24:14 +0100

    Link Checker strategy interface, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/strategies/LinkChecker.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.strategies;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.strategies
+ * Timestamp: 2018-05-28 8:24
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface LinkChecker {
+}
