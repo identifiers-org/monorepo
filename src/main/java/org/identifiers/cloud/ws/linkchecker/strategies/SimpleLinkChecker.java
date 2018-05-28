129aa184fcf282aa7aa5c08afc38b0d391a3038e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 08:44:51 +0100

    simple link checker, in progress

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/strategies/SimpleLinkChecker.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.ws.linkchecker.strategies;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.strategies
+ * Timestamp: 2018-05-28 8:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This class implements a simple link checking strategy based on HTTP GET request.
+ */
+public class SimpleLinkChecker implements LinkChecker {
+    @Override
+    public LinkCheckerReport check(String url) {
+        return null;
+    }
+}
