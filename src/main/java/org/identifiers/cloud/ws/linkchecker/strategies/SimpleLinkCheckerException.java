5b4f8f0cd9046b7d91455706bc9d20850ac5568e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 08:44:31 +0100

    simple link checker exception

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/strategies/SimpleLinkCheckerException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.ws.linkchecker.strategies;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.strategies
+ * Timestamp: 2018-05-28 8:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class SimpleLinkCheckerException extends LinkCheckerException {
+    public SimpleLinkCheckerException(String message) {
+        super(message);
+    }
+}
