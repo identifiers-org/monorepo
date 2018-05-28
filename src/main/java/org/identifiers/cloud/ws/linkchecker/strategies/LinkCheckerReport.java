7b1fc03b5dcc357bcc53040a292dafdaa5da1659
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 08:30:26 +0100

    link checker report

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/strategies/LinkCheckerReport.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.linkchecker.strategies;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.strategies
+ * Timestamp: 2018-05-28 8:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Report built by a link checking strategy.
+ */
+public class LinkCheckerReport {
+}
