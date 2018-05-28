c6031394033be0b3cf679cc0af7a0200fb3657fe
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 09:47:03 +0100

    refactoring the unit test

--- src/test/java/org/identifiers/cloud/ws/linkchecker/strategies/LinkCheckerTest.java
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/strategies/LinkCheckerTest.java
@@ -1,5 +1,14 @@
 package org.identifiers.cloud.ws.linkchecker.strategies;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.strategies
@@ -8,5 +17,21 @@ package org.identifiers.cloud.ws.linkchecker.strategies;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LinkCheckerTest {
+    private static final Logger logger = LoggerFactory.getLogger(LinkCheckerTest);
+
+
+    @Test
+    public void reportOverviewTest() {
+        LinkCheckerReport report = simpleLinkChecker.check("http://www.ebi.ac.uk/chebi/");
+        // Just checking how the report looks like
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            logger.debug(mapper.writer().writeValueAsString(report));
+        } catch (JsonProcessingException e) {
+            logger.error("WTF! Error serializing a simple POJO!");
+        }
+    }
 }
