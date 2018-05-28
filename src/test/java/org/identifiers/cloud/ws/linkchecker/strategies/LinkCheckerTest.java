5cff48ef2ab026d1d3d47c13c635b531b61c5f9c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-28 09:46:06 +0100

    LinkCheckerTest

---
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/strategies/LinkCheckerTest.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.strategies;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.strategies
+ * Timestamp: 2018-05-28 9:45
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkCheckerTest {
+}
