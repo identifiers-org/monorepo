4d5adf30bbddc42abb9ee02ba59f258e39e70cc5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-29 11:28:05 +0100

    helper for translation between link checking related models

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/LinkCheckModelsHelper.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data
+ * Timestamp: 2018-05-29 11:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkCheckModelsHelper {
+}
