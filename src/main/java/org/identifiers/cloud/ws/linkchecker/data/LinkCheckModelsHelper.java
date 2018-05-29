ec87f532eff1b458ef853c2100c7fd4ce27634ae
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-29 13:00:19 +0100

    getResultFromReport() helper, placeholder

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/LinkCheckModelsHelper.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/LinkCheckModelsHelper.java
@@ -1,5 +1,8 @@
 package org.identifiers.cloud.ws.linkchecker.data;
 
+import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckResult;
+import org.identifiers.cloud.ws.linkchecker.strategies.LinkCheckerReport;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.data
@@ -11,4 +14,7 @@ package org.identifiers.cloud.ws.linkchecker.data;
  * This class implements diverse helpers, specially for model transformation, to keep model decoupling.
  */
 public class LinkCheckModelsHelper {
+    public static LinkCheckResult getResultFromReport(LinkCheckerReport report) {
+        // TODO
+    }
 }
