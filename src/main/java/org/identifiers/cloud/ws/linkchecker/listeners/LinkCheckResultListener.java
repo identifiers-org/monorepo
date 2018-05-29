e7923f9d801c802a38553871a223e4d812941714
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-29 16:05:47 +0100

    link check result listener

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/listeners/LinkCheckResultListener.java
@@ -0,0 +1,17 @@
+package org.identifiers.cloud.ws.linkchecker.listeners;
+
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.listeners
+ * Timestamp: 2018-05-29 16:04
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class LinkCheckResultListener implements MessageListener {
+    // TODO
+}
