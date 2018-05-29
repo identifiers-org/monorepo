3a198762b50e2a425657275d1e24b0bd723c77ca
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-29 13:06:49 +0100

    getResultFromReport() helper implementationM

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/LinkCheckModelsHelper.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/LinkCheckModelsHelper.java
@@ -1,5 +1,6 @@
 package org.identifiers.cloud.ws.linkchecker.data;
 
+import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckRequest;
 import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckResult;
 import org.identifiers.cloud.ws.linkchecker.strategies.LinkCheckerReport;
 
@@ -14,7 +15,14 @@ import org.identifiers.cloud.ws.linkchecker.strategies.LinkCheckerReport;
  * This class implements diverse helpers, specially for model transformation, to keep model decoupling.
  */
 public class LinkCheckModelsHelper {
-    public static LinkCheckResult getResultFromReport(LinkCheckerReport report) {
-        // TODO
+    public static LinkCheckResult getResultFromReport(LinkCheckerReport report, LinkCheckRequest request) {
+        return new LinkCheckResult()
+                .setTimestamp(report.getTimestamp())
+                .setHttpStatus(report.getHttpStatus())
+                .setUrl(report.getUrl())
+                .setUrlAssessmentOk(report.isUrlAssessmentOk())
+                .setProviderId(request.getProviderId())
+                .setResourceId(request.getResourceId())
+                .setRequestTimestamp(request.getTimestamp());
     }
 }
