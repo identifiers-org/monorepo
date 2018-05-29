6bb052dd61d3c7d4abf24c36b31c80707dc2dbbf
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-29 10:47:22 +0100

    get a logger

--- src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/LinkChecker.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/LinkChecker.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.ws.linkchecker.daemons;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.daemons
@@ -10,5 +14,8 @@ package org.identifiers.cloud.ws.linkchecker.daemons;
  *
  * This daemon pulls link checking requests from a queue, runs them through a link checker, and lodges in the results.
  */
+@Component
 public class LinkChecker extends Thread {
+    private static final Logger logger = LoggerFactory.getLogger(LinkChecker.class);
+
 }
