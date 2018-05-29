50313360c9ca7def34b759e18d48d9d9c478275d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-29 11:24:12 +0100

    WAIT_TIME_LIMIT_SECONDS down to 600 seconds

--- src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/LinkChecker.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/LinkChecker.java
@@ -23,7 +23,7 @@ import java.util.Random;
  */
 @Component
 public class LinkChecker extends Thread {
-    private static final int WAIT_TIME_LIMIT_SECONDS = 1800;
+    private static final int WAIT_TIME_LIMIT_SECONDS = 600;
     private static final Logger logger = LoggerFactory.getLogger(LinkChecker.class);
 
     private boolean shutdown = false;
