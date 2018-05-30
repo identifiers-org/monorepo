6c30cd446d32144111d22fe5fef6eb00a0c4067f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-30 15:41:50 +0100

    not working yet

--- src/test/java/org/identifiers/cloud/ws/linkchecker/workout/LearningTest.java
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/workout/LearningTest.java
@@ -1,6 +1,5 @@
 package org.identifiers.cloud.ws.linkchecker.workout;
 
-import org.identifiers.cloud.ws.linkchecker.LinkCheckerApplication;
 import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.sql.Timestamp;
@@ -26,7 +24,7 @@ import java.util.stream.IntStream;
  * This unit test is for working out how things work with Spring Data Redis and backend operations.
  */
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = LinkCheckerApplication.class)
+//@ContextConfiguration(classes = {ApplicationConfig.class, LinkCheckerApplication.class})
 @SpringBootTest
 public class LearningTest {
     private static final Logger logger = LoggerFactory.getLogger(LearningTest.class);
