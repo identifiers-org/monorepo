56c0ed5f338fed71d1fe63dee4d4cbd3f9d3cdd0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-30 12:06:31 +0100

    wire a logger

--- src/test/java/org/identifiers/cloud/ws/linkchecker/workout/LearningTest.java
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/workout/LearningTest.java
@@ -1,5 +1,15 @@
 package org.identifiers.cloud.ws.linkchecker.workout;
 
+import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Timestamp;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.workout
@@ -7,8 +17,13 @@ package org.identifiers.cloud.ws.linkchecker.workout;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
- *
+ * <p>
  * This unit test is for working out how things work with Spring Data Redis and backend operations.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LearningTest {
+    private static final Logger logger = LoggerFactory.getLogger(LearningTest.class);
+
+
 }
