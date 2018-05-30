558386634cca216e3d509aa4bcda5b21e325c6a9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-30 15:45:16 +0100

    obviously, it was a desperate try, as I know unit tests need a default constructor

--- src/test/java/org/identifiers/cloud/ws/linkchecker/workout/LearningTest.java
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/workout/LearningTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -30,6 +31,10 @@ public class LearningTest {
 
     private Deque<LinkCheckRequest> linkCheckRequestQueue;
 
+    public LearningTest() {
+    }
+
+    @Autowired
     public LearningTest(Deque<LinkCheckRequest> linkCheckRequestQueue) {
         this.linkCheckRequestQueue = linkCheckRequestQueue;
     }
