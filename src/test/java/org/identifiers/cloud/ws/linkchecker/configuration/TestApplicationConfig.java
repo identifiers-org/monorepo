53206dc307e555de8ce3e4ab35176af74c73764e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-30 15:05:12 +0100

    trying the test configuration approach

--- src/test/java/org/identifiers/cloud/ws/linkchecker/configuration/TestApplicationConfig.java
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/configuration/TestApplicationConfig.java
@@ -3,8 +3,8 @@ package org.identifiers.cloud.ws.linkchecker.configuration;
 import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckRequest;
 import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckResult;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -27,7 +27,7 @@ import java.util.Deque;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-@TestConfiguration
+@Configuration
 @PropertySource("classpath:application.properties")
 @Profile("test")
 @EnableRedisRepositories
