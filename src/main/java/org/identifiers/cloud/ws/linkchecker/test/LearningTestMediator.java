8198c34abc1f3dab26e43ef08d7eca0e7b89a694
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 10:25:03 +0100

    post construct tasks+

--- src/main/java/org/identifiers/cloud/ws/linkchecker/test/LearningTestMediator.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/test/LearningTestMediator.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.test
@@ -19,4 +21,9 @@ import org.springframework.stereotype.Component;
 @Profile("test")
 public class LearningTestMediator {
     // TODO
+
+    @PostConstruct
+    public void postConstruct() {
+        // TODO
+    }
 }
