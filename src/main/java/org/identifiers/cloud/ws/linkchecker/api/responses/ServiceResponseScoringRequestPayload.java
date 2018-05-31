5ba4ff41d28d7126180de97276f24ee7b476bb5c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 12:52:32 +0100

    scoring payload, getters and setters

--- src/main/java/org/identifiers/cloud/ws/linkchecker/api/responses/ServiceResponseScoringRequestPayload.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/responses/ServiceResponseScoringRequestPayload.java
@@ -15,4 +15,13 @@ import java.io.Serializable;
 public class ServiceResponseScoringRequestPayload implements Serializable {
     // Default scoring
     public int score = 50;
+
+    public int getScore() {
+        return score;
+    }
+
+    public ServiceResponseScoringRequestPayload setScore(int score) {
+        this.score = score;
+        return this;
+    }
 }
