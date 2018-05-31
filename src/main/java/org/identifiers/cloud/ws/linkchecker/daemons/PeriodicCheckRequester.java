bfb330faeeea77bbf6d2bcb0a4eb1ad2d6a862d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 15:43:56 +0100

    periodic check requester, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/PeriodicCheckRequester.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.daemons;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.daemons
+ * Timestamp: 2018-05-31 15:43
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PeriodicCheckRequester {
+}
