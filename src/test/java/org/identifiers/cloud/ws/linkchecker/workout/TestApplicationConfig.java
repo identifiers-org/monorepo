93af34edd0dfd7dcbf8bc1c479ccd7001665219e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 10:26:50 +0100

    disable useless test configuration

--- src/test/java/org/identifiers/cloud/ws/linkchecker/workout/TestApplicationConfig.java
+++ src/test/java/org/identifiers/cloud/ws/linkchecker/workout/TestApplicationConfig.java
@@ -4,15 +4,12 @@ import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckRequest;
 import org.identifiers.cloud.ws.linkchecker.data.models.LinkCheckResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.support.collections.DefaultRedisList;
 import org.springframework.data.redis.support.collections.RedisList;
 
@@ -26,9 +23,9 @@ import java.util.Deque;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-@Configuration
-@PropertySource("classpath:application.properties")
-@EnableRedisRepositories
+//@Configuration
+//@PropertySource("classpath:application.properties")
+//@EnableRedisRepositories
 public class TestApplicationConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
