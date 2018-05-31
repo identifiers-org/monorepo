4f9ecb276c1ce1bbe352e5683a42ffa857f99448
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 10:23:25 +0100

    learning test mediator, placholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/test/LearningTestMediator.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.test;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.test
+ * Timestamp: 2018-05-31 10:23
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LearningTestMediator {
+}
