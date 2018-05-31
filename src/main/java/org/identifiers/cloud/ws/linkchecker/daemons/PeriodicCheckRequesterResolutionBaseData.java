82ee93f893a21338489a177053f1dfbd40793a7f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 15:51:10 +0100

    component

--- src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/PeriodicCheckRequesterResolutionBaseData.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/daemons/PeriodicCheckRequesterResolutionBaseData.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.ws.linkchecker.daemons;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.daemons
@@ -11,6 +13,7 @@ package org.identifiers.cloud.ws.linkchecker.daemons;
  * This is a check requester daemon that will use resolution insight data for periodically request link checking of
  * resources and providers.
  */
+@Component
 public class PeriodicCheckRequesterResolutionBaseData extends Thread {
     private static final int WAIT_TIME_MAX_BEFORE_NEXT_REQUEST_SECONDS = 86400; // 24 hours
     private static final int WAIT_TIME_MIN_BEFORE_NEXT_REQUEST_SECONDS = 21600; // 6 hours
