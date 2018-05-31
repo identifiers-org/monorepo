980f002235724fc01b18fc28944089da492b550e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-05-31 15:30:49 +0100

    handle possible error

--- src/main/java/org/identifiers/cloud/ws/linkchecker/api/models/LinkScoringApiModel.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/models/LinkScoringApiModel.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 /**
@@ -38,7 +39,13 @@ public class LinkScoringApiModel {
     public ServiceResponseScoringRequest getScoreForProvider(ScoringRequestWithIdPayload request) {
         logger.info("Provider scoring request for ID '{}', URL '{}'", request.getId(), request.getUrl());
         ServiceResponseScoringRequest response = getDefaultResponse();
-        response.getPayload().setScore((int) Math.round(historyTrackingService.getTrackerForProvider(request).getHistoryStats(HistoryTracker.HistoryStats.SIMPLE).getUpPercenetage()));
+        try {
+            response.getPayload().setScore((int) Math.round(historyTrackingService.getTrackerForProvider(request)
+                    .getHistoryStats(HistoryTracker.HistoryStats.SIMPLE).getUpPercenetage()));
+        } catch (Exception e) {
+            response.setErrorMessage(String.format("Scoring could not be calculated due to '%s'", e.getMessage()));
+            response.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return response;
     }
 }
