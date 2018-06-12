cbbb3401f6e50deb11ec7085c06d8406bd1ecc10
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-12 13:11:41 +0100

    TrackedResource, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/TrackedResource.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-06-12 13:11
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class TrackedResource {
+}
