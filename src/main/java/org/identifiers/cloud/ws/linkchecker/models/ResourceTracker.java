4678148852991b764151f74094113ba745ea904d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-12 13:28:12 +0100

    ResourceTracker, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/models/ResourceTracker.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.ws.linkchecker.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.models
+ * Timestamp: 2018-06-12 13:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceTracker extends HistoryTracker {
+    // TODO
+}
