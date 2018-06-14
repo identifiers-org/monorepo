86ba6506d1ef0a48694fb914cd6761088bdfcdbe
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-14 11:59:16 +0100

    health api controller implementation

--- src/main/java/org/identifiers/cloud/ws/linkchecker/api/controllers/HealthApiController.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/controllers/HealthApiController.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.ws.linkchecker.api.controllers;
 
+import org.identifiers.cloud.ws.linkchecker.api.models.HealthApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.api.controllers
@@ -7,6 +12,23 @@ package org.identifiers.cloud.ws.linkchecker.api.controllers;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This controller offers the endpoints that provide health checks for the service, e.g. liveness and health check
  */
+@RestController
 public class HealthApiController {
+    @Autowired
+    private HealthApiModel model;
+
+    // liveness probe
+    @RequestMapping(value = "/liveness_check")
+    public String livenessCheck() {
+        return model.livenessCheck();
+    }
+
+    // Readiness check
+    @RequestMapping(value = "/readiness_check")
+    public String readinessCheck() {
+        return model.readinessCheck();
+    }
 }
