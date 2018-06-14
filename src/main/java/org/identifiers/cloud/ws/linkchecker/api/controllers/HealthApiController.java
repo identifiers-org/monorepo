740b962c28798796c5da4f48ad9c00a7d25062a3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-14 11:56:50 +0100

    building the HealthApiController

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/controllers/HealthApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.api.controllers;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.controllers
+ * Timestamp: 2018-06-14 11:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HealthApiController {
+}
