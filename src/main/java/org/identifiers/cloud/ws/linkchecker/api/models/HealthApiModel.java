16efb594a9ad6367bfd1818001c48b586cce16ba
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-14 11:57:32 +0100

    HealthApiModel placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/models/HealthApiModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.api.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.models
+ * Timestamp: 2018-06-14 11:57
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HealthApiModel {
+}
