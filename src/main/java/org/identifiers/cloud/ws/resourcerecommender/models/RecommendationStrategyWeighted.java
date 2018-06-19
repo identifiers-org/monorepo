e306feef63472b7384ff7a78b59f0778b8100c0a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-19 15:22:25 +0100

    put the different recommendation strategies in separated profiles

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategyWeighted.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategyWeighted.java
@@ -1,5 +1,6 @@
 package org.identifiers.cloud.ws.resourcerecommender.models;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  * ---
  */
 @Component
+@Profile("recommendationStrategyWeighted")
 public class RecommendationStrategyWeighted implements RecommendationStrategy {
     @Override
     public List<ResourceRecommendation> getRecommendations(List<ResolvedResource> resolvedResources) {
