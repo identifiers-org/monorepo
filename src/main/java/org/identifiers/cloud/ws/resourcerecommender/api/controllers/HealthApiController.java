56509419d8e5b0fb270e4b54359884a7401c05fb
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-19 16:31:49 +0100

    controller for the health API

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/api/controllers/HealthApiController.java
@@ -0,0 +1,34 @@
+package org.identifiers.cloud.ws.resourcerecommender.api.controllers;
+
+import org.identifiers.cloud.ws.resourcerecommender.api.models.HealthApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.api.controllers
+ * Timestamp: 2018-06-19 16:31
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This controller offers the endpoints that provide health checks for the service, e.g. liveness and health check
+ */
+@RestController
+public class HealthApiController {
+    @Autowired
+    private HealthApiModel model;
+
+    // liveness probe
+    @RequestMapping(value = "/liveness_check")
+    public String livenessCheck() {
+        return model.livenessCheck();
+    }
+
+    // Readiness check
+    @RequestMapping(value = "/readiness_check")
+    public String readinessCheck() {
+        return model.readinessCheck();
+    }
+}
