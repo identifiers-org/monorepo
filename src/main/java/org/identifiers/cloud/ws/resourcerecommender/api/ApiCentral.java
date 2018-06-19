3b493f526ec39285fa794938e259e937597ce526
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-19 16:23:06 +0100

    API Central

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/api/ApiCentral.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.resourcerecommender.api;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.api
+ * Timestamp: 2018-06-19 16:22
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiCentral {
+    // Current API Version flag
+    public static final String apiVersion = "1.0";
+}
