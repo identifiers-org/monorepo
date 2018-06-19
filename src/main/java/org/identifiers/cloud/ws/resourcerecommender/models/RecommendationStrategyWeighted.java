48e541bd5ee7c2e008d9f09dbde429941e2ddc85
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-19 15:17:32 +0100

    new weighted recommendation strategy, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/RecommendationStrategyWeighted.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+import java.util.List;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-06-19 15:15
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class RecommendationStrategyWeighted implements RecommendationStrategy {
+    @Override
+    public List<ResourceRecommendation> getRecommendations(List<ResolvedResource> resolvedResources) {
+        return null;
+    }
+}
