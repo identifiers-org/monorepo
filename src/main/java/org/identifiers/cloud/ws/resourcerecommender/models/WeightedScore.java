692b8774cc6f7c39adae8830c5d7fa4d26a41712
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-20 11:34:41 +0100

    weighted score, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/WeightedScore.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-06-20 11:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This POJO represents a weighted score, it consists of the a weigth [0,100] and a score provider to which apply the
+ * weight.
+ */
+public class WeightedScore {
+    // TODO
+}
