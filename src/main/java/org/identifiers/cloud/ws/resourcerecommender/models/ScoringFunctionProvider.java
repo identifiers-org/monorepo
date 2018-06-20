5d42d0a2889beb1ab50dfb741efbd45e63cfbd1f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-20 11:31:44 +0100

    scoring function provider interface, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ScoringFunctionProvider.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-06-20 11:23
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This defines the interface of scoring function providers, different ways to implement how the scoring function is
+ * built.
+ */
+public interface ScoringFunctionProvider {
+    // TODO
+}
