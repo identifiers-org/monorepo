7ddefff8128ffcac8c979abd42876832372f499b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-20 11:10:54 +0100

    score provider exception

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ScoreProviderException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-06-20 11:10
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ScoreProviderException extends RuntimeException {
+    public ScoreProviderException(String message) {
+        super(message);
+    }
+}
