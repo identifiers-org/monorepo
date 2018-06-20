9e255e4bee9c809c5c3ca6f29d8e94a44330e68c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-20 09:11:34 +0100

    metric point interface placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/MetricPoint.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resourcerecommender.models;
+
+/**
+ * Project: resource-recommender
+ * Package: org.identifiers.cloud.ws.resourcerecommender.models
+ * Timestamp: 2018-06-20 9:11
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface MetricPoint {
+}
