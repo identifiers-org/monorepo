3c294e581dfb14655123b23ac39a1639531a1399
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-30 11:29:25 +0100

    component and wire in a logger

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckResultConfig.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckResultConfig.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.ws.linkchecker.data.models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 /**
  * Project: link-checker
  * Package: org.identifiers.cloud.ws.linkchecker.data.models
@@ -11,6 +15,8 @@ package org.identifiers.cloud.ws.linkchecker.data.models;
  * This class is a delegate between the Spring based configuration and link check results, as they're not under control
  * of Spring Boot
  */
+@Component
 public class LinkCheckResultConfig {
+    private static final Logger logger = LoggerFactory.getLogger(LinkCheckResultConfig.class);
     // TODO
 }
