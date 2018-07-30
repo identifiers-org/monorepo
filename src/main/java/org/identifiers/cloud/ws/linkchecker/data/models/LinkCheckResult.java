95ff5a072ccdc114898acca99c66c2a4fb9d6589
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-30 10:51:36 +0100

    refactoring TTL as a property

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckResult.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckResult.java
@@ -19,8 +19,9 @@ import java.util.Date;
  * This entity models a checked URL, for either a provider, a resource or a plain URL.
  */
 // TODO - Parameterize TTL via configuration, and check for multiple copies of the service running concurrently
-@RedisHash(value = "LinkCheckerLinkCheckResult", timeToLive = 10L)
+@RedisHash(value = "LinkCheckerLinkCheckResult")
 public class LinkCheckResult implements Serializable, Comparable<LinkCheckResult> {
+    // TTL as property
     // Result ID, hopefully manufactured by Redis
     @Id
     private String id;
