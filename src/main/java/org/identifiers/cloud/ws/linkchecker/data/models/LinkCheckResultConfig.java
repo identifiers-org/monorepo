7526dac4c5fe80e0e4df3f25f43c169ff5693aa6
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-30 11:28:20 +0100

    link check result configuration delegate, documentation correction

--- src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckResultConfig.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/LinkCheckResultConfig.java
@@ -8,7 +8,7 @@ package org.identifiers.cloud.ws.linkchecker.data.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  *
- * This class is a mediator between the Spring based configuration and link check results, as they're not under control
+ * This class is a delegate between the Spring based configuration and link check results, as they're not under control
  * of Spring Boot
  */
 public class LinkCheckResultConfig {
