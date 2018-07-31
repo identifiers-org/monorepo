0496993ea9f3e7a871294aaf244f303009d0586c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-31 11:14:48 +0100

    management api for link checker

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/controllers/ManagementApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.api.controllers;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.controllers
+ * Timestamp: 2018-07-31 11:13
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ManagementApiController {
+}
