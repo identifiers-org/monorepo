04c9f46cfb5026aa9fcfba6aaf57b7ad243fe7af
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-31 11:36:07 +0100

    payload for responses to management requests, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/responses/ServiceResponseManagementRequestPayload.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.api.responses;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.responses
+ * Timestamp: 2018-07-31 11:35
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseManagementRequestPayload {
+}
