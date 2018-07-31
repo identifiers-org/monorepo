040564cd15fa73e95c6efc490da39bc80f67fb85
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-31 11:41:43 +0100

    generic management request response

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/responses/ServiceResponseManagementRequest.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.api.responses;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.api.responses
+ * Timestamp: 2018-07-31 11:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseManagementRequest extends ServiceResponse<ServiceResponseManagementRequestPayload> {
+}
