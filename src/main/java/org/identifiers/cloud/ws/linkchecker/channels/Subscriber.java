ccf8d35b319c6d59ebb579ed1182edb6f5f61dce
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 13:46:38 +0100

    wire in the logger and prepare listener registration method

--- src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.ws.linkchecker.channels;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -15,8 +17,14 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
  * Subscriber for announcements on a Redis backend
  */
 public abstract class Subscriber<K, V> {
+    private static final Logger logger = LoggerFactory.getLogger(Subscriber.class);
+    
     protected abstract RedisMessageListenerContainer getRedisContainer();
     protected abstract ChannelTopic getChannelTopic();
     protected abstract RedisTemplate<K, V> getRedisTemplate();
+
+    protected void doRegisterListener() {
+
+    }
     // TODO
 }
