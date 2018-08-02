9aaadab50113773954ff37f6c6e8969264940a82
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 12:41:00 +0100

    channel publisher, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Publisher.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.channels;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.channels
+ * Timestamp: 2018-08-02 12:38
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class Publisher {
+}
