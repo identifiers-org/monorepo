cbdc61e07cd49f6629dada4408a9385a79bec7b7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 13:15:57 +0100

    subscriber abstract base class for a redis channel backend, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.ws.linkchecker.channels;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.channels
+ * Timestamp: 2018-08-02 13:15
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Subscriber for announcements on a Redis backend
+ */
+public class Subscriber {
+}
