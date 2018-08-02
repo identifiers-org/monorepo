7c1954149cf08d8d5e588da5e3218a91fb695e78
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 14:46:49 +0100

    specify the parameterised value

--- src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
@@ -24,7 +24,7 @@ import java.util.Set;
 public abstract class Subscriber<K, V> implements MessageListener {
     private static final Logger logger = LoggerFactory.getLogger(Subscriber.class);
 
-    private Set<Listener> listeners = new HashSet<>();
+    private Set<Listener<V>> listeners = new HashSet<>();
 
     protected abstract RedisMessageListenerContainer getRedisContainer();
     protected abstract ChannelTopic getChannelTopic();
@@ -35,7 +35,7 @@ public abstract class Subscriber<K, V> implements MessageListener {
         getRedisContainer().addMessageListener(this, getChannelTopic());
     }
 
-    public void addListener(Listener listener) {
+    public void addListener(Listener<V> listener) {
         listeners.add(listener);
     }
 
