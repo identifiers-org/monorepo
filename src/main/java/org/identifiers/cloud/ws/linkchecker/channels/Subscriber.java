8e42466562265e31da25b24529476cb878bff78c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 13:48:17 +0100

    listener registration code

--- src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/Subscriber.java
@@ -2,6 +2,7 @@ package org.identifiers.cloud.ws.linkchecker.channels;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -16,15 +17,16 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
  *
  * Subscriber for announcements on a Redis backend
  */
-public abstract class Subscriber<K, V> {
+public abstract class Subscriber<K, V> implements MessageListener {
     private static final Logger logger = LoggerFactory.getLogger(Subscriber.class);
-    
+
     protected abstract RedisMessageListenerContainer getRedisContainer();
     protected abstract ChannelTopic getChannelTopic();
     protected abstract RedisTemplate<K, V> getRedisTemplate();
 
     protected void doRegisterListener() {
-
+        logger.info("[REGISTER] for topic '{}'", getChannelTopic().getTopic());
+        getRedisContainer().addMessageListener(this, getChannelTopic());
     }
     // TODO
 }
