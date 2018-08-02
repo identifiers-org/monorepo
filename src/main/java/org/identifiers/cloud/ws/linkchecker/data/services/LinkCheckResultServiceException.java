f407f700b8b6851c3ce0607580e783d1d29749e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 10:29:59 +0100

    LinkCheckResultServiceException, placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/services/LinkCheckResultServiceException.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data.services;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.services
+ * Timestamp: 2018-08-02 10:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkCheckResultServiceException extends RuntimeException {
+}
