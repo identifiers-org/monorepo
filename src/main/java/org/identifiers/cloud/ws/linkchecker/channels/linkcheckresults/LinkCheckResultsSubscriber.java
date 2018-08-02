755c012b78be42d0f3f932ff0d50c69bd1cbb887
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-02 14:12:04 +0100

    placeholder for a link check results subscriber

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/linkcheckresults/LinkCheckResultsSubscriber.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.channels.linkcheckresults;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.channels.linkcheckresults
+ * Timestamp: 2018-08-02 14:10
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkCheckResultsSubscriber {
+}
