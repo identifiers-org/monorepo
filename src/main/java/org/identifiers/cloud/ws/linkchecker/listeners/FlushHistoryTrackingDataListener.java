d674717c70db73820296cc851d4bd513d5c43eec
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-03 11:40:40 +0100

    FlushHistoryTrackingDataListener placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/listeners/FlushHistoryTrackingDataListener.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.ws.linkchecker.listeners;
+
+import org.identifiers.cloud.ws.linkchecker.channels.Listener;
+import org.identifiers.cloud.ws.linkchecker.data.models.FlushHistoryTrackingDataMessage;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.listeners
+ * Timestamp: 2018-08-03 11:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class FlushHistoryTrackingDataListener extends Listener<FlushHistoryTrackingDataMessage> {
+    // TODO
+}
