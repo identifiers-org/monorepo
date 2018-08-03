fed05ab8079d8d5d700da29f25e4a968881d07df
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-03 11:19:39 +0100

    FlushHistoryTrackingDataSubscriber placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/channels/management/flushhistorytrackingdata/FlushHistoryTrackingDataSubscriber.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.channels.management.flushhistorytrackingdata;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.channels.management.flushhistorytrackingdata
+ * Timestamp: 2018-08-03 11:19
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class FlushHistoryTrackingDataSubscriber {
+}
