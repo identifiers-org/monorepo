3c938d33c0b26115c456486da4195b0250c9f7f4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-03 11:54:24 +0100

    delegate flush request to be completed by the history tracking service

--- src/main/java/org/identifiers/cloud/ws/linkchecker/api/models/ManagementApiModel.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/api/models/ManagementApiModel.java
@@ -2,9 +2,13 @@ package org.identifiers.cloud.ws.linkchecker.api.models;
 
 import org.identifiers.cloud.ws.linkchecker.api.responses.ServiceResponseManagementRequest;
 import org.identifiers.cloud.ws.linkchecker.api.responses.ServiceResponseManagementRequestPayload;
+import org.identifiers.cloud.ws.linkchecker.services.HistoryTrackingService;
+import org.identifiers.cloud.ws.linkchecker.services.HistoryTrackingServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,13 +24,23 @@ import org.springframework.stereotype.Component;
 public class ManagementApiModel {
     private static final Logger logger = LoggerFactory.getLogger(ManagementApiModel.class);
 
+    @Autowired
+    private HistoryTrackingService historyTrackingService;
+
     public ServiceResponseManagementRequest flushLinkCheckingHistory() {
         logger.warn("FLUSH REQUEST for link checking hisotrical data");
-        // TODO It is responsibility of the history tracking service used to flush the link checking history
-        // TODO change this when the code is completed
+        // It is responsibility of the history tracking service used to flush the link checking history
+        // change this when the code is completed
         ServiceResponseManagementRequest response = new ServiceResponseManagementRequest();
         response.setPayload(new ServiceResponseManagementRequestPayload()
                 .setMessage("This is a default response"));
+        try {
+            historyTrackingService.deleteHistoryTrackingData();
+        } catch (HistoryTrackingServiceException e) {
+            logger.error("The following error occurred while tryign to flush link checking historical data, '{}'", e.getMessage());
+            response.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            response.getPayload().setMessage("An error occurred while trying to flush link checking historical data, but DON'T PANIC, we'll get back on track");
+        }
         return response;
     }
 }
