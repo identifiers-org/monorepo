f1fc72c9d543306a31bc417c443239ae10198d17
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-03 10:02:36 +0100

    flush history tracking data message, model placeholder

---
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/data/models/FlushHistoryTrackingDataMessage.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.linkchecker.data.models;
+
+/**
+ * Project: link-checker
+ * Package: org.identifiers.cloud.ws.linkchecker.data.models
+ * Timestamp: 2018-08-03 10:02
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class FlushHistoryTrackingDataMessage {
+}
