f1eb91beaae9ce1feddb30669e5f9852ba88a905
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-10 11:34:03 +0100

    10 seconds connection timeout

--- src/main/java/org/identifiers/cloud/ws/linkchecker/strategies/SimpleLinkChecker.java
+++ src/main/java/org/identifiers/cloud/ws/linkchecker/strategies/SimpleLinkChecker.java
@@ -23,7 +23,7 @@ import java.sql.Timestamp;
 @Component
 @Scope("prototype")
 public class SimpleLinkChecker implements LinkChecker {
-    private static final int CONNECTION_TIMEOUT_SECONDS = 30;
+    private static final int CONNECTION_TIMEOUT_SECONDS = 10;
 
     @Override
     public LinkCheckerReport check(String url) {
