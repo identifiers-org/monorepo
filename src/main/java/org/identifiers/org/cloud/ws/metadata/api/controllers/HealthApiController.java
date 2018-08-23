ff65e7cbbc43e12647c70c755ff835d683714b22
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-23 11:15:34 +0100

    health api controller, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/api/controllers/HealthApiController.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.api.controllers
+ * Timestamp: 2018-08-23 11:15
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@RestController
+public class HealthApiController {
+}
