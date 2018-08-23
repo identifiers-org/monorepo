08c523dc55e272468fabe141129bf5afda45c1be
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-23 11:13:07 +0100

    health api model, implementation

--- src/main/java/org/identifiers/org/cloud/ws/metadata/api/models/HealthApiModel.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/api/models/HealthApiModel.java
@@ -2,6 +2,8 @@ package org.identifiers.org.cloud.ws.metadata.api.models;
 
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 /**
  * Project: metadata
  * Package: org.identifiers.org.cloud.ws.metadata.api.models
@@ -12,5 +14,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class HealthApiModel {
-    // TODO
+    private static String runningSessionId = UUID.randomUUID().toString();
+
+    public String livenessCheck() {
+        return runningSessionId;
+    }
+
+    // TODO - Refactor this to actually reflect whether the service ir ready or not
+    public String readinessCheck() {
+        return runningSessionId;
+    }
 }
