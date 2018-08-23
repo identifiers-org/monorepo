42d3117fe2f9ee69c7da99d8b74be93635b6842c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-23 11:12:52 +0100

    health api model, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/api/models/HealthApiModel.java
@@ -0,0 +1,16 @@
+package org.identifiers.org.cloud.ws.metadata.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.api.models
+ * Timestamp: 2018-08-23 11:12
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class HealthApiModel {
+    // TODO
+}
