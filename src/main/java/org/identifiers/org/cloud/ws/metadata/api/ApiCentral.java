14d3e594e9df33707285f424ce7fec9efec6360a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-23 11:08:59 +0100

    ApiCentral added

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/api/ApiCentral.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.metadata.api;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.api
+ * Timestamp: 2018-08-23 11:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiCentral {
+    // Current API Version flag
+    public static final String apiVersion = "1.0";
+}
