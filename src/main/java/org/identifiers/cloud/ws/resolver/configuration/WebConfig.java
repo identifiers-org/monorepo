c4fd58e43ce54e1f7dcd6111865e3b1bfe1f0d5b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-06 10:55:57 +0100

    trying to change web config

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.ws.resolver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.configuration
+ * Timestamp: 2018-09-06 10:54
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+}
