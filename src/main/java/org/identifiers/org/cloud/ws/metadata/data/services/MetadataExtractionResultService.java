09722e3c3132897f87f3bcf72deb286d67eb443f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-16 20:33:23 +0100

    metadata extraction result service placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/services/MetadataExtractionResultService.java
@@ -0,0 +1,13 @@
+package org.identifiers.org.cloud.ws.metadata.data.services;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.services
+ * Timestamp: 2018-09-16 20:33
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionResultService {
+    // TODO
+}
