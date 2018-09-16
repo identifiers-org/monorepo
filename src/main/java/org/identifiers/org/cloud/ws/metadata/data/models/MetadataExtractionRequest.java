079e6ee85d2abf99bee5ef9f010d75b8ef740d71
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-16 14:26:08 +0100

    metadata extraction request model, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionRequest.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.data.models;
+
+import java.io.Serializable;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.models
+ * Timestamp: 2018-09-16 14:25
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionRequest implements Serializable, Comparable<MetadataExtractionRequest> {
+    // TODO
+}
