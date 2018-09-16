4ad46617e01f4a41a30c805371ce4fa204105ccb
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-16 14:38:19 +0100

    metadata extraction request model, model

--- src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionRequest.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionRequest.java
@@ -1,6 +1,8 @@
 package org.identifiers.org.cloud.ws.metadata.data.models;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * Project: metadata
@@ -11,5 +13,50 @@ import java.io.Serializable;
  * ---
  */
 public class MetadataExtractionRequest implements Serializable, Comparable<MetadataExtractionRequest> {
-    // TODO
+    private String resolutionPath;
+    // When it has been checked (UTC)
+    private Timestamp timestamp = new Timestamp(new Date().getTime());
+    private String resourceId;
+    private String accessUrl;
+
+    public String getResolutionPath() {
+        return resolutionPath;
+    }
+
+    public MetadataExtractionRequest setResolutionPath(String resolutionPath) {
+        this.resolutionPath = resolutionPath;
+        return this;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public MetadataExtractionRequest setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+        return this;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public MetadataExtractionRequest setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+        return this;
+    }
+
+    public String getAccessUrl() {
+        return accessUrl;
+    }
+
+    public MetadataExtractionRequest setAccessUrl(String accessUrl) {
+        this.accessUrl = accessUrl;
+        return this;
+    }
+
+    @Override
+    public int compareTo(MetadataExtractionRequest o) {
+        return this.timestamp.compareTo(o.getTimestamp());
+    }
 }
