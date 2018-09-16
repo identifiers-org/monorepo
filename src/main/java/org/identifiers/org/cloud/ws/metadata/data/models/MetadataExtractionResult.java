f897475c471ac1e516de0ba41d0bab198c087135
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-16 15:20:26 +0100

    metadata extraction result model, core attributes

--- src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionResult.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionResult.java
@@ -1,6 +1,12 @@
 package org.identifiers.org.cloud.ws.metadata.data.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * Project: metadata
@@ -10,6 +16,16 @@ import java.io.Serializable;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@RedisHash(value = "MetadataMetadataExtractionResult")
 public class MetadataExtractionResult implements Serializable, Comparable<MetadataExtractionResult> {
     // TODO
+    @Id private String id;
+    @Indexed private String resourceId;
+    @Indexed private String accessUrl;
+    @Indexed private String timestamp = (new Timestamp(new Date().getTime())).toString();
+    // When this check was requested (UTC)
+    private String requestTimestamp;
+    @Indexed private int httpStatus;
+    private String metadataContent;
+    private String errorMessage;
 }
