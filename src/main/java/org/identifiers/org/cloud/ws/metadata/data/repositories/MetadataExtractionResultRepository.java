ab3dbf67153938937216c345cdace8eee8715850
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-16 19:41:57 +0100

    MetadataExtractionResultRepository

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/repositories/MetadataExtractionResultRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.org.cloud.ws.metadata.data.repositories;
+
+import org.identifiers.org.cloud.ws.metadata.data.models.MetadataExtractionResult;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.repositories
+ * Timestamp: 2018-09-16 19:39
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface MetadataExtractionResultRepository extends CrudRepository<MetadataExtractionResult, String> {
+    // TODO
+}
