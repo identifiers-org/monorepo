c5fa2c87f2aa146cbd180f2c8a50885ece99349f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:05:54 +0100

    metadata extraction strategy, interface placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataExtractionStrategy.java
@@ -0,0 +1,13 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-09-17 10:05
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface MetadataExtractionStrategy {
+    // TODO
+}
