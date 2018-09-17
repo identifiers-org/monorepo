584e66ce2def928093417ce3b457159d153b913e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:28:43 +0100

    metadata collection daemon, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/daemons/MetadataCollector.java
@@ -0,0 +1,16 @@
+package org.identifiers.org.cloud.ws.metadata.daemons;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.daemons
+ * Timestamp: 2018-09-17 10:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class MetadataCollector extends Thread {
+    // TODO
+}
