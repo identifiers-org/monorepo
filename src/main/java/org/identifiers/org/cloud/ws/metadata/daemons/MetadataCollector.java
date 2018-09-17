041cc7307a501bbffc105abc380fc7b664708bac
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:29:48 +0100

    metadata collection daemon, shutdown flag and random for waits

--- src/main/java/org/identifiers/org/cloud/ws/metadata/daemons/MetadataCollector.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/daemons/MetadataCollector.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 /**
  * Project: metadata
  * Package: org.identifiers.org.cloud.ws.metadata.daemons
@@ -17,5 +19,10 @@ public class MetadataCollector extends Thread {
     private static final int WAIT_TIME_LIMIT_SECONDS = 30;
     private static final int WAIT_TIME_POLL_METADATA_EXTRACTION_REQUEST_QUEUE_SECONDS = 3;
     private static final Logger logger = LoggerFactory.getLogger(MetadataCollector.class);
+
+    // Shutdown flag
+    private boolean shutdown = false;
+    // For random waits
+    private Random random = new Random(System.currentTimeMillis());
 
 }
