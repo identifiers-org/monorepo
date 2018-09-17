b7a5297300254d4956c358819317cda1aed7ae05
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:08:25 +0100

    MetadataExtractionStrategyException

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataExtractionStrategyException.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.models;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.models
+ * Timestamp: 2018-09-17 10:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionStrategyException extends RuntimeException {
+    public MetadataExtractionStrategyException(String message) {
+        super(message);
+    }
+}
