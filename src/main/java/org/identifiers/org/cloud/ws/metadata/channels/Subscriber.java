e8d684766abce9e1c91e1c0e9e790f26f8809b15
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:56:31 +0100

    channel subscriber abstract base class

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/channels/Subscriber.java
@@ -0,0 +1,45 @@
+package org.identifiers.org.cloud.ws.metadata.channels;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.channels
+ * Timestamp: 2018-09-17 10:54
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public abstract class Subscriber<K, V> implements MessageListener {
+    private static final Logger logger = LoggerFactory.getLogger(Subscriber.class);
+
+    private Set<Listener<V>> listeners = new HashSet<>();
+
+    protected abstract RedisMessageListenerContainer getRedisContainer();
+    protected abstract ChannelTopic getChannelTopic();
+    protected abstract RedisTemplate<K, V> getRedisTemplate();
+
+    protected void doRegisterSubscriber() {
+        logger.info("[REGISTER] for topic '{}'", getChannelTopic().getTopic());
+        getRedisContainer().addMessageListener(this, getChannelTopic());
+    }
+
+    public void addListener(Listener<V> listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] bytes) {
+        V value = (V) getRedisTemplate().getValueSerializer().deserialize(message.getBody());
+        listeners.parallelStream().forEach(listener -> listener.process(value));
+    }
+}
