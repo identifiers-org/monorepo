32b0fbad4a8bfb688ec3bfec2dd24ecb3b2ef47e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 23:40:02 +0100

    MetadataExtractionResultBuilderDefault

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionResultBuilderDefault.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.data.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.models
+ * Timestamp: 2018-09-17 23:39
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class MetadataExtractionResultBuilderDefault implements MetadataExtractionResultBuilder {
+}
