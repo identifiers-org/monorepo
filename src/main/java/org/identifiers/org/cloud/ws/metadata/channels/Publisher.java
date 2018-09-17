8ac88a9b00afa8f876e344f3b25359b28602c7a2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:50:06 +0100

    publisher interface

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/channels/Publisher.java
@@ -0,0 +1,13 @@
+package org.identifiers.org.cloud.ws.metadata.channels;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.channels
+ * Timestamp: 2018-09-17 10:49
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface Listener<V> {
+    void process(V value);
+}
