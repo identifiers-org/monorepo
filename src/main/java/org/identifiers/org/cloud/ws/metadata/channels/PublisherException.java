9cf088fa210273779735bd5a63d4531d4f906380
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:51:45 +0100

    publisher exception

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/channels/PublisherException.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.channels;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.channels
+ * Timestamp: 2018-09-17 10:51
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PublisherException extends RuntimeException {
+    public PublisherException(String message) {
+        super(message);
+    }
+}
