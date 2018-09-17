03e430903e295e4b210c169b4916b0bdf154e778
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:56:15 +0100

    Subscriber exception

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/channels/SubscriberException.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.channels;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.channels
+ * Timestamp: 2018-09-17 10:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class SubscriberException extends RuntimeException {
+    public SubscriberException(String message) {
+        super(message);
+    }
+}
