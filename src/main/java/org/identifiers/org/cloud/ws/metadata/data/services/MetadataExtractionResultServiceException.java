b89fe1c6d829732a0fac55af99c3aa2437d5348f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 13:52:29 +0100

    metadata extraction result service exception

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/services/MetadataExtractionResultServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.data.services;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.services
+ * Timestamp: 2018-09-17 13:52
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionResultServiceException extends RuntimeException {
+    public MetadataExtractionResultServiceException(String message) {
+        super(message);
+    }
+}
