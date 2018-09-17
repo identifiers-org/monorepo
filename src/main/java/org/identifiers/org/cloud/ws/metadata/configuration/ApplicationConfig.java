763aa83b5eb13f27d61421fcdc10b464efefc18b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 08:57:28 +0100

    first few configuration attributes

--- src/main/java/org/identifiers/org/cloud/ws/metadata/configuration/ApplicationConfig.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/configuration/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.metadata.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: metadata
@@ -7,7 +9,10 @@ package org.identifiers.org.cloud.ws.metadata.configuration;
  * Timestamp: 2018-02-07 11:46
  * ---
  */
+@Configuration
 public class ApplicationConfig {
-    
-
+    private String redisHost;
+    private int redisPort;
+    private String queueKeyMetadataExtractionRequest;
+    private String channelKeyMetadataExtractionResult;
 }
