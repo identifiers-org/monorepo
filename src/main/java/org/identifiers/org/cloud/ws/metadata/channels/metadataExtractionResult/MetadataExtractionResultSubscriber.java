64e644be4f76d6e2627218ed75a323e63276f4df
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 11:02:17 +0100

    MetadataExtractionResultSubscriber placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/channels/metadataExtractionResult/MetadataExtractionResultSubscriber.java
@@ -0,0 +1,16 @@
+package org.identifiers.org.cloud.ws.metadata.channels.metadataExtractionResult;
+
+import org.identifiers.org.cloud.ws.metadata.channels.Subscriber;
+import org.identifiers.org.cloud.ws.metadata.data.models.MetadataExtractionResult;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.channels.metadataExtractionResult
+ * Timestamp: 2018-09-17 11:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionResultSubscriber extends Subscriber<String, MetadataExtractionResult> {
+    // TODO
+}
