a22cb9d41bd422ae0a6810d68e737471139eeebd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 23:27:06 +0100

    MetadataExtractionRequestFactory with default factory method

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionRequestFactory.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.metadata.data.models;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.models
+ * Timestamp: 2018-09-17 23:26
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionRequestFactory {
+    public static MetadataExtractionRequest getMetadataExtractionRequest() {
+        return new MetadataExtractionRequest();
+    }
+}
