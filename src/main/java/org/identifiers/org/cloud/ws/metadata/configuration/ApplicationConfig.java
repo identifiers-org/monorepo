ddd0553360c77b5c01c8a1932954933d820344f6
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 09:09:02 +0100

    wire in redis port configuration

--- src/main/java/org/identifiers/org/cloud/ws/metadata/configuration/ApplicationConfig.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/configuration/ApplicationConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
-    
+    @Value("${spring.redis.port}")
     private int redisPort;
     private String queueKeyMetadataExtractionRequest;
     private String channelKeyMetadataExtractionResult;
