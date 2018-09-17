6a70d660e5cddc95cb850f6d0d986dafb4194576
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:11:42 +0100

    metadata extraction result factory

---
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/data/models/MetadataExtractionResultFactory.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.metadata.data.models;
+
+/**
+ * Project: metadata
+ * Package: org.identifiers.org.cloud.ws.metadata.data.models
+ * Timestamp: 2018-09-17 10:11
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MetadataExtractionResultFactory {
+}
