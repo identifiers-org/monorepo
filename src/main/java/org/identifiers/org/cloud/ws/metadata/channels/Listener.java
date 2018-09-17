abcb08b30faa3abea29e169fa11492551f470f32
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-17 10:50:44 +0100

    typo, listener interface

--- src/main/java/org/identifiers/org/cloud/ws/metadata/channels/Listener.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/channels/Listener.java
@@ -3,7 +3,7 @@ package org.identifiers.org.cloud.ws.metadata.channels;
 /**
  * Project: metadata
  * Package: org.identifiers.org.cloud.ws.metadata.channels
- * Timestamp: 2018-09-17 10:49
+ * Timestamp: 2018-09-17 10:50
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
