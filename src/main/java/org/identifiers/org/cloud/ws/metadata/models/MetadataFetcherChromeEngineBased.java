3612f7fd3f6b947788f606c32196de92aac03dde
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-09-18 17:58:53 +0100

    wire in the path to google chrome driver

--- src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherChromeEngineBased.java
+++ src/main/java/org/identifiers/org/cloud/ws/metadata/models/MetadataFetcherChromeEngineBased.java
@@ -2,6 +2,7 @@ package org.identifiers.org.cloud.ws.metadata.models;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,9 @@ import org.springframework.stereotype.Component;
 public class MetadataFetcherChromeEngineBased implements MetadataFetcher {
     private static final Logger logger = LoggerFactory.getLogger(MetadataFetcherChromeEngineBased.class);
 
+    @Value("${org.identifiers.cloud.ws.metadata.backend.selenium.driver.chrome.path.bin}")
+    private String pathChromedriver;
+    
     @Override
     public Object fetchMetadataFor(String url) throws MetadataFetcherException {
         return null;
