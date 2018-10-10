a5f4c3476a7bf02396a310316dc01534607cdcd3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-10 14:01:04 +0100

    institution data model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/Institution.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2018-10-10 14:00
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class Institution {
+}
