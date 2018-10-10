61097b9f07e439d81a6a2e132d02513e58d7265e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-10 14:13:06 +0100

    institution data model, initial model attributes

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/Institution.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/Institution.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.registry.api.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigInteger;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.api.models
@@ -8,5 +13,9 @@ package org.identifiers.cloud.hq.ws.registry.api.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Document
 public class Institution {
+    @Id private BigInteger id;
+    private String name;
+    private String description;
 }
