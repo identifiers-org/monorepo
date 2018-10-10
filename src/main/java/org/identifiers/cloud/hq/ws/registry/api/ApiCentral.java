46b738275f7e6c356b42655672390dcd5af65348
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-10 13:46:14 +0100

    API Central

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/ApiCentral.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.api;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api
+ * Timestamp: 2018-10-10 13:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiCentral {
+    // Current API Version flag
+    public static final String apiVersion = "1.0";
+}
