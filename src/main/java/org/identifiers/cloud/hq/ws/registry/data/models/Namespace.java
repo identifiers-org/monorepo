6e2cc2c210aac4ecb22a6b71f2b42001b2674dfd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-11 11:58:22 +0100

    namespace data model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Namespace.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2018-10-11 11:57
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This entity models a Prefix (or namespace) in the registry
+ */
+public class Namespace {
+    
+}
