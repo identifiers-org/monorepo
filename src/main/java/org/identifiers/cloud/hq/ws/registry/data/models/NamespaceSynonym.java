c88001acecdf9424f3c4cd0030b92ff6c47ee3e0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-11 12:09:26 +0100

    NamespaceSynonym model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/NamespaceSynonym.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2018-10-11 12:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Data model for namespace synonyms
+ */
+@Document
+public class NamespaceSynonym {
+
+}
