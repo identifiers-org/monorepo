9b7197766af5de6474a4e752f402f3b1924290ac
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-11 11:40:54 +0100

    resource data model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Resource.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2018-10-11 11:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class Resource {
+}
