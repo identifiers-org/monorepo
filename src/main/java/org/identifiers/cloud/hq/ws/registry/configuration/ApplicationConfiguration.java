6b494da4d8a58389dafe962c7d9d497637449ba1
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-11 20:59:04 +0100

    application configuration model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.configuration;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.configuration
+ * Timestamp: 2018-10-11 20:58
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApplicationConfiguration {
+}
