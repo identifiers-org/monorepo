88e2c2f1b862d74a3e1e4951ad90c53504ebb3cd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-11 11:56:46 +0100

    Location model, core attributes

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Location.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Location.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import org.springframework.data.annotation.Id;
+
+import java.math.BigInteger;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -7,6 +11,10 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This data model holds just enough information about a location, used in the registry
  */
 public class Location {
+    @Id private BigInteger id;
+    private String countryCode;
 }
