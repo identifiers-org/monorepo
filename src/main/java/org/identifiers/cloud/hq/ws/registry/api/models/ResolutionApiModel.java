b866fc5ac27abc8d6ef9076e70ab5ec460ecf0e4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-16 12:58:33 +0100

    ResolutionApiModel placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/ResolutionApiModel.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2018-10-16 12:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class ResolutionApiModel {
+}
