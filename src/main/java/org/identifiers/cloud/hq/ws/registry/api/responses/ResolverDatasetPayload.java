f0aee6ac985aa89fc60f24313dc2d97cff3b20d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-16 14:24:42 +0100

    resolver dataset payload, model implementation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ResolverDatasetPayload.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ResolverDatasetPayload.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.api.responses;
 
+import org.identifiers.cloud.hq.ws.registry.data.models.Namespace;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.api.responses
@@ -8,6 +14,15 @@ package org.identifiers.cloud.hq.ws.registry.api.responses;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-public class ResolverDatasetPayload {
-    // TODO
+public class ResolverDatasetPayload implements Serializable {
+    private List<Namespace> namespaces = new ArrayList<>();
+
+    public List<Namespace> getNamespaces() {
+        return namespaces;
+    }
+
+    public ResolverDatasetPayload setNamespaces(List<Namespace> namespaces) {
+        this.namespaces = namespaces;
+        return this;
+    }
 }
