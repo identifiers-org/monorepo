a38409b81032bb4b2322e6773723f1d1c93be8ca
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-16 14:33:42 +0100

    namespace service, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/NamespaceService.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2018-10-16 14:33
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class NamespaceService {
+    // TODO
+}
