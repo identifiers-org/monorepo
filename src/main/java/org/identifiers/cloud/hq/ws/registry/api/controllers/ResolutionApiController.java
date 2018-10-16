a9f3fc0204688eed6755fa55e9a319c08959eb52
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-16 12:47:11 +0100

    resolution API controller

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResolutionApiController.java
@@ -0,0 +1,17 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2018-10-16 12:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@RestController
+@RequestMapping("resolutionApi")
+public class ResolutionApiController {
+}
