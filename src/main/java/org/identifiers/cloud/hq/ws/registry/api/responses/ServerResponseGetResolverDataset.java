1f7f3406dbc757adc9582c15172f90c02c45be2a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-16 14:25:30 +0100

    ServerResponseGetResolverDataset service response

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServerResponseGetResolverDataset.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2018-10-16 14:25
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServerResponseGetResolverDataset extends ServiceResponse<ResolverDatasetPayload> {
+}
