54f15ac6b1580c94c9e370f1a859c5eb613723d5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-17 12:05:07 +0100

    api data model resource, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models
+ * Timestamp: 2018-10-17 12:04
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class Resource {
+    // TODO
+}
