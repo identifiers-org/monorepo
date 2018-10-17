c693e42508acd3d1f9e2acfa372a63f6c2972716
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-17 12:30:16 +0100

    data model helper in the context of the resolution API, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/helpers/ResolutionApiHelper.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.helpers;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.helpers
+ * Timestamp: 2018-10-17 12:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is a data helper in the context of the Resolution API data models
+ */
+public class ResolutionApiHelper {
+    // TODO
+}
