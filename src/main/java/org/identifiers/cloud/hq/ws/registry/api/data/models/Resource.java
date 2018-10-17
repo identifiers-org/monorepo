e84500ad5aa09277699c9fb0dd22c77b7c0a7ef3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-17 12:27:28 +0100

    getters setters

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
@@ -27,4 +27,94 @@ public class Resource implements Serializable {
     private String resourceUrl;
     private String institution;
     private String location;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public Resource setId(BigInteger id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getMirId() {
+        return mirId;
+    }
+
+    public Resource setMirId(String mirId) {
+        this.mirId = mirId;
+        return this;
+    }
+
+    public String getAccessUrl() {
+        return accessUrl;
+    }
+
+    public Resource setAccessUrl(String accessUrl) {
+        this.accessUrl = accessUrl;
+        return this;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public Resource setInfo(String info) {
+        this.info = info;
+        return this;
+    }
+
+    public boolean isOfficial() {
+        return official;
+    }
+
+    public Resource setOfficial(boolean official) {
+        this.official = official;
+        return this;
+    }
+
+    public String getResourcePrefix() {
+        return resourcePrefix;
+    }
+
+    public Resource setResourcePrefix(String resourcePrefix) {
+        this.resourcePrefix = resourcePrefix;
+        return this;
+    }
+
+    public String getLocalId() {
+        return localId;
+    }
+
+    public Resource setLocalId(String localId) {
+        this.localId = localId;
+        return this;
+    }
+
+    public String getResourceUrl() {
+        return resourceUrl;
+    }
+
+    public Resource setResourceUrl(String resourceUrl) {
+        this.resourceUrl = resourceUrl;
+        return this;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public Resource setInstitution(String institution) {
+        this.institution = institution;
+        return this;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Resource setLocation(String location) {
+        this.location = location;
+        return this;
+    }
 }
