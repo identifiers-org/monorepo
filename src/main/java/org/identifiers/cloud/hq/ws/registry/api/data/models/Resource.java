28552d465d740f3e01426eb0998a4d9e7ce369c9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-17 12:22:37 +0100

    Resource api data model core attributes

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
@@ -1,5 +1,8 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.models;
 
+import java.io.Serializable;
+import java.math.BigInteger;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.api.data.models
@@ -8,6 +11,18 @@ package org.identifiers.cloud.hq.ws.registry.api.data.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-public class Resource {
-    // TODO
+public class Resource implements Serializable {
+    private BigInteger id;
+    private String mirId;
+    private String accessUrl;
+    private String info;
+    private boolean official;
+    // TODO This should be a provider code
+    private String resourcePrefix;
+    // TODO This should be Sample ID
+    private String localId;
+    // TODO This should be Resource Home URL
+    private String resourceUrl;
+    private String institution;
+    private String location;
 }
