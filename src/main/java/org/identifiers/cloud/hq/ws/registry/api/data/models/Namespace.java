813ef4c11ee1b26da4037339b9fc30012ed9d0d6
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-17 12:27:55 +0100

    getters setters for namespace api data model

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Namespace.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Namespace.java
@@ -29,4 +29,103 @@ public class Namespace implements Serializable {
     private boolean deprecated = false;
     private Timestamp deprecationDate;
     private List<Resource> resources;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public Namespace setId(BigInteger id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public Namespace setPrefix(String prefix) {
+        this.prefix = prefix;
+        return this;
+    }
+
+    public String getMirId() {
+        return mirId;
+    }
+
+    public Namespace setMirId(String mirId) {
+        this.mirId = mirId;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Namespace setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public Namespace setPattern(String pattern) {
+        this.pattern = pattern;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Namespace setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public Namespace setCreated(Timestamp created) {
+        this.created = created;
+        return this;
+    }
+
+    public Timestamp getModified() {
+        return modified;
+    }
+
+    public Namespace setModified(Timestamp modified) {
+        this.modified = modified;
+        return this;
+    }
+
+    public boolean isDeprecated() {
+        return deprecated;
+    }
+
+    public Namespace setDeprecated(boolean deprecated) {
+        this.deprecated = deprecated;
+        return this;
+    }
+
+    public Timestamp getDeprecationDate() {
+        return deprecationDate;
+    }
+
+    public Namespace setDeprecationDate(Timestamp deprecationDate) {
+        this.deprecationDate = deprecationDate;
+        return this;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    public Namespace setResources(List<Resource> resources) {
+        this.resources = resources;
+        return this;
+    }
 }
