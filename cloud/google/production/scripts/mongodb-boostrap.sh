f5153bf41f970994d258c8f316f7574c261dca81
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-22 14:24:34 +0100

    script start

--- cloud/google/production/scripts/mongodb-boostrap.sh
+++ cloud/google/production/scripts/mongodb-boostrap.sh
@@ -6,5 +6,12 @@ source tinylogger.bash
 LOGGER_LVL=debug
 
 # Environment configuration
+HQ_MONGODB_BOOTSTRAP_N_REPLICAS=${HQ_MONGODB_BOOTSTRAP_N_REPLICAS:="3"}
+
+# Helpers
+function info() {
+    tlog info "[DEPLOYMENT][INFO] Replicas: ${HQ_MONGODB_BOOTSTRAP_N_REPLICAS}"
+}
 
 tlog info "[--- MongoDB Backend Bootstrap ---]"
+info
