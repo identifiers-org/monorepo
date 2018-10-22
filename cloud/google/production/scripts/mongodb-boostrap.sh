6fcee10d962e5a40e9735c73916c93f7708e6102
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-22 14:34:57 +0100

    get replicas and cluster name

--- cloud/google/production/scripts/mongodb-boostrap.sh
+++ cloud/google/production/scripts/mongodb-boostrap.sh
@@ -7,10 +7,12 @@ LOGGER_LVL=debug
 
 # Environment configuration
 HQ_MONGODB_BOOTSTRAP_N_REPLICAS=${HQ_MONGODB_BOOTSTRAP_N_REPLICAS:="3"}
+KUBERNETES_CLUSTER_NAME=`kubectl config current-context`
 
 # Helpers
 function info() {
-    tlog info "[DEPLOYMENT][INFO] Replicas: ${HQ_MONGODB_BOOTSTRAP_N_REPLICAS}"
+    tlog info "[DEVOPS] Replicas: ${HQ_MONGODB_BOOTSTRAP_N_REPLICAS}"
+    tlog info "[DEVOPS] Kubernetes Cluster: ${KUBERNETES_CLUSTER_NAME}"
 }
 
 tlog info "[--- MongoDB Backend Bootstrap ---]"
