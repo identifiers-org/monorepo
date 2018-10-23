a0851e49c40d68659f2ebeaf552ad77bb2988c7a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-23 09:18:01 +0100

    parameterizing the bootstrap script

--- cloud/google/production/scripts/mongodb-boostrap.sh
+++ cloud/google/production/scripts/mongodb-boostrap.sh
@@ -7,14 +7,14 @@ LOGGER_LVL=debug
 
 # Environment configuration
 HQ_MONGODB_BOOTSTRAP_N_REPLICAS=${HQ_MONGODB_BOOTSTRAP_N_REPLICAS:="3"}
-KUBERNETES_CLUSTER_NAME=`kubectl config current-context`
-KUBERNETES_CLUSTER_REGION=`gcloud container clusters list --filter=${KUBERNETES_CLUSTER_NAME} --format=yaml | grep "zone:" | cut -f2 -d' '`
+HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME=${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME:=`kubectl config current-context`}
+HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_REGION=${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_REGION:=`gcloud container clusters list --filter=${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME} --format=yaml | grep "zone:" | cut -f2 -d' '`}
 
 # Helpers
 function info() {
     tlog info "[DEVOPS] Replicas: ${HQ_MONGODB_BOOTSTRAP_N_REPLICAS}"
-    tlog info "[DEVOPS] Kubernetes Cluster: ${KUBERNETES_CLUSTER_NAME}"
-    tlog info "[DEVOPS] Kubernetes Region: ${KUBERNETES_CLUSTER_REGION}"
+    tlog info "[DEVOPS] Kubernetes Cluster: ${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME}"
+    tlog info "[DEVOPS] Kubernetes Region: ${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_REGION}"
 }
 
 tlog info "[--- MongoDB Backend Bootstrap ---]"
