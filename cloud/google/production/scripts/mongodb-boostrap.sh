c957d41bae000a7e4c7824b8a7cfe7dc7d9c4096
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-23 09:28:53 +0100

    MongoDB default admin user

--- cloud/google/production/scripts/mongodb-boostrap.sh
+++ cloud/google/production/scripts/mongodb-boostrap.sh
@@ -9,6 +9,7 @@ LOGGER_LVL=debug
 HQ_MONGODB_BOOTSTRAP_N_REPLICAS=${HQ_MONGODB_BOOTSTRAP_N_REPLICAS:="3"}
 HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME=${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME:=`kubectl config current-context`}
 HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_REGION=${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_REGION:=`gcloud container clusters list --filter=${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME} --format=yaml | grep "zone:" | cut -f2 -d' '`}
+HQ_MONGODB_BOOTSTRAP_MONGODB_ADMIN_USERNAME=${HQ_MONGODB_BOOTSTRAP_MONGODB_ADMIN_USERNAME:="mongadmin"}
 
 # Helpers
 function info() {
@@ -17,5 +18,6 @@ function info() {
     tlog info "[DEVOPS] Kubernetes Region: ${HQ_MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_REGION}"
 }
 
+# General Information before we start deploying MongoDB for the given configuration
 tlog info "[--- MongoDB Backend Bootstrap ---]"
 info
