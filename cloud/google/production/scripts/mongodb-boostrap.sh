3628ea8e2bec5e3914d3f69433e9090306ddeb59
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-23 14:00:29 +0100

    list status

--- cloud/google/production/scripts/mongodb-boostrap.sh
+++ cloud/google/production/scripts/mongodb-boostrap.sh
@@ -61,6 +61,9 @@ function setup_storage_class() {
     tlog info "[${MONGODB_BOOTSTRAP_KUBERNETES_CLUSTER_NAME}] Setting up Storage class '${MONGODB_BOOTSTRAP_KUBERNETES_STORAGE_CLASS_NAME}'"
     # TODO actually do it
     #kubectl apply -f "${MONGODB_BOOTSTRAP_FILE_KUBERNETES_STORAGE_CLASS}"
+    tlog info "---"
+    kubectl get all
+    tlog info "---"
 }
 
 function create_persistent_disks() {
@@ -73,7 +76,9 @@ function create_persistent_disks() {
         tlog info "[CLOUD] Creating Persistent Disk (${DISK_ZONE}) #$i (${MONGODB_BOOTSTRAP_KUBERNETES_STORAGE_VOLUME_SIZE}) - $DISK_NAME"
         #gcloud compute disks create --size ${MONGODB_BOOTSTRAP_KUBERNETES_STORAGE_VOLUME_SIZE} --type ${MONGODB_BOOTSTRAP_KUBERNETES_STORAGE_TYPE} ${DISK_NAME} --zone=${DISK_ZONE}
     done
-
+    tlog info "---"
+    gcloud compute disks list
+    tlog info "---"
 }
 
 
