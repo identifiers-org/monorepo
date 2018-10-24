0100a4f79aa17a3f5eb52ce7366aba882fdbdcf4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-24 14:11:31 +0100

    this is not working...

--- cloud/google/production/scripts/mongodb-boostrap.sh
+++ cloud/google/production/scripts/mongodb-boostrap.sh
@@ -146,9 +146,11 @@ function init_mongodb_cluster() {
     N_MINUS_ONE_REPLICAS=`echo "${MONGODB_BOOTSTRAP_N_REPLICAS} - 1" | bc`
     echo 'rs.initiate({_id: "MainRepSet", version: 1, members: [' > ${FILE_INIT_COMMAND}
     for i in $(seq 0 ${N_MINUS_ONE_REPLICAS}); do
-        echo -ne "\t{_id: $i, host:\"mongod-$i.${MONGODB_CLUSTER_DOMAIN}:27017\"}" >> ${FILE_INIT_COMMAND}
+        echo -ne "{ _id: $i, host : \"mongod-$i.${MONGODB_CLUSTER_DOMAIN}:27017\" }" >> ${FILE_INIT_COMMAND}
         if [ "${i}" != "${N_MINUS_ONE_REPLICAS}" ]; then
-            echo "," >> ${FILE_INIT_COMMAND}
+            echo -n "," >> ${FILE_INIT_COMMAND}
+        else
+            echo -n "" >> ${FILE_INIT_COMMAND}
         fi
     done
     echo "]});" >> ${FILE_INIT_COMMAND}
