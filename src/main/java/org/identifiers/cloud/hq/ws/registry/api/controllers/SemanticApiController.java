7bb56c31909c0731ecc1aaed3a3f71a764ddc955
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 11:55:31 +0100

    wire model for semantic api

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/SemanticApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/SemanticApiController.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.hq.ws.registry.api.controllers;
 
+import org.identifiers.cloud.hq.ws.registry.api.models.SemanticApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("semanticApi")
 public class SemanticApiController {
+    @Autowired
+    private SemanticApiModel model;
+
+    
 }
