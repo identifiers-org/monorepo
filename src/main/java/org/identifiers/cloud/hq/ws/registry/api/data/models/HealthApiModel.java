86d1f4dacbeea8ffdb3b53921b93413ee71371fc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 11:51:18 +0100

    health api model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/HealthApiModel.java
@@ -0,0 +1,27 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models;
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models
+ * Timestamp: 2018-11-15 11:50
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class HealthApiModel {
+    private static String runningSessionId = UUID.randomUUID().toString();
+
+    public String livenessCheck() {
+        return runningSessionId;
+    }
+
+    public String readinessCheck() {
+        return runningSessionId;
+    }
+
+}
