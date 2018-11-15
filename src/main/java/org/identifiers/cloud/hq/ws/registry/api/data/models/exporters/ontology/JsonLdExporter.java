27585c0996e07084e03749d8d8fdb95b070ec4ba
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:21:23 +0100

    jsonld exporter, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ontology/JsonLdExporter.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology;
+
+import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.Exporter;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology
+ * Timestamp: 2018-11-15 12:20
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class JsonLdExporter implements Exporter {
+}
