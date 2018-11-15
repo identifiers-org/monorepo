a96e25a9f0c6fe2e565ff5ce2da755f909eff24f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 13:23:18 +0100

    refactoring, include build context method

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ontology/JsonLdDocumentBuilder.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ontology/JsonLdDocumentBuilder.java
@@ -1,8 +1,7 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology;
 
-import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ExportedDocument;
-import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ExportDocumentBuilder;
 import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ExportDocumentBuilderException;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ExportedDocument;
 import org.identifiers.cloud.hq.ws.registry.data.models.Namespace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,17 +14,14 @@ import org.slf4j.LoggerFactory;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-public class JsonLdDocumentBuilder implements ExportDocumentBuilder {
+public class JsonLdDocumentBuilder implements ExportOntologyDocumentBuilder {
     private static final Logger logger = LoggerFactory.getLogger(JsonLdDocumentBuilder.class);
 
     private JsonLdDocument document = new JsonLdDocument();
 
-    public JsonLdDocumentBuilder() {
-        document.addContext("dc", "http://purl.org/dc/terms/");
-        document.addContext("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
-        document.addContext("foaf", "http://xmlns.com/foaf/0.1/");
-        document.addContext("owl", "http://www.w3.org/2002/07/owl#");
-        document.addContext("skos", "http://www.w3.org/2004/02/skos/core#");
+    @Override
+    public void buildContext(String namespace, String reference) throws ExportOntologyDocumentBuilderException {
+        document.addContext(namespace, reference);
     }
 
     @Override
