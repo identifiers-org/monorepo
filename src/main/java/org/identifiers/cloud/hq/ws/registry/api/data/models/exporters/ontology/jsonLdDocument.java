985392483e1389bbde39c68ce4bb436c54694b07
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:34:12 +0100

    jsonld document product, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ontology/jsonLdDocument.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology
+ * Timestamp: 2018-11-15 12:33
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class jsonLdDocument {
+}
