17db0503813a25e60d5db50e9c40785c29a17f95
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 11:52:31 +0100

    health api controller

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/HealthApiController.java
@@ -0,0 +1,32 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+import org.identifiers.cloud.hq.ws.registry.api.models.HealthApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2018-11-15 11:51
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@RestController
+public class HealthApiController {
+    @Autowired
+    private HealthApiModel model;
+
+    // liveness probe
+    @RequestMapping(value = "/liveness_check")
+    public String livenessCheck() {
+        return model.livenessCheck();
+    }
+
+    // Readiness check
+    @RequestMapping(value = "/readiness_check")
+    public String readinessCheck() {
+        return model.readinessCheck();
+    }
+}
