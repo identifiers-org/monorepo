d5a01b35830d105dd0faf0a0c068480772738fad
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:11:07 +0100

    context placeholder for the ontology export

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/ontology/Context.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.ontology;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.ontology
+ * Timestamp: 2018-11-15 12:10
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class Context {
+}
