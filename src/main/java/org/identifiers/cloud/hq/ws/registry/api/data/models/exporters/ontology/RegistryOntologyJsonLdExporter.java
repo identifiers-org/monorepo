6cdcf55e9ef109e96c1b570006ab35d1168f7d58
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 13:07:46 +0100

    RegistryOntologyJsonLdExporter, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ontology/RegistryOntologyJsonLdExporter.java
@@ -0,0 +1,23 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology;
+
+import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ExportedDocument;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.RegistryExporter;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.RegistryExporterException;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology
+ * Timestamp: 2018-11-15 13:03
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * I'll probably refactor this in the future to make it more pattern generic
+ */
+public class RegistryOntologyJsonLdExporter implements RegistryExporter {
+    @Override
+    public ExportedDocument export() throws RegistryExporterException {
+        // TODO -
+        return null;
+    }
+}
