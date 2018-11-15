d1da043287e28b0e424215ed1395358edaaa4af4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:58:03 +0100

    registry exporter director, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/RegistryExporter.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters
+ * Timestamp: 2018-11-15 12:57
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is the director of the export process for the registry data
+ */
+public class RegistryExporter {
+    // TODO
+}
