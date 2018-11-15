d4071c6ebf0f733cac6f1be69d40b757e2f6924e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 11:54:20 +0100

    semantic api model, TODO

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/SemanticApiModel.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2018-11-15 11:54
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class SemanticApiModel {
+}
