3d85b5575ae76b88ec9d7ee348698f0ae051c593
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:21:58 +0100

    abstract factory of exporters

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ExporterFactory.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters
+ * Timestamp: 2018-11-15 12:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * Abstract Factory of exporters
+ */
+public class ExporterFactory {
+    // TODO
+}
