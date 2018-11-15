33a9b27775ec62c037908f2504891af97c7b5845
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:23:34 +0100

    abstract base class for for the exported documents

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ExportedDocument.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ExportedDocument.java
@@ -1,14 +1,16 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters;
 
+import java.io.Serializable;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters
- * Timestamp: 2018-11-15 12:20
+ * Timestamp: 2018-11-15 12:22
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This si the product of the different exporters registered for the registry dataset
  */
-public interface Exporter {
-    // TODO
-
+public abstract class ExportedDocument implements Serializable {
 }
