201e4247b7f6d63d82714bc21f1d0c7805aa7b9d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:17:46 +0100

    context for ontology export

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/ontology/Context.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/ontology/Context.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.models.ontology;
 
+import java.io.Serializable;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.ontology
@@ -7,6 +9,15 @@ package org.identifiers.cloud.hq.ws.registry.api.data.models.ontology;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This is the first ontological view we implement for the registry dataset, it's gonna be a quick dirty hack, maybe, to
+ * get the prototype up and running by the end of the day, and, in the future, all this should be refactored with the
+ * concept of exporters or multiple views of the registry.
  */
-public class Context {
+public class Context implements Serializable {
+    private String dc = "";
+    private String rdfs = "";
+    private String foaf = "";
+    private String owl = "";
+    private String skos = "";
 }
