925dfc25c658e3308da263e663f4656706e2c484
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 13:06:59 +0100

    ExportOntologyDocumentBuilderException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/ontology/ExportOntologyDocumentBuilderException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ontology
+ * Timestamp: 2018-11-15 13:06
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ExportOntologyDocumentBuilderException extends RuntimeException {
+    public ExportOntologyDocumentBuilderException(String message) {
+        super(message);
+    }
+}
