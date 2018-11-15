8aa0fe6aa60c3d3114c0ee959ca1c3561b2be2df
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 13:30:11 +0100

    ServiceResponseSemanticExportRequest

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseSemanticExportRequest.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+import org.identifiers.cloud.hq.ws.registry.api.data.models.exporters.ExportedDocument;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2018-11-15 13:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseSemanticExportRequest extends ServiceResponse<ExportedDocument> {
+}
