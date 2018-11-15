7d0b35e0599958d7cd5eb54ce02a5c46f7da1e25
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 11:53:02 +0100

    API subpath for health checks

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/HealthApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/HealthApiController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * ---
  */
 @RestController
+@RequestMapping("healthApi")
 public class HealthApiController {
     @Autowired
     private HealthApiModel model;
