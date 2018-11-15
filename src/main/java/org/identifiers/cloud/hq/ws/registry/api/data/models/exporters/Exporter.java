bedcf168d6a04eb80ffce92fc00b8d15ceba720a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-11-15 12:20:40 +0100

    exporter interface

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/exporters/Exporter.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.exporters;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.exporters
+ * Timestamp: 2018-11-15 12:20
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface Exporter {
+    // TODO
+}
