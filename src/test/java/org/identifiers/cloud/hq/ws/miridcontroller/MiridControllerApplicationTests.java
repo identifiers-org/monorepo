e9302c2c1590f0d4e82080f191984a4424598007
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-01-22 11:07:01 +0000

    source tree

---
+++ src/test/java/org/identifiers/cloud/hq/ws/miridcontroller/MiridControllerApplicationTests.java
@@ -0,0 +1,17 @@
+package org.identifiers.cloud.hq.ws.miridcontroller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiridControllerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
