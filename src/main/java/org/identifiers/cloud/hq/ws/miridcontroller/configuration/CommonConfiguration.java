c32c5072c0ab72f111233e91fb8518d794736918
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 14:09:56 +0000

    common configuration class

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/configuration/CommonConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.configuration;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.configuration
+ * Timestamp: 2019-02-19 14:09
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class CommonConfiguration {
+}
