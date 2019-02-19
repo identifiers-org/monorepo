f72ae3afc1180970babe04458d7bb19983c09185
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 16:31:58 +0000

    ReturnedMirIdRepository, TODO

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/repositories/ReturnedMirIdRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.data.repositories;
+
+import org.identifiers.cloud.hq.ws.miridcontroller.data.models.ReturnedMirId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.data.repositories
+ * Timestamp: 2019-02-19 16:30
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ReturnedMirIdRepository extends JpaRepository<ReturnedMirId, Long> {
+    // TODO
+}
