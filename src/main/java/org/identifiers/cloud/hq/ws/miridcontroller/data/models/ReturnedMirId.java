6f2d4e545e68fe047ecbadacb21c786aff011e1e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 14:36:36 +0000

    ReturnedMirId model, model description

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ReturnedMirId.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ReturnedMirId.java
@@ -7,6 +7,8 @@ package org.identifiers.cloud.hq.ws.miridcontroller.data.models;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This model represents a MIR ID that has been "returned", i.e. deactivated
  */
 public class ReturnedMirId {
 }
