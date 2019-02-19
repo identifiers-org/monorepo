193235a4b891147ec2a458260d0f6457488f3831
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 14:05:58 +0000

    active mir ID model, core attributes

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ActiveMirId.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ActiveMirId.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 import javax.persistence.Entity;
+import java.util.Date;
 
 /**
  * Project: mirid-controller
@@ -21,4 +22,8 @@ import javax.persistence.Entity;
 @Accessors(chain = true)
 @Entity
 public class ActiveMirId {
+    private long id;
+    private Date created;
+    private Date lastConfirmed;
+    // TODO
 }
