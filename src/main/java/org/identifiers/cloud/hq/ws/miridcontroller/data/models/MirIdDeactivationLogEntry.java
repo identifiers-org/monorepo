097c3b1d8438e62344fffe9c9dad2216d69c1a00
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 15:01:08 +0000

    MirIdDeactivationLogEntry model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/MirIdDeactivationLogEntry.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.data.models;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.data.models
+ * Timestamp: 2019-02-19 15:00
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MirIdDeactivationLogEntry {
+}
