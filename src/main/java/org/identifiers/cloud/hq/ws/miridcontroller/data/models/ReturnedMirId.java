34f5934e6ab3332154cbda3b3bf55c42cc40de80
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 14:36:01 +0000

    ReturnedMirId model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ReturnedMirId.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.data.models;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.data.models
+ * Timestamp: 2019-02-19 14:35
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ReturnedMirId {
+}
