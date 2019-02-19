281027d8da0c37c6fa0d61432fa675a05c303fc3
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-19 14:17:54 +0000

    'created' field defined to be listened to, but not updatable

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ActiveMirId.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/data/models/ActiveMirId.java
@@ -1,10 +1,13 @@
 package org.identifiers.cloud.hq.ws.miridcontroller.data.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.Id;
@@ -32,7 +35,12 @@ public class ActiveMirId {
     private long mirId;
 
     // This is the date this particular MIR ID was created
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date created;
+
+    
     private Date lastConfirmed;
     // TODO
 }
