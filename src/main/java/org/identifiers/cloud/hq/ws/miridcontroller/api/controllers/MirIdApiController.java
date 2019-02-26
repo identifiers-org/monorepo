d6cc2c4c415a26c335b50b32136c3a0e1e295746
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-26 11:17:56 +0000

    mir id api controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/controllers/MirIdApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.api.controllers;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.api.controllers
+ * Timestamp: 2019-02-26 11:17
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MirIdApiController {
+}
