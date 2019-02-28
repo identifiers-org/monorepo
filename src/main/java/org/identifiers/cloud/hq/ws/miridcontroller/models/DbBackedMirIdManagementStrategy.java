b0a3338ea55b64a7249e47ad2c6e4ed4fe5b4b17
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-28 11:08:42 +0000

    mir id management strategy interface refactoring, default is success

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
@@ -64,8 +64,10 @@ public class DbBackedMirIdManagementStrategy implements MirIdManagementStrategy 
     @Transactional
     @Override
     public MirIdManagementStrategyOperationReport loadId(long id) throws MirIdManagementStrategyException {
+        MirIdManagementStrategyOperationReport report = new MirIdManagementStrategyOperationReport()
+                .setStatus(MirIdManagementStrategyOperationReport.Status.SUCCESS);
         // TODO
-        return null;
+        return report;
     }
 
     @Transactional
