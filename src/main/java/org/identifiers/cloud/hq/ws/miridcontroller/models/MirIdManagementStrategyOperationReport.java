a278f3f90905bca67b776feb38995c112dc6d59a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-28 10:54:48 +0000

    MirIdManagementStrategyOperationReport, lombok annotations

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/MirIdManagementStrategyOperationReport.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/MirIdManagementStrategyOperationReport.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.miridcontroller.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 /**
  * Project: mirid-controller
  * Package: org.identifiers.cloud.hq.ws.miridcontroller.models
@@ -11,9 +16,14 @@ package org.identifiers.cloud.hq.ws.miridcontroller.models;
  * This models a report on how the MIR ID Management strategy completed the requested operation. This report will allow
  * clients to get insight informatino for more detailed error reporting back to requester
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
 public class MirIdManagementStrategyOperationReport {
     // TODO
     public enum Status {
         BAD_REQUEST;
     }
+
 }
