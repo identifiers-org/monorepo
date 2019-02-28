9fbec3ccd0effc9c960bd456795dfbf1862182ce
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-28 13:43:26 +0000

    mir id api controller, mintId() endpoint

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/controllers/MirIdApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/controllers/MirIdApiController.java
@@ -2,7 +2,10 @@ package org.identifiers.cloud.hq.ws.miridcontroller.api.controllers;
 
 import org.identifiers.cloud.hq.ws.miridcontroller.api.models.MirIdApiModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,5 +22,10 @@ public class MirIdApiController {
     @Autowired
     private MirIdApiModel model;
 
+    @GetMapping("/mintId")
+    public @ResponseBody
+    ResponseEntity<?> mintId() {
+        return model.mintId();
+    }
     // TODO
 }
