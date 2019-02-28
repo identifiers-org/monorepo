563a01b954319ce2b5e2aca79bd169c1cd8a1800
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-28 10:26:59 +0000

    MirIdManagementStrategyOperationReport, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/MirIdManagementStrategyOperationReport.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.models;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.models
+ * Timestamp: 2019-02-28 10:26
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MirIdManagementStrategyOperationReport {
+}
