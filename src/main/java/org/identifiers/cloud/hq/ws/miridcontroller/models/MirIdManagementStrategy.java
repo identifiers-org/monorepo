bba533298d96ddfebaed9ce44767f090ff3c47a7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-02-28 10:09:05 +0000

    mir id management strategy interface refactoring

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/MirIdManagementStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/MirIdManagementStrategy.java
@@ -21,21 +21,27 @@ public interface MirIdManagementStrategy {
     /**
      * Tell the MIR ID management subsystem that a particular MIR ID has been confirmed as 'still active'
      * @param id MIR ID
+     * @return the given MIR ID if the request was completed successfully or 'null' if it was a bad request, e.g. the
+     * MIR ID is not active, thus, it cannot be kept alive
      * @throws MirIdManagementStrategyException
      */
-    void keepAlive(long id) throws MirIdManagementStrategyException;
+    long keepAlive(long id) throws MirIdManagementStrategyException;
 
     /**
      * Tell the MIR ID management subsystem to load a particular MIR ID as 'active', i.e. 'in use' ID
      * @param id MIR ID
+     * @return the given MIR ID if the request was completed successfully or 'null' if it was a bad request, e.g. the
+     * MIR ID to be loaded was already active.
      * @throws MirIdManagementStrategyException
      */
-    void loadId(long id) throws MirIdManagementStrategyException;
+    long loadId(long id) throws MirIdManagementStrategyException;
 
     /**
      * Request deactivation of a given MIR ID to the MIR ID management subsystem.
      * @param id MIR ID to be deactivated
+     * @return the given MIR ID if the request was completed successfully or 'null' if it was a bad request, e.g. the
+     * given ID was not active, thus it cannot be returned
      * @throws MirIdManagementStrategyException
      */
-    void returnId(long id) throws MirIdManagementStrategyException;
+    long returnId(long id) throws MirIdManagementStrategyException;
 }
