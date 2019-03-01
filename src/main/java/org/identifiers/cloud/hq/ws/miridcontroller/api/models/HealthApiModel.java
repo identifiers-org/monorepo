c3912db66fc3602530ad933eeee8acbbb3c6d8f8
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-01 10:00:31 +0000

    HealthApiModel, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/models/HealthApiModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.api.models;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.api.models
+ * Timestamp: 2019-03-01 10:00
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HealthApiModel {
+}
