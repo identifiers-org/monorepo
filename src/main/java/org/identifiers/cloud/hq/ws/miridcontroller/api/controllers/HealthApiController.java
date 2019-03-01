cdc000ca43d6af57b24ebf4c4e7346c38df6bcf3
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-01 10:21:43 +0000

    HealthApiController, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/controllers/HealthApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.api.controllers;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.api.controllers
+ * Timestamp: 2019-03-01 10:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HealthApiController {
+}
