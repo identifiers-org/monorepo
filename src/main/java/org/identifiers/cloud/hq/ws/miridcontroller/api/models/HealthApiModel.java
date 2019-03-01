24bfcff01d96da9c7645abf4ff17b46ac39047ed
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-01 11:37:39 +0000

    health api model, implementation

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/models/HealthApiModel.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/api/models/HealthApiModel.java
@@ -2,6 +2,8 @@ package org.identifiers.cloud.hq.ws.miridcontroller.api.models;
 
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 /**
  * Project: mirid-controller
  * Package: org.identifiers.cloud.hq.ws.miridcontroller.api.models
@@ -13,4 +15,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class HealthApiModel {
     // TODO
+    private static String runningSessionId = UUID.randomUUID().toString();
+
+    public String livenessCheck() {
+        return runningSessionId;
+    }
+
+    // TODO - Refactor this to actually reflect whether the service ir ready or not
+    public String readinessCheck() {
+        return runningSessionId;
+    }
 }
