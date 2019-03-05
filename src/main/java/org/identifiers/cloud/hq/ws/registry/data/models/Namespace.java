00259a71eb49d87cb9982735aae5d3989f347f6a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-05 16:42:47 +0000

    namespace core attributes

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Namespace.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Namespace.java
@@ -1,14 +1,13 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
-import javax.validation.constraints.NotNull;
-import java.math.BigInteger;
+import javax.persistence.Entity;
 import java.sql.Timestamp;
-import java.util.List;
 
 /**
  * Project: registry
@@ -20,17 +19,17 @@ import java.util.List;
  *
  * This entity models a Prefix (or namespace) in the registry
  */
-@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@Entity
 // TODO - refactoring to relational
 public class Namespace {
-    @Id private BigInteger id;
-
-    @NotNull(message = "The prefix itself must be provided, otherwise the entry makes no sense")
-    @Indexed(unique = true)
+    private long id;
     private String prefix;
-    @Indexed(unique = true)
     private String mirId;
-    @Indexed
     private String name;
     private String pattern;
     private String description;
@@ -38,114 +37,5 @@ public class Namespace {
     private Timestamp modified;
     private boolean deprecated = false;
     private Timestamp deprecationDate;
-    @DBRef private List<Resource> resources;
-    @DBRef private List<NamespaceSynonym> namespaceSynonyms;
-
-    public List<NamespaceSynonym> getNamespaceSynonyms() {
-        return namespaceSynonyms;
-    }
-
-    public Namespace setNamespaceSynonyms(List<NamespaceSynonym> namespaceSynonyms) {
-        this.namespaceSynonyms = namespaceSynonyms;
-        return this;
-    }
-
-    public BigInteger getId() {
-        return id;
-    }
-
-    public Namespace setId(BigInteger id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public Namespace setPrefix(String prefix) {
-        this.prefix = prefix;
-        return this;
-    }
-
-    public String getMirId() {
-        return mirId;
-    }
-
-    public Namespace setMirId(String mirId) {
-        this.mirId = mirId;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Namespace setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public Namespace setPattern(String pattern) {
-        this.pattern = pattern;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Namespace setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public Timestamp getCreated() {
-        return created;
-    }
-
-    public Namespace setCreated(Timestamp created) {
-        this.created = created;
-        return this;
-    }
-
-    public Timestamp getModified() {
-        return modified;
-    }
-
-    public Namespace setModified(Timestamp modified) {
-        this.modified = modified;
-        return this;
-    }
-
-    public boolean isDeprecated() {
-        return deprecated;
-    }
-
-    public Namespace setDeprecated(boolean deprecated) {
-        this.deprecated = deprecated;
-        return this;
-    }
-
-    public Timestamp getDeprecationDate() {
-        return deprecationDate;
-    }
-
-    public Namespace setDeprecationDate(Timestamp deprecationDate) {
-        this.deprecationDate = deprecationDate;
-        return this;
-    }
-
-    public List<Resource> getResources() {
-        return resources;
-    }
-
-    public Namespace setResources(List<Resource> resources) {
-        this.resources = resources;
-        return this;
-    }
+    private String sampleId;
 }
