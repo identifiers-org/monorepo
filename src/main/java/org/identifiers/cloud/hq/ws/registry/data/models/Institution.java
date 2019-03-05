f77ce4423a6a6b706b0483641ab98afd69603a62
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-05 15:44:09 +0000

    build index on 'name' field

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Institution.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Institution.java
@@ -24,6 +24,7 @@ import javax.persistence.*;
 @EqualsAndHashCode
 @Accessors(chain = true)
 @Entity
+@Table(indexes = {@Index(name = "idx_name", columnList = "name", unique = true)})
 // TODO - refactoring to relational
 public class Institution {
     @Id
