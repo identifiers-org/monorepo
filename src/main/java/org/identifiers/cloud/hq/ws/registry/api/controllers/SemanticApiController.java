3ae388ced969d4295ca81f2b2f43af8ac700fe83
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-06 14:02:21 +0000

    disable the semantic endpoint

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/SemanticApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/SemanticApiController.java
@@ -1,8 +1,8 @@
 package org.identifiers.cloud.hq.ws.registry.api.controllers;
 
 import org.identifiers.cloud.hq.ws.registry.api.models.SemanticApiModel;
-import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseSemanticExportRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,9 +22,11 @@ public class SemanticApiController {
     @Autowired
     private SemanticApiModel model;
 
+    // TODO The semantic endpoint has been disabled
     @RequestMapping(value = "getRegistryOntology", method = RequestMethod.GET)
     public ResponseEntity<?> getRegistryOntology() {
-        ServiceResponseSemanticExportRequest response = model.getRegistryOntology();
-        return new ResponseEntity<>(response.getPayload(), response.getHttpStatus());
+        //ServiceResponseSemanticExportRequest response = model.getRegistryOntology();
+        //return new ResponseEntity<>(response.getPayload(), response.getHttpStatus());
+        return new ResponseEntity<>("(^_^) we'll be back _.~\"~._.~\"~._.~\"~._.~\"~._", HttpStatus.I_AM_A_TEAPOT);
     }
 }
