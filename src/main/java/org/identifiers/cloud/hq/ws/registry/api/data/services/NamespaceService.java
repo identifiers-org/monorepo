3a65d14aa7eb9d2f98a6b49a5f596c41d1e19af5
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-06 14:29:17 +0000

    namespace service for the api models

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/services/NamespaceService.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.services
+ * Timestamp: 2019-03-06 14:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class NamespaceService {
+}
