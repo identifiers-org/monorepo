17e73d29817f52f711266e14967fa5e2c5f28124
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-13 17:46:21 +0000

    Person model, JPA stuff

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Person.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Person.java
@@ -1,10 +1,17 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Project: registry
@@ -21,7 +28,22 @@ import lombok.experimental.Accessors;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Accessors(chain = true)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {@Index(name = "idx_fullname", columnList = "fullName")})
 public class Person {
-    private String fullName;
+    @Id
     private String email;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date created;
+
+    @LastModifiedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date modified;
 }
