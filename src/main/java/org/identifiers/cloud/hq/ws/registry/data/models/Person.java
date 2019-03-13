eb9fce632ac73312963656496d424167e4cd8bc6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-13 17:41:46 +0000

    Person model, lombok annotations

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Person.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/Person.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -10,5 +16,11 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  *
  * This model represents a person's contact details (minimum)
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class Person {
+    // TODO
 }
