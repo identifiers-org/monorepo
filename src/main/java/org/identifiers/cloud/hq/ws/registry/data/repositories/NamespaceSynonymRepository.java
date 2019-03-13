9385e00c1402f36854b0baa14c3512c67131153a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-13 18:13:15 +0000

    NamespaceSynonymRepository, it was missing

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/NamespaceSynonymRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.NamespaceSynonym;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2019-03-13 18:12
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface NamespaceSynonymRepository extends JpaRepository<NamespaceSynonym, Long> {
+    // TODO
+}
