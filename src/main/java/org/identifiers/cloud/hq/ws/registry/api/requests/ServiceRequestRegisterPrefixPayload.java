5bd537d3eca3067d5e2b4f0bc34343c6213a086f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 13:44:15 +0000

    ServiceRequestRegisterPrefixPayload model

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterPrefixPayload.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterPrefixPayload.java
@@ -1,5 +1,13 @@
 package org.identifiers.cloud.hq.ws.registry.api.requests;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.Requester;
+
 import java.io.Serializable;
 
 /**
@@ -10,5 +18,22 @@ import java.io.Serializable;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ServiceRequestRegisterPrefixPayload implements Serializable {
+    private String name;
+    private String description;
+    private String homePage;
+    private String organization;
+    private String preferredPrefix;
+    private String resourceAccessRule;
+    private String exampleIdentifier;
+    private String regexPattern;
+    private String[] references;
+    private String additionalInformation;
+    private Requester requester;
 }
