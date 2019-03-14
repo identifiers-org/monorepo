e141cb4004aac58f0610bd36639f2015e2d1bf18
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 13:40:19 +0000

    ServiceRequestRegisterPrefixPayload, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterPrefixPayload.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+import java.io.Serializable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-03-14 13:39
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestRegisterPrefixPayload implements Serializable {
+}
