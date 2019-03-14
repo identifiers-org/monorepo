a142e3ad581fe59c5f74572e81aef9ddcf8171d8
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 15:01:59 +0000

    requester validator exception

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/RequesterValidatorException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-03-14 15:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class RequesterValidatorException extends RuntimeException {
+    public RequesterValidatorException(String message) {
+        super(message);
+    }
+}
