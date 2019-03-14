e0a6d4c4bf01b65fc1a6c059562c46b0547d3c5b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 13:47:44 +0000

    prefix registration request specialization

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterPrefix.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-03-14 13:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestRegisterPrefix extends ServiceRequest<ServiceRequestRegisterPrefixPayload> {
+}
