92d4ea6b8bf98a787b3a0ddf64176bde94ce9084
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 13:57:55 +0000

    service response to prefix registration requests

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseRegisterPrefix.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2019-03-14 13:57
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseRegisterPrefix extends ServiceResponse<ServiceResponseRegisterPrefixPayload> {
+}
