1c1db76f884ef38821acbe74e5b9950f4eda09d7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 15:56:33 +0000

    default web page checker

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/WebPageCheckerDefault.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-03-14 15:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class WebPageCheckerDefault implements WebPageChecker {
+    // Default web page checker doesn't do anything extraordinary
+}
