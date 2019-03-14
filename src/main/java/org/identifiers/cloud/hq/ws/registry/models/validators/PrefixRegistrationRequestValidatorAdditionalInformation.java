003fd7928b7739cfd8085773452dff5312807c95
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 15:32:19 +0000

    PrefixRegistrationRequestValidatorAdditionalInformation

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorAdditionalInformation.java
@@ -0,0 +1,33 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-03-14 15:31
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+@Scope("prototype")
+@Qualifier("prefixRegistrationRequestValidatorAdditionalInformation")
+public class PrefixRegistrationRequestValidatorAdditionalInformation implements PrefixRegistrationRequestValidator {
+    private static Logger logger = LoggerFactory.getLogger(PrefixRegistrationRequestValidatorAdditionalInformation.class);
+
+    @Override
+    public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
+        // Validate by default, no requirements are enforced
+        logger.info("DEFAULT policy for Additional Information is ALWAYS VALID");
+        if (request.getAdditionalInformation() == null) {
+            logger.info("NO Additional Information has been provided");
+        }
+        return true;
+    }
+}
