0c5e6ffda65df13079a422e2af51d26977064dbb
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 15:01:21 +0000

    RequesterValidator, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/RequesterValidator.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-03-14 15:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface RequesterValidator {
+}
