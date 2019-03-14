3519df4f5a13f067817214e939492a637f990477
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 15:08:46 +0000

    interface description

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/RequesterValidator.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/RequesterValidator.java
@@ -9,6 +9,8 @@ import org.identifiers.cloud.hq.ws.registry.api.data.models.Requester;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This interface models the contract every validator around a 'requester' data model should fullfil.
  */
 public interface RequesterValidator {
     boolean validate(Requester requester) throws RequesterValidatorException;
