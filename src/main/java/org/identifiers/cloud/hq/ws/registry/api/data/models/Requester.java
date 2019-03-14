5dc4539b7c199764b95f5fdcf91226c7277755dd
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 13:43:57 +0000

    requester model for api payload

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Requester.java
@@ -0,0 +1,29 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models
+ * Timestamp: 2019-03-14 13:43
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Requester implements Serializable {
+    private String name;
+    private String email;
+}
