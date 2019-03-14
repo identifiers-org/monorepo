afbbd9b82db09d59fefc8fa4cee0c294a0fc3734
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 15:48:50 +0000

    PrefixRegistrationRequestValidatorRequester

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorRequester.java
@@ -0,0 +1,33 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixPayload;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-03-14 15:48
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+@Scope("prototype")
+@Qualifier("prefixRegistrationRequestValidatorRequester")
+public class PrefixRegistrationRequestValidatorRequester implements PrefixRegistrationRequestValidator {
+    @Override
+    public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
+        if (request.getRequester() == null) {
+            throw new PrefixRegistrationRequestValidatorException("MISSING REQUIRED Requester information");
+        }
+        // Delegate validation to specialized validator
+        try {
+            RequesterValidatorFactory.getDefaultValidator().validate(request.getRequester());
+        } catch (RequesterValidatorException e) {
+            throw new PrefixRegistrationRequestValidatorException(e.getMessage());
+        }
+        return true;
+    }
+}
