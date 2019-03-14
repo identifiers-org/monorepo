4a06f85fad82a130843f6b454a8f4a5d288403a2
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-14 13:57:15 +0000

    ServiceResponseRegisterPrefixPayload model definition

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseRegisterPrefixPayload.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseRegisterPrefixPayload.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.api.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
 /**
@@ -10,5 +16,15 @@ import java.io.Serializable;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class ServiceResponseRegisterPrefixPayload implements Serializable {
+    // Comments on the prefix registration request
+    private String comment = "No comments on your prefix registration request";
+    // This is an ephemeral token whose lifecycle is tight to the lifecycle of the prefix registration requests, so,
+    // once the prefix registration has been solved, either rejected or accepted, this token is no longer valid.
+    private String token = "";
 }
