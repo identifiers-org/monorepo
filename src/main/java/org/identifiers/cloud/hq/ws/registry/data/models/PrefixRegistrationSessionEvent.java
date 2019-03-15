568e02199a6443a8e4588a40f568fc8d3c855227
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 11:43:53 +0000

    prefix registration session event data model, lombok annotations

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSessionEvent.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSessionEvent.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -8,6 +14,11 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class PrefixRegistrationSessionEvent {
     // TODO
 }
