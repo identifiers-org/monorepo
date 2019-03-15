7c83de5b0b20d728a188231b57693ee82bd484d7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 13:17:14 +0000

    prefix registration request model, for persistence, 'organizationDescription' is missing in the original request model

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
@@ -38,6 +38,9 @@ public class PrefixRegistrationRequest {
     private String homePage;
     // Organization, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String organization;
+    // WARNING - This is missing in the original original identifiers.org form at https://identifiers.org/request/prefix,
+    // because I understand things where done by hand, but it is no longer the case
+    private String organizationDescription;
     // This is from 'preferredPrefix', ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String prefix;
     // Originally called 'resourceAccessUrl', ported from the original identifiers.org form at https://identifiers.org/request/prefix
