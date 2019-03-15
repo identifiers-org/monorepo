75f1fed3cf204554da09a7bd7f69c58c6d24257e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 10:32:02 +0000

    prefix registration management service, interface placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationRequestManagementService.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-15 10:26
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface PrefixRegistrationRequestManagementService {
+}
