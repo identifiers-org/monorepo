3e7f2b08fa1ade2056e45502dc3dec04585eb046
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 11:43:13 +0000

    prefix registration session event data model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSessionEvent.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2019-03-15 11:42
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationSessionEvent {
+    // TODO
+}
