c4808d986c466c2bb21dc4945fb495d973b72adc
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 13:11:11 +0000

    prefix registration request model, for persistence, core attributes documentation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
@@ -30,16 +30,33 @@ import javax.persistence.EntityListeners;
 public class PrefixRegistrationRequest {
     // TODO
     private long id;
+    // Name, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String name;
+    // A sensible description for this request, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String description;
+    // Home page of this first resource / provider within the context of this new prefix, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String homePage;
+    // Organization, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String organization;
+    // This is from 'preferredPrefix', ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String prefix;
+    // Originally called 'resourceAccessUrl', ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String urlPattern;
+    // Originally called 'exampleIdentifier', ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String sampleId;
+    // Originally called regexPattern, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String idRegexPattern;
+    // Originally called 'references', but, apparently, when hibernate produces the DDL, it doesn't do it very well and
+    // the attribute name clashes with the reserved keyword 'references' of the SQL dialect used by the RDBMS backend,
+    // ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String supportingReferences;
+    // Additional information to be included as part of the request, ported from the original identifiers.org form at
+    // https://identifiers.org/request/prefix
     private String additionalInformation;
+    // Contact person for this request, ported from the original identifiers.org form at
+    // https://identifiers.org/request/prefix
     private String requesterName;
+    // Contact person for this request, ported from the original identifiers.org form at
+    // https://identifiers.org/request/prefix
     private String requesterEmail;
 }
