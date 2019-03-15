b50feef1d10f96afc6d0505eac5bdc2630622ae8
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 12:13:28 +0000

    prefix registration request model, for persistence

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2019-03-15 12:11
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This model represents a prefix registration request from the point of view of its persisted representation.
+ */
+public class PrefixRegistrationRequest {
+    // TODO
+}
