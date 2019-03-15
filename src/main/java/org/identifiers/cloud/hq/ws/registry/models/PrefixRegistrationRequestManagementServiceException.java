c5abff8cbe6c43cf5c3cfc2c2ff49d93719a8c5a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 10:46:32 +0000

    PrefixRegistrationRequestManagementServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationRequestManagementServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-15 10:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationRequestManagementServiceException extends RuntimeException {
+    public PrefixRegistrationRequestManagementServiceException(String message) {
+        super(message);
+    }
+}
