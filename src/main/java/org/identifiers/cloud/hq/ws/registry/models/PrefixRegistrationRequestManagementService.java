99024ac49ed26706190cc16ce5f4b59b0ab5404b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 10:46:03 +0000

    prefix registration management service, interface description

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationRequestManagementService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationRequestManagementService.java
@@ -7,6 +7,14 @@ package org.identifiers.cloud.hq.ws.registry.models;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * Attending a prefix registration request involves registering the request itself, making possible ammendments to it,
+ * possibly associating comments to the request, and closing the request by either accepting it or rejecting it.
+ *
+ * A prefix registration request management service implements this operations to support this workflow, and it follows
+ * a strategy pattern.
+ *
  */
 public interface PrefixRegistrationRequestManagementService {
+    // TODO
 }
