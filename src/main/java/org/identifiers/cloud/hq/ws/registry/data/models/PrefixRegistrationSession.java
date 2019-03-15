65164d7ebccfb50d87e9357d534864da8b54fca1
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-15 11:28:14 +0000

    prefix registration session data model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSession.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2019-03-15 11:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationSession {
+}
