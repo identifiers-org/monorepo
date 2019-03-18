1c1aa8c86894c375f6104cbc57f138b9675bd466
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 15:07:11 +0000

    PrefixRegistrationRequestValidationApiController, endpoint for validation of additional information

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/PrefixRegistrationRequestValidationApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/PrefixRegistrationRequestValidationApiController.java
@@ -110,5 +110,11 @@ public class PrefixRegistrationRequestValidationApiController {
         ServiceResponseValidateRequest response = model.validateReferences(request);
         return new ResponseEntity<>(response, response.getHttpStatus());
     }
+
+    @PostMapping(value = "/validateAdditionalInformation")
+    public ResponseEntity<?> validateAdditionalInformation(@RequestBody ServiceRequestValidate request) {
+        ServiceResponseValidateRequest response = model.validateAdditionalInformation(request);
+        return new ResponseEntity<>(response, response.getHttpStatus());
+    }
     // TODO
 }
