001d40be4adbaffce6de6e02305d9c6aa72fd2b1
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 13:35:34 +0000

    validation API model, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/ValidationApiModel.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2019-03-18 13:35
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ValidationApiModel {
+}
