ea97e764ff71fd5539bea2af83c60c5d47ba9c8b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 14:54:12 +0000

    PrefixRegistrationRequestValidationApiController, wire in the model

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/PrefixRegistrationRequestValidationApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/PrefixRegistrationRequestValidationApiController.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.hq.ws.registry.api.controllers;
 
+import org.identifiers.cloud.hq.ws.registry.api.models.PrefixRegistrationRequestValidationApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController(value = "/prefixRegistrationApi")
 public class PrefixRegistrationRequestValidationApiController {
+    @Autowired
+    private PrefixRegistrationRequestValidationApiModel model;
+
+    
+    // TODO
 }
