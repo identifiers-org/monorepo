22237866e3671b29a274614b9a716759062dde86
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 13:40:23 +0000

    validation API model, autowire validator for provider name

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/ValidationApiModel.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/ValidationApiModel.java
@@ -33,4 +33,8 @@ public class ValidationApiModel {
     @Qualifier("prefixRegistrationRequestValidatorProviderHomeUrl")
     private PrefixRegistrationRequestValidator providerHomeUrlValidator;
 
+    @Autowired
+    @Qualifier("prefixRegistrationRequestValidatorProviderName")
+    private PrefixRegistrationRequestValidator providerNameValidator;
+
 }
