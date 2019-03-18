345d8d11e10ecc37688084a14a00a82dcd41c859
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 14:44:57 +0000

    PrefixRegistrationRequestValidationApiController, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/PrefixRegistrationRequestValidationApiController.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2019-03-18 14:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationRequestValidationApiController {
+}
