86ddaf559d83cca589ed3ec2bdba211e8e190b41
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 11:34:43 +0000

    provider description validator, validate() implementation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorProviderDescription.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorProviderDescription.java
@@ -1,6 +1,9 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
 import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixPayload;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * Project: registry
@@ -10,11 +13,18 @@ import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterP
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Scope("prototype")
+@Qualifier("PrefixRegistrationRequestValidatorProviderDescription")
 public class PrefixRegistrationRequestValidatorProviderDescription implements PrefixRegistrationRequestValidator {
     public static final int DESCRIPTIONI_CONTENT_MIN_LENGTH = 50;
-    
+
     @Override
     public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
-        return false;
+        if ((request.getProviderDescription() == null) || (request.getProviderDescription().length() < DESCRIPTIONI_CONTENT_MIN_LENGTH)) {
+            throw new PrefixRegistrationRequestValidatorException(String.format("Provider description MUST BE " +
+                    "present, and longer than %d characters", DESCRIPTIONI_CONTENT_MIN_LENGTH));
+        }
+        return true;
     }
 }
