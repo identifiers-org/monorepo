61fedb84a91ca3a300cfdea246282bedb44d353e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 12:24:32 +0000

    institution description validator, handle validation error

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorInstitutionDescription.java
@@ -0,0 +1,21 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixPayload;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-03-18 12:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationRequestValidatorInstitutionDescription implements PrefixRegistrationRequestValidator {
+    @Override
+    public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
+        if (request.getInstitutionDescription() == null) {
+            throw new PrefixRegistrationRequestValidatorException("Institution Description is REQUIRED, but it's missing.");
+        }
+        return false;
+    }
+}
