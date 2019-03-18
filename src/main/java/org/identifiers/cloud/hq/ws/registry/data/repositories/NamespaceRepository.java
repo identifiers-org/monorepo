be91317894e864f10cbe397fc19e035e30d01f65
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-18 10:13:15 +0000

    update namespace repository with find method by prefix

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/NamespaceRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/NamespaceRepository.java
@@ -13,4 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface NamespaceRepository extends JpaRepository<Namespace, Long> {
     // TODO
+    Namespace findByPrefix(String prefix);
 }
