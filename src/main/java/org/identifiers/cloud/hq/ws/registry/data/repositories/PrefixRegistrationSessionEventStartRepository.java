e45359fd5ba78d5d5adc7d7caf7cceb54654670b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-19 13:30:55 +0000

    PrefixRegistrationSessionEventStartRepository

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/PrefixRegistrationSessionEventStartRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSessionEventStart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: hq-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2019-03-19 13:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface PrefixRegistrationSessionEventStartRepository extends JpaRepository<PrefixRegistrationSessionEventStart, Long> {
+    // TODO
+}
