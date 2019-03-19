fb7fa519a4fdb6fa8200997dfe61d45f44e477eb
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-19 10:16:20 +0000

    prefix registration request data model, column definition for 'requestedPrefix'

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationRequest.java
@@ -72,6 +72,7 @@ public class PrefixRegistrationRequest {
     private String institutionLocation;
 
     // Originally called 'preferredPrefix', ported from the original identifiers.org form at https://identifiers.org/request/prefix
+    @Column(nullable = false)
     private String requestedPrefix;
 
     // Originally called 'resourceAccessRule', ported from the original identifiers.org form at https://identifiers.org/request/prefix
