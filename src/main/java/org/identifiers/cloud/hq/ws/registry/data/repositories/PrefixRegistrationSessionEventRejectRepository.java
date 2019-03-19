d1915a8f1a9087972bf255c6ab277f322cca23e8
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-19 13:25:43 +0000

    PrefixRegistrationSessionEventRejectRepository

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/PrefixRegistrationSessionEventRejectRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSessionEventReject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: hq-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2019-03-19 13:23
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface PrefixRegistrationSessionEventRejectRepository extends JpaRepository<PrefixRegistrationSessionEventReject, Long> {
+    // TODO
+}
