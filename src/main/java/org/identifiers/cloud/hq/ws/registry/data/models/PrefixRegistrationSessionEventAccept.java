295118d23135316ee8ffd58d0fd0b5c3dea777f9
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-19 11:35:29 +0000

    prefix registration requests event model for acceptance, lombok annotations

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSessionEventAccept.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSessionEventAccept.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
 /**
  * Project: hq-registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -8,5 +12,8 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Data
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class PrefixRegistrationSessionEventAccept {
 }
