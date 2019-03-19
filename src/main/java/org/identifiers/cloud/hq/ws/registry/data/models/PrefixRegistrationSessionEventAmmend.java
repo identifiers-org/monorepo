824afe8d9619b0342f903498bdf95c71c340c996
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-19 11:46:25 +0000

    PrefixRegistrationSessionEventAmmend data model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSessionEventAmmend.java
@@ -0,0 +1,26 @@
+package org.identifiers.cloud.hq.ws.registry.data.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+
+/**
+ * Project: hq-registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.models
+ * Timestamp: 2019-03-19 11:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@Entity
+public class PrefixRegistrationSessionEventAmmend extends PrefixRegistrationSessionEvent {
+    public PrefixRegistrationSessionEventAmmend() {
+        super();
+        this.setEventName("AMENDMENT");
+    }
+}
