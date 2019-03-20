b358c7a9eb8a8b98acd0b194043479a209e495f1
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-20 11:02:12 +0000

    search by country code in locations

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/LocationRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/LocationRepository.java
@@ -13,4 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface LocationRepository extends JpaRepository<Location, String> {
     // TODO
+    Location findByCountryCode(String countryCode);
 }
