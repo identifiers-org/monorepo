105078af480847a895df2a2840f2c91787ad8814
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-20 13:52:11 +0000

    api data model helper, TODO on getPrefixRegistrationRequest()

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/helpers/ApiDataModelHelper.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/helpers/ApiDataModelHelper.java
@@ -1,5 +1,8 @@
 package org.identifiers.cloud.hq.ws.registry.models.helpers;
 
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixPayload;
+import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationRequest;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.helpers
@@ -11,4 +14,9 @@ package org.identifiers.cloud.hq.ws.registry.models.helpers;
  * This helper provides methods for transformations between api models and data models.
  */
 public class ApiDataModelHelper {
+
+    public static final PrefixRegistrationRequest getPrefixRegistrationRequest(ServiceRequestRegisterPrefixPayload sourceModel) {
+        // TODO
+        return null;
+    }
 }
