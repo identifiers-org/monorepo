529bef9582c020747b0e876296d99f87809170ec
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-20 13:49:29 +0000

    api data model helper

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/helpers/ApiDataModelHelper.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.models.helpers;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.helpers
+ * Timestamp: 2019-03-20 13:49
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiDataModelHelper {
+}
