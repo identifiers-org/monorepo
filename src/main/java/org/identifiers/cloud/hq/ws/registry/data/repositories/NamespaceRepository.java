7599c5c72b8c7472df49d53ad6399db79bf8e382
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-20 10:40:23 +0000

    namespace search method for start of a prefix

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/NamespaceRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/NamespaceRepository.java
@@ -3,6 +3,8 @@ package org.identifiers.cloud.hq.ws.registry.data.repositories;
 import org.identifiers.cloud.hq.ws.registry.data.models.Namespace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
@@ -14,4 +16,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface NamespaceRepository extends JpaRepository<Namespace, Long> {
     // TODO
     Namespace findByPrefix(String prefix);
+
+    List<Namespace> findAllByPrefixStartsWith(String prefixStart);
 }
