bf0e57c9c8a3a06f498cbee61f65c93d43f9e96f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-20 14:09:15 +0000

    amendRequest() TODO steps

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationRequestManagementServiceSimpleWorkflow.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationRequestManagementServiceSimpleWorkflow.java
@@ -23,6 +23,7 @@ public class PrefixRegistrationRequestManagementServiceSimpleWorkflow implements
         // TODO Open a new prefix registration session
         // TODO Set the given prefix registration request
         // TODO Create a 'start' event
+        // TODO Return the event
         return null;
     }
 
@@ -30,6 +31,11 @@ public class PrefixRegistrationRequestManagementServiceSimpleWorkflow implements
     public PrefixRegistrationSessionEvent amendRequest(PrefixRegistrationSession prefixRegistrationSession,
                                                        PrefixRegistrationRequest amendedRequest, String actor,
                                                        String additionalInformation) throws PrefixRegistrationRequestManagementServiceException {
+        // TODO Check the prefix registration session is open
+        // TODO Create the event
+        // TODO Persist the amended request
+        // TODO Reference the amended request in the newly created event
+        // TODO Update the prefix registration request referenced at session level
         return null;
     }
 
