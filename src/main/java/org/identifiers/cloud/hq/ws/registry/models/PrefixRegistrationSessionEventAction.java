4cfc8988dca072ad37a9e5e07712ca2ebc3af631
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-22 16:12:50 +0000

    PrefixRegistrationSessionEventAction placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionEventAction.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-22 16:12
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ *
+ */
+public interface PrefixRegistrationSessionEventAction {
+}
