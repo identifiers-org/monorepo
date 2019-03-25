822806c527a83c32d8022107ace64149ccb9995b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-25 13:02:44 +0000

    PrefixRegistrationSessionActionChainRunner, helper placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionChainRunner.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-25 13:02
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is a helper for running chains of actions upon prefix registration session completion.
+ */
+public class PrefixRegistrationSessionActionChainRunner {
+}
