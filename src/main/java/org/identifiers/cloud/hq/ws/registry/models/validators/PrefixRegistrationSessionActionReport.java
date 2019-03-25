ab1b9d3c35ec11a4bd712a1e820df7ec9446f061
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-25 11:07:22 +0000

    PrefixRegistrationSessionActionReport model

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationSessionActionReport.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationSessionActionReport.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.models.validators;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models.validators
@@ -10,5 +16,16 @@ package org.identifiers.cloud.hq.ws.registry.models.validators;
  *
  * This is a report built by any prefix registration session action after running.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class PrefixRegistrationSessionActionReport {
+    // Whether the action completed successfully or not
+    private boolean success = true;
+    // Error message that can be provided upon unsuccessful completion of action
+    private String errorMessage = "--- No error message set ---";
+    // Additional Information to action completion, optional
+    private String additionalInformation = "--- No additional information set ---";
 }
