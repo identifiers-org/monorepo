33f9912d9ff67b2c3c1ae1294586b97beddacab3
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-25 12:03:59 +0000

    PrefixRegistrationSessionActionAcceptance, buildActionSequence() builder

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionAcceptance.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionAcceptance.java
@@ -4,6 +4,8 @@ import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSessio
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models
@@ -17,6 +19,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Qualifier("PrefixRegistrationSessionActionAcceptance")
 public class PrefixRegistrationSessionActionAcceptance implements PrefixRegistrationSessionAction {
+
+
+    private List<PrefixRegistrationSessionAction> buildActionSequence() {
+        // TODO
+    }
+
     @Override
     public PrefixRegistrationSessionActionReport performAction(PrefixRegistrationSession session) throws PrefixRegistrationSessionActionException {
         return null;
