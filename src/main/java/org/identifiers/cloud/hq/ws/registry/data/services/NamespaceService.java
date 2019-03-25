7781de95608412a1a0201dd9677bbe708556d9aa
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-25 13:38:45 +0000

    namespace service, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/NamespaceService.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-25 13:37
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is service that implements complex persistence operations for Namespace objects
+ */
+public class NamespaceService {
+    // TODO
+}
