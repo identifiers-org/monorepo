ce45585153997b7828dbb29c3c8f6066a34576ec
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-25 15:56:08 +0000

    algorithm skeleton for the namespace service, registerNewNamespace()

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/NamespaceService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/NamespaceService.java
@@ -1,5 +1,6 @@
 package org.identifiers.cloud.hq.ws.registry.data.services;
 
+import org.identifiers.cloud.hq.ws.registry.data.models.Namespace;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,4 +16,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class NamespaceService {
     // TODO
+    public void registerNewNamespace(Namespace namespace) throws NamespaceServiceException {
+        // TODO
+        // TODO Check that you're not trying to register an already existing namespace
+        // TODO Check if the person needs to be created or not
+        // TODO Get a MIR ID for the new namespace (libapi?)
+        // TODO Persist the new namespace
+    }
 }
