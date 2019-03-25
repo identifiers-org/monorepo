87dc027542d147840e95cdbe146d659d6263490b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-25 12:01:02 +0000

    PrefixRegistrationSessionActionAcceptance, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionAcceptance.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSession;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-25 12:00
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationSessionActionAcceptance implements PrefixRegistrationSessionAction {
+    @Override
+    public PrefixRegistrationSessionActionReport performAction(PrefixRegistrationSession session) throws PrefixRegistrationSessionActionException {
+        return null;
+    }
+}
