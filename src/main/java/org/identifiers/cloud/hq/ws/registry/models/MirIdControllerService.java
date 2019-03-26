f241016b6fa7890caf4a6688f6b855c18856babb
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 13:22:57 +0000

    MirIdControllerService interface, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdControllerService.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-26 13:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This interface specifies the contract a delegate for MIR ID Services should fullfil
+ */
+public interface MirIdControllerService {
+}
