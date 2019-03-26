a5c1fbdc48a9182f06670c5d6c80cf2a51fdfe96
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 14:34:04 +0000

    MirIdServiceWsClient, re-try configuration

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClient.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClient.java
@@ -11,6 +11,9 @@ package org.identifiers.cloud.hq.ws.registry.models;
  * This implementation of MIR ID Service delegates the operations on identifiers.org MIR ID Controller API.
  */
 public class MirIdServiceWsClient implements MirIdService {
+    private static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
+    private static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
+
     @Override
     public String mintId() throws MirIdServiceException {
         return null;
