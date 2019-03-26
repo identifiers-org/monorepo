68b5921859f271b991eecbf014a46aed16f4d47e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 12:26:38 +0000

    configuration model for development, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/ApplicationConfigurationDevelopment.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.configuration;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.configuration
+ * Timestamp: 2019-03-26 12:25
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApplicationConfigurationDevelopment {
+}
