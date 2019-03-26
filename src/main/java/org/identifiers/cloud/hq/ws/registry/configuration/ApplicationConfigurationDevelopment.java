b849ecfba686036540e8533678580c8b0fb5c059
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 12:27:49 +0000

    configuration model for development, cors configurer

--- src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/ApplicationConfigurationDevelopment.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/ApplicationConfigurationDevelopment.java
@@ -1,7 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Project: registry
@@ -16,4 +20,14 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile({"development", "standalone"})
 public class ApplicationConfigurationDevelopment {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
