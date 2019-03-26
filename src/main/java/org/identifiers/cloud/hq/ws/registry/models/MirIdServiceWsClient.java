c37e4e5c4bc70a17f82cfe22410b2df1d133fe0d
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 14:48:08 +0000

    POM file, spring-retry dependency added

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClient.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClient.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.hq.ws.registry.models;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models
@@ -10,10 +12,26 @@ package org.identifiers.cloud.hq.ws.registry.models;
  *
  * This implementation of MIR ID Service delegates the operations on identifiers.org MIR ID Controller API.
  */
+@Component
 public class MirIdServiceWsClient implements MirIdService {
     private static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     private static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
 
+    // Helpers
+    public static RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(WS_REQUEST_RETRY_MAX_ATTEMPTS);
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(WS_REQUEST_RETRY_BACK_OFF_PERIOD);
+
+        retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+        return retryTemplate;
+    }
+    // END - Helpers
     @Override
     public String mintId() throws MirIdServiceException {
         return null;
