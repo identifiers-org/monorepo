5c2757ee80b92c43fdfd4b8b1fb6796048027ac6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 16:03:13 +0000

    mir id service client, mintId() implementation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClient.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClient.java
@@ -5,6 +5,12 @@ import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models
@@ -19,6 +25,8 @@ import org.springframework.stereotype.Component;
 public class MirIdServiceWsClient implements MirIdService {
     private static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     private static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
+    private static final int WS_REQUEST_CONNECT_TIMEOUT = 1000; // 1 second
+    private static final int WS_REQUEST_READ_TIMEOUT = 1000; // 1 second
 
     @Value("${org.identifiers.cloud.hq.ws.registry.backend.service.miridcontroller.host}")
     private String wsMirIdControllerHost;
@@ -29,7 +37,7 @@ public class MirIdServiceWsClient implements MirIdService {
     private String getMirIdServiceBaseUrl() {
         // We should allow HTTP / HTTPS configurability in case we want the cluster internal traffic to be encrypted,
         // otherwise this is fine, as all HQ services are deployed within the same cluster
-        return String.format("http://%s:%s/", wsMirIdControllerHost, wsMirIdControllerPort);
+        return String.format("http://%s:%s/mirIdApi", wsMirIdControllerHost, wsMirIdControllerPort);
     }
     // END - Helpers
 
@@ -38,7 +46,38 @@ public class MirIdServiceWsClient implements MirIdService {
     @Override
     public String mintId() throws MirIdServiceException {
         // TODO
-        return null;
+        int status = 0;
+        String mirId = null;
+        HttpURLConnection connection = null;
+        try {
+            URL requestUrl = new URL(String.format("%s/mintId", getMirIdServiceBaseUrl()));
+            connection = (HttpURLConnection) requestUrl.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(false);
+            status = connection.getResponseCode();
+            if (status == 200) {
+                // Whaaaaat!?!? Thank you Baeldung! https://www.baeldung.com/java-http-request
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                mirId = content.toString();
+            }
+        } catch (RuntimeException | IOException e) {
+            throw new MirIdServiceException(e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        if (status != 200) {
+            throw new MirIdServiceException(String.format("MIR ID minting FAILED, status code '%d'", status));
+        }
+        return mirId;
     }
 
     @Retryable(maxAttempts = WS_REQUEST_RETRY_MAX_ATTEMPTS,
