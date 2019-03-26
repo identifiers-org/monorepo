5c16553a18e2971eb4f9ef71cf857ae9c0a0898e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 16:05:23 +0000

    MirIdServiceWsClientTest placeholder

---
+++ src/test/java/org/identifiers/cloud/hq/ws/registry/models/MirIdServiceWsClientTest.java
@@ -0,0 +1,22 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-26 16:04
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@SpringBootTest
+public class MirIdServiceWsClientTest {
+
+    @Test
+    public void mintId() {
+    }
+}
