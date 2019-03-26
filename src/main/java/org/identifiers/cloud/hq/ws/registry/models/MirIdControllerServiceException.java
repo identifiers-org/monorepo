0765b00eaf85d0a223c3d5a52c11eff058ce7259
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 13:38:44 +0000

    MirIdControllerServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/MirIdControllerServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-26 13:35
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MirIdControllerServiceException extends RuntimeException {
+    public MirIdControllerServiceException(String message) {
+        super(message);
+    }
+}
