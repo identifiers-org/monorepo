41a928ac53a62f4f93b6750ab0f091b04839bdad
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-27 15:38:20 +0000

    institution service exception

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/InstitutionServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-27 15:38
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class InstitutionServiceException extends RuntimeException {
+    public InstitutionServiceException(String message) {
+        super(message);
+    }
+}
