f747d4bb6e20e08c52e639fe613519219092d2aa
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-27 15:37:45 +0000

    person service exception

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/PersonServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-27 15:37
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PersonServiceException extends RuntimeException {
+    public PersonServiceException(String message) {
+        super(message);
+    }
+}
