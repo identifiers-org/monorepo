c7f02066bee6851cc54d45435bd439218ccc97b1
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-27 15:29:46 +0000

    ResourceServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/ResourceServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-27 15:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceServiceException extends RuntimeException {
+    public ResourceServiceException(String message) {
+        super(message);
+    }
+}
