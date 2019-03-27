4d67a3815770b1b97fe5f61540c8e57550f3fd2f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-27 14:55:24 +0000

    completed TODO

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/NamespaceService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/NamespaceService.java
@@ -68,7 +68,6 @@ public class NamespaceService {
                     namespace.getPrefix(),
                     namespace.getMirId());
         } catch (MirIdServiceException e) {
-            // TODO
             throw new NamespaceServiceException(String.format("REGISTERING NAMESPACE '%s', " +
                     "MIR ID minting resulted in the following error: '%s'",
                     namespace.getPrefix(),
