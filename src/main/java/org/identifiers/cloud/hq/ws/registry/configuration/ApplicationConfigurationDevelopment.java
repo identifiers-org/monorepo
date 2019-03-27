8cf17b48e702ad255436fd3c0bfb94984aee4cfc
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-27 22:24:28 +0000

    updated spring boot dependency, and not working CORS mess

--- src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/ApplicationConfigurationDevelopment.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/ApplicationConfigurationDevelopment.java
@@ -1,11 +1,11 @@
 package org.identifiers.cloud.hq.ws.registry.configuration;
 
-import org.springframework.context.annotation.Bean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Project: registry
@@ -18,16 +18,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * This is the configuration active for development
  */
 @Configuration
+@EnableWebMvc
 @Profile({"development", "standalone"})
-public class ApplicationConfigurationDevelopment {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*");
-            }
-        };
+@Slf4j
+public class ApplicationConfigurationDevelopment implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("CORS configuration for DEVELOPMENT");
+        registry.addMapping("/restApi/**").allowedMethods("*");
+
+        // Add more mappings...
     }
 }
