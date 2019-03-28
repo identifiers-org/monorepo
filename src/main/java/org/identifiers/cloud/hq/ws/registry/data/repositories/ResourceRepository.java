59a31b2609a2a1ab05115cf6ff3d37eb578f491e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 12:37:51 +0000

    resource repository findByNamespaceIdAAndProviderCode()

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRepository.java
@@ -17,5 +17,7 @@ public interface ResourceRepository extends JpaRepository<Resource, Long> {
     // TODO
     List<Resource> findAllByNamespaceId(long id);
 
+    List<Resource> findByNamespaceIdAAndProviderCode(long namespaceId, String providerCode);
+
     List<Resource> findByProviderCode(String providerCode);
 }
