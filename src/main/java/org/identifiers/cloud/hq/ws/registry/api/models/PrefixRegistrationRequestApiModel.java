c14e4d6b793ce188bb644cf65431dc80ad28ccd5
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 15:30:04 +0000

    prefix registration api model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/PrefixRegistrationRequestApiModel.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2019-03-28 15:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class PrefixRegistrationRequestApiModel {
+    // TODO
+}
