0e7e1eed9f53f6c2ef7b9939e6a539dd086f1edd
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 10:42:22 +0000

    institution service placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/InstitutionService.java
@@ -0,0 +1,20 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-28 10:41
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ * <p>
+ * This Service implements complex persistence logic for Institutions.
+ */
+@Service
+@Slf4j
+public class InstitutionService {
+    // TODO
+}
