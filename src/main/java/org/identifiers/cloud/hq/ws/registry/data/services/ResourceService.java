a7783f078fe8eb52eb1dd841e5bd155f611617f9
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 10:41:25 +0000

    resource service, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/ResourceService.java
@@ -0,0 +1,20 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-28 10:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This Service implements complex persistence logic for resources.
+ */
+@Service
+@Slf4j
+public class ResourceService {
+    // TODO
+}
