a1e184090cb9d253dcc6ae108554a0ed483e15fd
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 15:27:47 +0000

    prefix registration request API controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/PrefixRegistrationRequestRegistrationApiController.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2019-03-28 15:26
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This controller supports the API that handles prefix registration requests
+ */
+public class PrefixRegistrationRequestRegistrationApiController {
+    // TODO
+}
