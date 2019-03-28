7ef22d6cc8829f1116a4337883d16ff367b76364
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 13:47:54 +0000

    DataModelConversionHelper, placeholder and description

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/DataModelConversionHelper.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-03-28 13:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This helper implements methods for converting between different data models
+ */
+public class DataModelConversionHelper {
+    // TODO
+}
