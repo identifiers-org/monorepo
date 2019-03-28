c5ef360e82664d643083be314cddc7ba659e7b6b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 10:57:02 +0000

    location service, heading

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/LocationService.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-28 10:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This class implements complex persistence logic for Location.
+ */
+public class LocationService {
+    // TODO
+}
