b08d0215e254eb9f85cc4ddbcc7c3431c5a17488
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-28 10:56:23 +0000

    LocationServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/LocationServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.data.services;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.services
+ * Timestamp: 2019-03-28 10:56
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LocationServiceException extends RuntimeException {
+    public LocationServiceException(String message) {
+        super(message);
+    }
+}
