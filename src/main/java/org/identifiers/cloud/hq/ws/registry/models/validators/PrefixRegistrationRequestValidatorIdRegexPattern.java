29f3b20c8005641e4cc080bc9e2588d6b3580b91
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-29 12:31:21 +0000

    TODO on refactoring and refining

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorIdRegexPattern.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorIdRegexPattern.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Component;
 public class PrefixRegistrationRequestValidatorIdRegexPattern implements PrefixRegistrationRequestValidator {
     @Override
     public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
+        // TODO In future iterations, use a different mechanism for reporting back why this is not valid, and leave exceptions for non-recoverable conditions
         if (request.getIdRegexPattern() == null) {
             throw new PrefixRegistrationRequestValidatorException("MISSING REQUIRED ID Regex Pattern");
         }
