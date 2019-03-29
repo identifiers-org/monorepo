6104e30822fc5eec28eb5a302ba4caec926e3b68
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-29 11:08:08 +0000

    prefix registration api model, delegation skeleton

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/PrefixRegistrationRequestApiModel.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/PrefixRegistrationRequestApiModel.java
@@ -1,5 +1,9 @@
 package org.identifiers.cloud.hq.ws.registry.api.models;
 
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefix;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixSessionEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,4 +18,32 @@ import org.springframework.stereotype.Component;
 public class PrefixRegistrationRequestApiModel {
     // TODO
 
+    public ResponseEntity<?> registerPrefix(ServiceRequestRegisterPrefix request) {
+        // TODO 
+        return new ResponseEntity<>("registerPrefix()", HttpStatus.OK);
+    }
+
+    // TODO - Amend prefix registration request
+    public ResponseEntity<?> amendPrefixRegistrationRequest(long sessionId, ServiceRequestRegisterPrefixSessionEvent request) {
+        // TODO
+        return new ResponseEntity<>("amendPrefixRegistrationRequest()", HttpStatus.OK);
+    }
+
+    // TODO - Comment on prefix registration request
+    public ResponseEntity<?> commentPrefixRegistrationRequest(long sessionId, ServiceRequestRegisterPrefixSessionEvent request) {
+        // TODO
+        return new ResponseEntity<>("commentPrefixRegistrationRequest()", HttpStatus.OK);
+    }
+
+    // TODO - Reject prefix registration request
+    public ResponseEntity<?> rejectPrefixRegistrationRequest(long sessionId, ServiceRequestRegisterPrefixSessionEvent request) {
+        // TODO
+        return new ResponseEntity<>("rejectPrefixRegistrationRequest()", HttpStatus.OK);
+    }
+
+    // TODO - Accept prefix registration request
+    public ResponseEntity<?> acceptPrefixRegistrationRequest(long sessionId, ServiceRequestRegisterPrefixSessionEvent request) {
+        // TODO
+        return new ResponseEntity<>("acceptPrefixRegistrationRequest()", HttpStatus.OK);
+    }
 }
