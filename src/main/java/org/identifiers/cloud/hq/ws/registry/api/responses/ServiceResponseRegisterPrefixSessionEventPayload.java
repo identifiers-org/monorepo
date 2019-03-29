89e10c1986e1dd4fe04361f6fc7d4cc48c039ee0
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-29 11:31:36 +0000

    ServiceResponseRegisterPrefixSessionEventPayload, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseRegisterPrefixSessionEventPayload.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+import java.io.Serializable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2019-03-29 11:30
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseRegisterPrefixSessionEventPayload implements Serializable {
+    // TODO
+}
