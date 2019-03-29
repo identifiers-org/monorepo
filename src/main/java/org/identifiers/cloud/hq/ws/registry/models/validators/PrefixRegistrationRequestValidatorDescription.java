86bad7082e75e2f21b6f5262acf5a0602b1ed64a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-29 12:31:03 +0000

    TODO on refactoring and refining

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorDescription.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorDescription.java
@@ -22,6 +22,7 @@ public class PrefixRegistrationRequestValidatorDescription implements PrefixRegi
     @Override
     public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
         if ((request.getDescription() == null) || (request.getDescription().length() < DESCRIPTION_CONTENT_MIN_CHARS)) {
+            // TODO In future iterations, use a different mechanism for reporting back why this is not valid, and leave exceptions for non-recoverable conditions
             throw new PrefixRegistrationRequestValidatorException(String.format("Prefix request description must be longer than %d characters", DESCRIPTION_CONTENT_MIN_CHARS));
         }
         return true;
