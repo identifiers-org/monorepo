a7b0e5cf068fd5d9b2a800c4f9d4e0242d96b21d
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-29 10:35:19 +0000

    ServiceRequestRegisterPrefixSessionEventPayload, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterPrefixSessionEventPayload.java
@@ -0,0 +1,28 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-03-29 10:34
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ServiceRequestRegisterPrefixSessionEventPayload implements Serializable {
+    // TODO
+}
