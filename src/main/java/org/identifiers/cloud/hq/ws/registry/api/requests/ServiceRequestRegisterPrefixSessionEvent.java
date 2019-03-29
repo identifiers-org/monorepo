f45d0320fde794c3ac191dcaf058894e5ddfb6e0
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-29 10:43:18 +0000

    ServiceRequestRegisterPrefixSessionEvent

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterPrefixSessionEvent.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-03-29 10:42
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestRegisterPrefixSessionEvent extends ServiceRequest<ServiceRequestRegisterPrefixSessionEventPayload> {
+}
