3359599df9232aef105ab8198e09814566f81989
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-01 15:39:19 +0100

    institution data model for api domain, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Institution.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models
+ * Timestamp: 2019-04-01 15:38
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * API domain data model for institution
+ */
+public class Institution {
+}
