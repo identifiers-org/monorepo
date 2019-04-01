4b90655e0b3733792be32951d46626cac3836800
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-01 15:48:09 +0100

    institution api domain data model, completed

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Institution.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Institution.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
 /**
@@ -12,6 +17,10 @@ import java.io.Serializable;
  *
  * API domain data model for institution
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class Institution implements Serializable {
     // identifiers.org internal ID for this institution
     private long id;
