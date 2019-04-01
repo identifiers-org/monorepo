63cf4bcd7e69e34dca92f979b34f82139f314ab6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-01 15:30:52 +0100

    location data model at API level

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Location.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models
+ * Timestamp: 2019-04-01 15:30
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is the location data model at API level
+ */
+public class Location {
+    // TODO
+}
