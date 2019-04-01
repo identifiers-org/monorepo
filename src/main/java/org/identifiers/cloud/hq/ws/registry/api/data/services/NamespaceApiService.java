9571011d0a6eedd26d2d901e6d0eb1b8928faea2
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-01 16:12:17 +0100

    namespace api service, namespace builder steps

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/services/NamespaceApiService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/services/NamespaceApiService.java
@@ -1,11 +1,13 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.services;
 
 import org.identifiers.cloud.hq.ws.registry.api.data.models.Namespace;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.Resource;
 import org.identifiers.cloud.hq.ws.registry.data.repositories.NamespaceRepository;
 import org.identifiers.cloud.hq.ws.registry.data.repositories.ResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,7 +34,18 @@ public class NamespaceApiService {
     public List<Namespace> getNamespaceTreeDownToLeaves() {
         return namespaceRepository.findAll().parallelStream().map(namespace -> {
             // TODO
-            return new Namespace();
+            // TODO - Locate the resources within the namespace
+            List<Resource> resources = new ArrayList<>();
+            return new Namespace()
+                    .setId(namespace.getId())
+                    .setPrefix(namespace.getPrefix())
+                    .setName(namespace.getName())
+                    .setPattern(namespace.getPattern())
+                    .setDescription(namespace.getDescription())
+                    .setCreated(namespace.getCreated())
+                    .setModified(namespace.getModified())
+                    .setSampleId(namespace.getSampleId())
+                    .setResources(resources);
         }).collect(Collectors.toList());
     }
 }
