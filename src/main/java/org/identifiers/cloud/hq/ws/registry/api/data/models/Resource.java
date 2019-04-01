1f82fc53940083d632255b789fd79239d5dc1b3c
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-01 15:47:23 +0100

    resource api domain data model, completed

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/Resource.java
@@ -1,7 +1,6 @@
 package org.identifiers.cloud.hq.ws.registry.api.data.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
@@ -18,20 +17,27 @@ import java.io.Serializable;
  *
  * This class models how the microservice exposes information about resources in the registry through its Resolution API.
  */
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 @Accessors(chain = true)
 public class Resource implements Serializable {
+    // identifiers.org internal ID for this resource
     private long id;
     private String mirId;
-    private String accessUrl;
-    private String info;
+    private String urlPattern;
+    // This is known as 'info' in the old data model from the EBI platform
+    private String name;
+    // Now you can optionally provide a description
+    private String description;
     private boolean official;
+    // We should require every provider to have a code
     private String providerCode;
+    // This is a sample ID at provider level
     private String sampleId;
+    // This is a home URL for this resource within the context of the namespace it belongs to
     private String resourceHomeUrl;
-    private String institution;
-    private String location;
+    private Institution institution;
+    private Location location;
+
 }
