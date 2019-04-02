3a0c7178b2bccd2047b2c02fa5bb9e3c59affc31
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 13:53:32 +0100

    model description

--- src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/HqServiceResponseGetResolverDataset.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/HqServiceResponseGetResolverDataset.java
@@ -7,6 +7,8 @@ package org.identifiers.cloud.ws.resolver.daemons.models;
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
+ *
+ * This data model represents the response that comes from the HQ Registry API when queried about the resolver dataset
  */
 public class HqServiceResponseGetResolverDataset extends ServiceResponse<ResolverDatasetPayload> {
 }
