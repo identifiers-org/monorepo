4e0a3b0077d6ec58b6e668633c1dd042e0757d86
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 13:34:04 +0100

    ResolverDatasetPayload model

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ResolverDatasetPayload.java
@@ -0,0 +1,30 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.identifiers.cloud.ws.resolver.data.models.Namespace;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2019-04-02 13:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResolverDatasetPayload implements Serializable {
+    private List<Namespace> namespaces;
+}
