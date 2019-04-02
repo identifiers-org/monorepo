951a80ba15d9289336b019477ccec6d689605234
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 13:35:09 +0100

    HqServiceResponseGetResolverDataset model

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/HqServiceResponseGetResolverDataset.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2019-04-02 13:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class HqServiceResponseGetResolverDataset extends ServiceResponse<ResolverDatasetPayload> {
+}
