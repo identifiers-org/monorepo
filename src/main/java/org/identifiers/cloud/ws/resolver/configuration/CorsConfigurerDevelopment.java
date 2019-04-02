a51f10d6b61fdcd5c22b49123cacf14941bab5c0
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 13:26:10 +0100

    CORS configurer for development

--- src/main/java/org/identifiers/cloud/ws/resolver/configuration/CorsConfigurerDevelopment.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/configuration/CorsConfigurerDevelopment.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 // This iteration is using the same profile for 'development' and 'thirdparty' profiles, as they are not different yet.
 @Profile({"development", "thirdparty"})
-public class DevelopmentConfiguration {
+public class CorsConfigurerDevelopment {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
