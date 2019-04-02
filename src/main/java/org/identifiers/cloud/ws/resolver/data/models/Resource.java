abff72281d136bcaf1da32e2ff4e1061f799ea91
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 11:30:05 +0100

    refactoring data models, resource

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/data/models/Resource.java
@@ -0,0 +1,43 @@
+package org.identifiers.cloud.ws.resolver.data.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.data.models
+ * Timestamp: 2019-04-02 11:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+public class Resource implements Serializable {
+    // identifiers.org internal ID for this resource
+    private long id;
+    private String mirId;
+    private String urlPattern;
+    // This is known as 'info' in the old data model from the EBI platform
+    private String name;
+    // Now you can optionally provide a description
+    private String description;
+    private boolean official;
+    // We should require every provider to have a code
+    private String providerCode;
+    // This is a sample ID at provider level
+    private String sampleId;
+    // This is a home URL for this resource within the context of the namespace it belongs to
+    private String resourceHomeUrl;
+    private Institution institution;
+    private Location location;
+
+}
