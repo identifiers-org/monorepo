52c80b524c52a5c1b4ed18ea25e15813eec7cfd7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 13:34:21 +0100

    ServiceResponse model

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/daemons/models/ServiceResponse.java
@@ -0,0 +1,30 @@
+package org.identifiers.cloud.ws.resolver.daemons.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.daemons.models
+ * Timestamp: 2019-04-02 13:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ServiceResponse<T> implements Serializable {
+    private String apiVersion;
+    private String errorMessage;
+    private T payload;
+}
