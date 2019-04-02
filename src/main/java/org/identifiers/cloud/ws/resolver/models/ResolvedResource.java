0dcd4587377d607692773b5fda44fcef8b091787
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 11:42:19 +0100

    ResolvedResource lombok-ization

--- src/main/java/org/identifiers/cloud/ws/resolver/models/ResolvedResource.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/models/ResolvedResource.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.ws.resolver.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
 /**
@@ -9,6 +15,11 @@ import java.io.Serializable;
  * Timestamp: 2018-03-07 7:28
  * ---
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 public class ResolvedResource implements Serializable {
     private String id;
     private String resourcePrefix;
@@ -19,85 +30,4 @@ public class ResolvedResource implements Serializable {
     private boolean official;
     private String resourceURL;
     private Recommendation recommendation;
-
-    public String getId() {
-        return id;
-    }
-
-    public ResolvedResource setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getResourcePrefix() {
-        return resourcePrefix;
-    }
-
-    public ResolvedResource setResourcePrefix(String resourcePrefix) {
-        this.resourcePrefix = resourcePrefix;
-        return this;
-    }
-
-    public String getAccessUrl() {
-        return accessUrl;
-    }
-
-    public ResolvedResource setAccessUrl(String accessUrl) {
-        this.accessUrl = accessUrl;
-        return this;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public ResolvedResource setInfo(String info) {
-        this.info = info;
-        return this;
-    }
-
-    public String getInstitution() {
-        return institution;
-    }
-
-    public ResolvedResource setInstitution(String institution) {
-        this.institution = institution;
-        return this;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public ResolvedResource setLocation(String location) {
-        this.location = location;
-        return this;
-    }
-
-    public boolean isOfficial() {
-        return official;
-    }
-
-    public ResolvedResource setOfficial(boolean official) {
-        this.official = official;
-        return this;
-    }
-
-    public String getResourceURL() {
-        return resourceURL;
-    }
-
-    public ResolvedResource setResourceURL(String resourceURL) {
-        this.resourceURL = resourceURL;
-        return this;
-    }
-
-    public Recommendation getRecommendation() {
-        return recommendation;
-    }
-
-    public ResolvedResource setRecommendation(Recommendation recommendation) {
-        this.recommendation = recommendation;
-        return this;
-    }
 }
