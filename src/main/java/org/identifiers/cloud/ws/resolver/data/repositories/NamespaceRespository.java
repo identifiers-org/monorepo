98c83e68b37014d7255547127c0ba3c5a1802e8b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-02 11:33:20 +0100

    namespace repository

---
+++ src/main/java/org/identifiers/cloud/ws/resolver/data/repositories/NamespaceRespository.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.ws.resolver.data.repositories;
+
+/**
+ * Project: resolver
+ * Package: org.identifiers.cloud.ws.resolver.data.repositories
+ * Timestamp: 2019-04-02 11:33
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface NamespaceRespository {
+    // TODO
+}
