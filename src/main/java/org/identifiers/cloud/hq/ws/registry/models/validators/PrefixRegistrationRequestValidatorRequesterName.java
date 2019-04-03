3cf9a30871d86f6083b7808a620eff74d2dcbe26
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-03 14:03:35 +0100

    PrefixRegistrationRequestValidatorRequesterName validator

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorRequesterName.java
@@ -0,0 +1,29 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterPrefixPayload;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-04-03 13:54
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+@Scope("prototype")
+@Qualifier("PrefixRegistrationRequestValidatorRequesterName")
+public class PrefixRegistrationRequestValidatorRequesterName implements PrefixRegistrationRequestValidator {
+    @Override
+    public boolean validate(ServiceRequestRegisterPrefixPayload request) throws PrefixRegistrationRequestValidatorException {
+        try {
+            return RequesterValidatorFactory.getNameValidator().validate(request.getRequester());
+        } catch (RequesterValidatorException e) {
+            throw new PrefixRegistrationRequestValidatorException(e.getMessage());
+        }
+        return false;
+    }
+}
