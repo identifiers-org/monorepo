fdf579e99e1f462983c069966c379520f0c31ffa
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-10 10:28:18 +0100

    findByPrefixRegistrationSessionId() for prefix registration session events repository

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/PrefixRegistrationSessionEventRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/PrefixRegistrationSessionEventRepository.java
@@ -3,6 +3,8 @@ package org.identifiers.cloud.hq.ws.registry.data.repositories;
 import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSessionEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Project: hq-registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
@@ -13,4 +15,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface PrefixRegistrationSessionEventRepository extends JpaRepository<PrefixRegistrationSessionEvent, Long> {
     // TODO
+    List<PrefixRegistrationSessionEvent> findByPrefixRegistrationSessionId(long id);
 }
