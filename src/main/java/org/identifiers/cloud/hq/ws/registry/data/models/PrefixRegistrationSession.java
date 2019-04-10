eda12e9af3844a05ba61833c4f36c67207192a70
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-10 10:48:30 +0100

    prefix registration session index on 'closed'

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSession.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/PrefixRegistrationSession.java
@@ -30,6 +30,7 @@ import java.util.Date;
 @Accessors(chain = true)
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {@Index(name = "idx_closed", columnList = "closed")})
 public class PrefixRegistrationSession {
     @Id
     @GeneratedValue
