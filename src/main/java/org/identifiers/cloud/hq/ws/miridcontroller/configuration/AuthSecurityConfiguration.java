91961763676902cefecfbb7ead729322529e5eb2
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-24 12:34:34 +0100

    AuthSecurityConfiguration, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/configuration/AuthSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.configuration
+ * Timestamp: 2019-04-24 12:34
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class AuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // TODO
+}
