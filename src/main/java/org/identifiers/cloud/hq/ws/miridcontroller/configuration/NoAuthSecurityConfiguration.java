9e5f88c135dbb4415a6a3846764ea9c88ca4ae62
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-24 11:09:06 +0100

    no auth configuration class, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/configuration/NoAuthSecurityConfiguration.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.miridcontroller.configuration;
+
+/**
+ * Project: mirid-controller
+ * Package: org.identifiers.cloud.hq.ws.miridcontroller.configuration
+ * Timestamp: 2019-04-24 11:06
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class NoAuthSecurityConfiguration {
+}
