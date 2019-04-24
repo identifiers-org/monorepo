2dc09cbcf0c1f224759b11a38e03a5df6238cab6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-24 11:09:43 +0100

    no auth configuration class, annotations

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/configuration/NoAuthSecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/configuration/NoAuthSecurityConfiguration.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.hq.ws.miridcontroller.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * Project: mirid-controller
  * Package: org.identifiers.cloud.hq.ws.miridcontroller.configuration
@@ -8,5 +14,10 @@ package org.identifiers.cloud.hq.ws.miridcontroller.configuration;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-public class NoAuthSecurityConfiguration {
+@Configuration
+@Slf4j
+@Profile("noauthprofile")
+@EnableWebSecurity
+public class NoAuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // TODO
 }
