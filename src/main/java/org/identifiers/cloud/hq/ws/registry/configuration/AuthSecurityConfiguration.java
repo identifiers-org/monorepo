2c16d425e5cea055e60c36544ee55a5e193dd3af
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-25 09:54:36 +0100

    initial lock down for all requests

--- src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/AuthSecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/AuthSecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -32,5 +33,12 @@ public class AuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
         log.info("[CONFIG] (AAA) ENABLED");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().denyAll()
+                .and()
+                .oauth2ResourceServer().jwt();
+    }
     // TODO
 }
