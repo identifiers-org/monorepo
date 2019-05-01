eb2317e205d27bd7c0236bc5635479ce2ab92146
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-01 10:46:22 +0100

    POM file, add re-try pattern dependency

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
@@ -9,7 +9,6 @@ import org.identifiers.cloud.hq.ws.miridcontroller.data.repositories.MirIdDeacti
 import org.identifiers.cloud.hq.ws.miridcontroller.data.repositories.ReturnedMirIdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Isolation;
 
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -33,7 +32,7 @@ public class DbBackedMirIdManagementStrategy implements MirIdManagementStrategy 
     @Autowired
     private ReturnedMirIdRepository returnedMirIdRepository;
 
-    @org.springframework.transaction.annotation.Transactional(isolation = Isolation.SERIALIZABLE)
+    @Transactional
     @Override
     public long mintId() throws MirIdManagementStrategyException {
         // TODO THIS BIT IS FAILING TO BE CONCURRENCY SAFE - A SOLUTION NEEDS TO BE PUT IN PLACE URGENTLY
