f622b0fa723628a376cfd87ba049dfd7502bddc4
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-01 10:29:50 +0100

    trying to serialize this operation

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
@@ -9,6 +9,7 @@ import org.identifiers.cloud.hq.ws.miridcontroller.data.repositories.MirIdDeacti
 import org.identifiers.cloud.hq.ws.miridcontroller.data.repositories.ReturnedMirIdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
 
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -32,7 +33,7 @@ public class DbBackedMirIdManagementStrategy implements MirIdManagementStrategy 
     @Autowired
     private ReturnedMirIdRepository returnedMirIdRepository;
 
-    @Transactional
+    @org.springframework.transaction.annotation.Transactional(isolation = Isolation.SERIALIZABLE)
     @Override
     public long mintId() throws MirIdManagementStrategyException {
         // TODO THIS BIT IS FAILING TO BE CONCURRENCY SAFE - A SOLUTION NEEDS TO BE PUT IN PLACE URGENTLY
