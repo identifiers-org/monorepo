5a59988786cc008c9001b9ea01443b238bf29152
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-01 15:26:48 +0100

    lock configuration for operation 'returnId'

--- src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/miridcontroller/models/DbBackedMirIdManagementStrategy.java
@@ -39,6 +39,9 @@ public class DbBackedMirIdManagementStrategy implements MirIdManagementStrategy 
     private static final String CONCURRENCY_LOCK_OPERATION_LOAD_ID = "DbBackedMirIdManagementStrategy_concurrency_lock_operation_load_id";
     private static final int CONCURRENCY_LOCK_OPERATION_LOAD_ID_TIME_SECONDS_LEASE_TIME = 3;
     private static final int CONCURRENCY_LOCK_OPERATION_LOAD_ID_TIME_SECONDS_WAIT_FOR_LOCK_TIME = 6;
+    private static final String CONCURRENCY_LOCK_OPERATION_RETURN_ID = "DbBackedMirIdManagementStrategy_concurrency_lock_operation_return_id";
+    private static final int CONCURRENCY_LOCK_OPERATION_RETURN_ID_TIME_SECONDS_LEASE_TIME = 3;
+    private static final int CONCURRENCY_LOCK_OPERATION_RETURN_ID_TIME_SECONDS_WAIT_FOR_LOCK_TIME = 6;
 
     // Repositories
     @Autowired
