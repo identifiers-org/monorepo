cb520ceddade889a419e52dbbb942071378f6f58
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-13 19:15:06 +0100

    CSRF configuration for validation api in prefix registration requests

--- src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/AuthSecurityConfiguration.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/configuration/AuthSecurityConfiguration.java
@@ -172,12 +172,12 @@ public class AuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .antMatchers("/prefixRegistrationApi/commentPrefixRegistrationRequest/**").access(String.format("isAuthenticated() and (principal?.claims.get('%s') != null) and (principal?.claims['%s'].get('%s') != null) and (principal?.claims['%s']['%s']['roles'].contains('ApiPrefixRegistrationCommentPrefixRegistrationRequest'))", JWT_SCOPE_RESOURCE_ACCESS, JWT_SCOPE_RESOURCE_ACCESS, clientId, JWT_SCOPE_RESOURCE_ACCESS, clientId))
                     .antMatchers("/prefixRegistrationApi/rejectPrefixRegistrationRequest/**").access(String.format("isAuthenticated() and (principal?.claims.get('%s') != null) and (principal?.claims['%s'].get('%s') != null) and (principal?.claims['%s']['%s']['roles'].contains('ApiPrefixRegistrationRejectPrefixRegistrationRequest'))", JWT_SCOPE_RESOURCE_ACCESS, JWT_SCOPE_RESOURCE_ACCESS, clientId, JWT_SCOPE_RESOURCE_ACCESS, clientId))
                     .antMatchers("/prefixRegistrationApi/acceptPrefixRegistrationRequest/**").access(String.format("isAuthenticated() and (principal?.claims.get('%s') != null) and (principal?.claims['%s'].get('%s') != null) and (principal?.claims['%s']['%s']['roles'].contains('ApiPrefixRegistrationAcceptPrefixRegistrationRequest'))", JWT_SCOPE_RESOURCE_ACCESS, JWT_SCOPE_RESOURCE_ACCESS, clientId, JWT_SCOPE_RESOURCE_ACCESS, clientId))
-                    .antMatchers(HttpMethod.POST, "/prefixRegistrationApi/validateName").permitAll()
+                    .antMatchers(HttpMethod.POST, "/prefixRegistrationApi/validate*").permitAll()
                     .anyRequest().denyAll()
                 .and()
                 .csrf()
                     .ignoringAntMatchers("/prefixRegistrationApi/registerPrefix")
-                    .ignoringAntMatchers("/prefixRegistrationApi/validateName")
+                    .ignoringAntMatchers("/prefixRegistrationApi/validate*")
                 .and()
                 .oauth2ResourceServer().jwt();
     }
