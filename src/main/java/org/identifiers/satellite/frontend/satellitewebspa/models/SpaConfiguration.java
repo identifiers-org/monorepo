e9b3f778e128ab704073272964e9d5f55f214e36
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-15 13:09:21 +0100

    spa configuration object placeholder

---
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/models/SpaConfiguration.java
@@ -0,0 +1,18 @@
+package org.identifiers.satellite.frontend.satellitewebspa.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: satellite-webspa
+ * Package: org.identifiers.satellite.frontend.satellitewebspa.models
+ * Timestamp: 2019-05-15 13:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This object defines the configuration information for the Web SPA
+ */
+@Component
+public class SpaConfiguration {
+    // TODO
+}
