1d6e6150eb6ba74a775fa7efd30ec4d097aa9357
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-15 13:02:12 +0100

    common configuration placeholder

---
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/configuration/CommonConfiguration.java
@@ -0,0 +1,16 @@
+package org.identifiers.satellite.frontend.satellitewebspa.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project: satellite-webspa
+ * Package: org.identifiers.satellite.frontend.satellitewebspa.configuration
+ * Timestamp: 2019-05-15 13:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Configuration
+public class CommonConfiguration {
+    // TODO
+}
