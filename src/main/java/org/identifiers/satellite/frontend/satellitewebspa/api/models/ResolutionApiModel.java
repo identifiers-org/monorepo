271204fdf9870eb81ac5fef8012bfc68f907e72f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-15 14:12:28 +0100

    resolution api, model, placeholder

---
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/api/models/ResolutionApiModel.java
@@ -0,0 +1,16 @@
+package org.identifiers.satellite.frontend.satellitewebspa.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: satellite-webspa
+ * Package: org.identifiers.satellite.frontend.satellitewebspa.api.models
+ * Timestamp: 2019-05-15 14:12
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class ResolutionApiModel {
+    // TODO
+}
