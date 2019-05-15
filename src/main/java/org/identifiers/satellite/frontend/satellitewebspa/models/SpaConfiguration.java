5a959e769aa0c54d1e0937eb8c301343fa151a1c
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-15 13:32:49 +0100

    I forgot the annotations

--- src/main/java/org/identifiers/satellite/frontend/satellitewebspa/models/SpaConfiguration.java
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/models/SpaConfiguration.java
@@ -1,7 +1,14 @@
 package org.identifiers.satellite.frontend.satellitewebspa.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
 
 /**
  * Project: satellite-webspa
@@ -14,7 +21,12 @@ import org.springframework.stereotype.Component;
  * This object defines the configuration information for the Web SPA
  */
 @Component
-public class SpaConfiguration {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+public class SpaConfiguration implements Serializable {
 
     @Value("${org.identifiers.satellite.frontend.satellitewebspa.config.ws.resolver.url}")
     private String urlResolver;
