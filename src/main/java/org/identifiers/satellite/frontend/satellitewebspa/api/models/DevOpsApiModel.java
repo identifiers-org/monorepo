d2e6166f5d47eb430923d5cce8702a3ad1ec34bc
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-15 13:10:33 +0100

    devops api model

---
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/api/models/DevOpsApiModel.java
@@ -0,0 +1,18 @@
+package org.identifiers.satellite.frontend.satellitewebspa.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: satellite-webspa
+ * Package: org.identifiers.satellite.frontend.satellitewebspa.api.models
+ * Timestamp: 2019-05-15 13:09
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This model bakcks the DevOps API
+ */
+@Component
+public class DevOpsApiModel {
+    // TODO
+}
