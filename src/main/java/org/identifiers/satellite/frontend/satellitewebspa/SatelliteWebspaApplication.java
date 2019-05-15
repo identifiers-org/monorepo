b44d82d6591f4e1b78cb845399ee0f95172de80f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-15 12:54:43 +0100

    project source tree

---
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/SatelliteWebspaApplication.java
@@ -0,0 +1,13 @@
+package org.identifiers.satellite.frontend.satellitewebspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelliteWebspaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatelliteWebspaApplication.class, args);
+    }
+
+}
