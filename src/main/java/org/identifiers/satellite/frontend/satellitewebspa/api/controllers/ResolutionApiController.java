e00e2577ff8129321ec8f4fd5311feaad9a094f9
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-16 10:23:48 +0100

    resolution api controller, resolveRawCompactIdentifier() implementation

--- src/main/java/org/identifiers/satellite/frontend/satellitewebspa/api/controllers/ResolutionApiController.java
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/api/controllers/ResolutionApiController.java
@@ -1,6 +1,16 @@
 package org.identifiers.satellite.frontend.satellitewebspa.api.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.identifiers.satellite.frontend.satellitewebspa.api.models.ResolutionApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Project: satellite-webspa
@@ -11,6 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
  * ---
  */
 @RestController
+@Slf4j
 public class ResolutionApiController {
+    @Autowired
+    private ResolutionApiModel model;
+
     // TODO
+    @RequestMapping(value = "/{resolutionRequest}/**", method = RequestMethod.GET)
+    public ResponseEntity<?> resolveRawCompactIdentifier(@PathVariable String resolutionRequest, HttpServletRequest request) {
+        // Extract the request path
+        final String path =
+                request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
+        log.info("Resolution request, PATH '{}'", path);
+        // Resolve
+        return model.resolveRawCompactIdentifier(path.replaceFirst("/", ""));
+    }
 }
