d3229e17fa47c6aa4582890d7e2304e830c5086f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-16 10:20:34 +0100

    resolution api controller, placeholder

---
+++ src/main/java/org/identifiers/satellite/frontend/satellitewebspa/api/controllers/ResolutionApiController.java
@@ -0,0 +1,16 @@
+package org.identifiers.satellite.frontend.satellitewebspa.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: satellite-webspa
+ * Package: org.identifiers.satellite.frontend.satellitewebspa.api.controllers
+ * Timestamp: 2019-05-16 10:19
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@RestController
+public class ResolutionApiController {
+    // TODO
+}
