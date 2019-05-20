ce79cf606882775a7478be77a61877d6fbd97dcd
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-05-20 12:31:21 +0100

    log scoring provider based on whether a resource is official or not

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ScoreProviderOfficiality.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/models/ScoreProviderOfficiality.java
@@ -1,5 +1,7 @@
 package org.identifiers.cloud.ws.resourcerecommender.models;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Project: resource-recommender
  * Package: org.identifiers.cloud.ws.resourcerecommender.models
@@ -10,6 +12,7 @@ package org.identifiers.cloud.ws.resourcerecommender.models;
  *
  * This Score Provider calculates its score taking into account whether a resource is tagged as official or not.
  */
+@Slf4j
 public class ScoreProviderOfficiality implements ScoreProvider {
 
     /**
@@ -21,8 +24,10 @@ public class ScoreProviderOfficiality implements ScoreProvider {
     @Override
     public int getScoreForResource(ResolvedResource resolvedResource) {
         if (resolvedResource.isOfficial()) {
+            log.info(String.format("Score Provider Official, resource ID '%s', URL '%s', SCORE - '%d'", resolvedResource.getId(), resolvedResource.getAccessURL(), MAX_SCORE));
             return MAX_SCORE;
         }
+        log.info(String.format("Score Provider Official, resource ID '%s', URL '%s', SCORE - '%d'", resolvedResource.getId(), resolvedResource.getAccessURL(), MIN_SCORE));
         return MIN_SCORE;
     }
 
