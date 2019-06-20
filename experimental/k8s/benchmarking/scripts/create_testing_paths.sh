aa48a8ab81461bdcd7c3a6c9d330d8fceee9c419
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-06-20 14:59:31 +0100

    script, paths creation for hq registry rest api, clean results

--- experimental/k8s/benchmarking/scripts/create_testing_paths.sh
+++ experimental/k8s/benchmarking/scripts/create_testing_paths.sh
@@ -2,3 +2,20 @@
 # Produce paths for the rest API
 # Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
+url_registry_base='https://registry.api.identifiers.org/restApi/resources/'
+file_output_paths='registry_restapi_paths.txt'
+
+# Helpers
+function log() {
+    header="$1"
+    shift
+    echo -e "[$header] $@"
+}
+
+function loginfo() {
+    log INFO $@
+}
+
+# Clean
+loginfo "[HOSEKEEPING] Empty the output file at '$file_output_paths'"
+> $file_output_paths
