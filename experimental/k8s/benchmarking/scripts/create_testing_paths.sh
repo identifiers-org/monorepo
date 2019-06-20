39176bea6156914d6fcfb88cbc4779ab31a397b6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-06-20 15:05:45 +0100

    script, paths creation for hq registry rest api, first version

--- experimental/k8s/benchmarking/scripts/create_testing_paths.sh
+++ experimental/k8s/benchmarking/scripts/create_testing_paths.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Produce paths for the rest API
+# Produce paths for the rest API, this is something I need quick and dirty to test out something on the rest API
 # Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
-url_registry_base='https://registry.api.identifiers.org/restApi/resources/'
+url_registry_base='https://registry.api.identifiers.org'
 file_output_paths='registry_restapi_paths.txt'
 
 # Helpers
@@ -19,3 +19,18 @@ function loginfo() {
 # Clean
 loginfo "[HOSEKEEPING] Empty the output file at '$file_output_paths'"
 > $file_output_paths
+loginfo "[SEARCH] Calculating URLs for resources"
+for i in $( seq 1 1000 ); do
+    curl -i ${url_registry_base}/restApi/resources/$i | grep HTTP | grep 200
+    if [ "`echo $?`" == "0" ]; then
+        echo "/restApi/resources/$i" >> $file_output_paths
+    fi
+done
+loginfo "[SEARCH] Calculating URLs for namespaces"
+for i in $( seq 1 1000 ); do
+    curl -i ${url_registry_base}/restApi/namespaces/$i | grep HTTP | grep 200
+    if [ "`echo $?`" == "0" ]; then
+        echo "/restApi/namespaces/$i" >> $file_output_paths
+    fi
+done
+loginfo "[DONE] URL calculation has now been completed."
