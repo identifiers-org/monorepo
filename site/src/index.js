69eb842c97153fc7c494b5c64eb93a1671845682
Javier Ferrer <javilol@gmail.com>
2019-06-20 17:00:10 +0100

    banner icon changed

--- site/src/index.js
+++ site/src/index.js
@@ -32,7 +32,7 @@ const betaBannerMQ = window.matchMedia( "(max-width: 1024px)" );
 if (betaBannerMQ.matches) {
   swalBannerMobile.fire({
     html: `
-      <i class="icon icon-common icon-beta size-300"></i>
+      <i class="icon icon-common icon-info size-300"></i>
       <div class="mt-2">
         Welcome to the new Identifiers.org! If you are an user from the old platform, and want to know how to migrate to the new
         API, check out the <a class="text-primary" href="https://docs.identifiers.org/articles/migration.html">migration guide</a>.
@@ -48,7 +48,7 @@ else {
     html: `
       <div class="row">
         <div class="col col-sm-12 col-md-2 align-self-center">
-          <i class="icon icon-common icon-beta size-300"></i>
+          <i class="icon icon-common icon-info size-300"></i>
         </div>
         <div class="col col-sm-12 col-md-10 beta-banner__text">
           Welcome to the new Identifiers.org! If you are an user from the old platform, and want to know how to migrate to the new
