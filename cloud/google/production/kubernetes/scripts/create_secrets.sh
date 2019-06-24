5372469d33bb9e6bbd564b388456cbbf228452e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-06-24 15:48:56 +0100

    first version of script

--- cloud/google/production/kubernetes/scripts/create_secrets.sh
+++ cloud/google/production/kubernetes/scripts/create_secrets.sh
@@ -3,6 +3,9 @@
 # This is a quick script to get the secrets
 # Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
+# Tools
+JQ="$HOME/apps/bin/jq"
+
 # Environment
 BASEDIR=$(dirname "$0")
 path_file_config=$1
@@ -11,4 +14,16 @@ path_file_config=$1
 source $BASEDIR/logging.sh
 
 loginfo "[CONFIG] Loading configuration from $path_file_config"
-loginfo "[DEPLOYMENT] Deploying secrets"
+for secret in $( cat $path_file_config | $JQ -c ".[]" ); do
+    secret_name=$(echo $secret | $JQ -r '.name')
+    kubectl_command="kubectl create secret generic $secret_name"
+    loginfo "[DEPLOYMENT] Deploying secret '$secret_name'"
+    for secret_content_item in $( echo $secret | $JQ -c '.keyValuePayload[]' ); do
+        for key in $( echo $secret_content_item | $JQ -c -r 'keys[]' ); do
+            value=$(echo $secret_content_item | $JQ -c -r ".$key")
+            loginfo "\tIncluding '$key=$value'"
+            kubectl_command="$kubectl_command --from-literal=$key=$value"
+        done
+    done
+    loginfo "[COMMAND] $kubectl_command"
+done
