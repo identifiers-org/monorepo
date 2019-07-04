091605b58913cd49e29817c555b977c8ae7cd09a
jferrer <jferrer@visualtis.com>
2019-07-04 10:12:44 +0100

    option 1: text below search bar

--- site/src/components/pages/HomePage.js
+++ site/src/components/pages/HomePage.js
@@ -19,24 +19,34 @@ class MainPage extends React.Component {
     const { query } = this.state;
 
     return (
-        <>
-          <div className="row justify-content-center">
-            <div className="col col-5">
-              <div className="logo">
-                <img src={identifiersLogo} />
-                <div className="logo-text">
-                  <h1>Identifiers.org</h1>
-                  <p className="logo-subtitle">Central registry</p>
-                </div>
+      <>
+        <div className="row justify-content-center">
+          <div className="col col-5">
+            <div className="logo">
+              <img src={identifiersLogo} />
+              <div className="logo-text">
+                <h1>Identifiers.org</h1>
+                <p className="logo-subtitle">Central registry</p>
               </div>
             </div>
           </div>
-          <div className="row justify-content-center mt-2">
-            <div className="col col-xs-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-              <Search query={query} />
-            </div>
+        </div>
+        <div className="row justify-content-center mt-2">
+          <div className="col col-xs-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+            <Search query={query} />
           </div>
-        </>
+        </div>
+        <div className="mt-5 d-flex align-items-center">
+          <i className="icon icon-common icon-info size-400 text-primary mr-4" />
+          <h4 className="mb-0 text-justify">
+            The <span className="text-dark">Identifiers.org</span> resolution system provides consistent access
+            to life science data using Compact Identifiers (CIDs). Compact Identifiers consist of an assigned
+            unique prefix and a local provider designated accession number (prefix:accession). The resolving
+            location of Compact Identifiers is determined using information that is stored in the Identifiers.org
+            Registry.
+          </h4>
+        </div>
+      </>
     );
   }
 }
