83d0052e0270e8e7fcd1a6bae2be3fe7fa262f49
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 12:48:24 +0100

    serivce request payload for resource registration request, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterResourcePayload.java
@@ -0,0 +1,29 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-07-25 12:46
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ServiceRequestRegisterResourcePayload implements Serializable {
+
+    // TODO
+}
