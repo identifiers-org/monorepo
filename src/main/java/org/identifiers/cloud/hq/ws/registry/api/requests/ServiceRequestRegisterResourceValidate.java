69bef81aa8e5b441976a47a6453ea38987a4bee6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 16:17:06 +0100

    ServiceRequestRegisterResourceValidate

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRegisterResourceValidate.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-07-25 16:16
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestRegisterResourceValidate extends ServiceRequest<ServiceRequestRegisterResourcePayload> {
+}
