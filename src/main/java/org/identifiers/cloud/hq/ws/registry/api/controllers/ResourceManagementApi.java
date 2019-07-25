d96a4ea78564fa8ca3d586bbcdff824d2815dcfa
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 13:40:07 +0100

    placeholder endpoints for Resource Registration Request Management

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
@@ -3,9 +3,8 @@ package org.identifiers.cloud.hq.ws.registry.api.controllers;
 import org.identifiers.cloud.hq.ws.registry.api.models.ResourceManagementApiModel;
 import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterResource;
 import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterResourceSessionEvent;
-import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseRegisterPrefixSessionEvent;
-import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseRegisterResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,40 +27,45 @@ public class ResourceManagementApi {
 
 
     // TODO --- Resource Registration Request Management ---
+    // TODO Resource Registration Request
     @PostMapping(value = "/registerPrefix")
     public ResponseEntity<?> registerPrefix(@RequestBody ServiceRequestRegisterResource request) {
-        ServiceResponseRegisterResource response = model.registerResource(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+//        ServiceResponseRegisterResource response = model.registerResource(request);
+//        return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
-    @PostMapping(value = "/amendPrefixRegistrationRequest/{sessionId}")
-    public ResponseEntity<?> amendPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
-        ServiceResponseRegisterResource response = model.amendResourceRegistrationRequest(sessionId, request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/commentPrefixRegistrationRequest/{sessionId}")
-    public ResponseEntity<?> commentPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
-        ServiceResponseRegisterResourceSessionEvent response = model.commentResourceRegistrationRequest(sessionId, request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/rejectPrefixRegistrationRequest/{sessionId}")
-    public ResponseEntity<?> rejectPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
-        ServiceResponseRegisterResourceSessionEvent response = model.rejectResourceRegistrationRequest(sessionId, request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/acceptPrefixRegistrationRequest/{sessionId}")
-    public ResponseEntity<?> acceptPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
-        ServiceResponseRegisterResourceSessionEvent response = model.acceptResourceRegistrationRequest(sessionId, request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-    // TODO Resource Registration Request
     // TODO Resource Registration Session, amend
+    @PostMapping(value = "/amendResourceRegistrationRequest/{sessionId}")
+    public ResponseEntity<?> amendPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
+//        ServiceResponseRegisterResource response = model.amendResourceRegistrationRequest(sessionId, request);
+//        return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+    }
+
     // TODO Resource Registration Session, comment
+    @PostMapping(value = "/commentResourceRegistrationRequest/{sessionId}")
+    public ResponseEntity<?> commentPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
+//        ServiceResponseRegisterResourceSessionEvent response = model.commentResourceRegistrationRequest(sessionId, request);
+//        return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+    }
+
     // TODO Resource Registration Session, reject
+    @PostMapping(value = "/rejectResourceRegistrationRequest/{sessionId}")
+    public ResponseEntity<?> rejectPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
+//        ServiceResponseRegisterResourceSessionEvent response = model.rejectResourceRegistrationRequest(sessionId, request);
+//        return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+    }
+
     // TODO Resource Registration Session, accept
+    @PostMapping(value = "/acceptResourceRegistrationRequest/{sessionId}")
+    public ResponseEntity<?> acceptPrefixRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
+//        ServiceResponseRegisterResourceSessionEvent response = model.acceptResourceRegistrationRequest(sessionId, request);
+//        return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+    }
 
     // TODO --- Resource Registration Request Validation ---
 
