0c838f086ddc245a08bb434e9ff256b3b4acf627
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 12:16:01 +0100

    resource management API, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2019-07-25 12:11
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This API is intended for management of resources at a more complex level than just the REST repositories.
+ */
+public class ResourceManagementApi {
+}
