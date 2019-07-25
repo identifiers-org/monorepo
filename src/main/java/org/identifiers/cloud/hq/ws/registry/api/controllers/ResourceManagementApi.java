594be0462b9acaf998ed16da7c227504e81f8c77
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 16:34:07 +0100

    resource registration validation endpoint placeholders

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
@@ -3,6 +3,7 @@ package org.identifiers.cloud.hq.ws.registry.api.controllers;
 import org.identifiers.cloud.hq.ws.registry.api.models.ResourceManagementApiModel;
 import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterResource;
 import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterResourceSessionEvent;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterResourceValidate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -69,124 +70,109 @@ public class ResourceManagementApi {
 
     // TODO --- Resource Registration Request Validation ---
 
-    @PostMapping(value = "/validateName")
-    public ResponseEntity<?> validateName(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateName(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/validateDescription")
-    public ResponseEntity<?> validateDescription(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateDescription(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
     @PostMapping(value = "/validateProviderHomeUrl")
-    public ResponseEntity<?> validateProviderHomeUrl(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateProviderHomeUrl(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateProviderHomeUrl(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateProviderHomeUrl(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateProviderName")
-    public ResponseEntity<?> validateProviderName(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateProviderName(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateProviderName(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateProviderName(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateProviderDescription")
-    public ResponseEntity<?> validateProviderDescription(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateProviderDescription(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateProviderDescription(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateProviderDescription(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateProviderLocation")
-    public ResponseEntity<?> validateProviderLocation(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateProviderLocation(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateProviderLocation(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateProviderLocation(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateProviderCode")
-    public ResponseEntity<?> validateProviderCode(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateProviderCode(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateProviderCode(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateProviderCode(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateInstitutionName")
-    public ResponseEntity<?> validateInstitutionName(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateInstitutionName(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateInstitutionName(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateInstitutionName(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateInstitutionHomeUrl")
-    public ResponseEntity<?> validateInstitutionHomeUrl(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateInstitutionHomeUrl(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateInstitutionHomeUrl(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateInstitutionHomeUrl(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateInstitutionDescription")
-    public ResponseEntity<?> validateInstitutionDescription(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateInstitutionDescription(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateInstitutionDescription(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateInstitutionDescription(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateInstitutionLocation")
-    public ResponseEntity<?> validateInstitutionLocation(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateInstitutionLocation(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/validateRequestedPrefix")
-    public ResponseEntity<?> validateRequestedPrefix(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateRequestedPrefix(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateInstitutionLocation(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateInstitutionLocation(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateProviderUrlPattern")
-    public ResponseEntity<?> validateProviderUrlPattern(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateProviderUrlPattern(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateProviderUrlPattern(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateProviderUrlPattern(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateSampleId")
-    public ResponseEntity<?> validateSampleId(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateSampleId(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/validateIdRegexPattern")
-    public ResponseEntity<?> validateIdRegexPattern(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateIdRegexPattern(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
-    }
-
-    @PostMapping(value = "/validateReferences")
-    public ResponseEntity<?> validateReferences(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateReferences(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateSampleId(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateSampleId(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateAdditionalInformation")
-    public ResponseEntity<?> validateAdditionalInformation(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateAdditionalInformation(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateAdditionalInformation(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateAdditionalInformation(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateRequester")
-    public ResponseEntity<?> validateRequester(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateRequester(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateRequester(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateRequester(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateRequesterName")
-    public ResponseEntity<?> validateRequesterName(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateRequesterName(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateRequesterName(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateRequesterName(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping(value = "/validateRequesterEmail")
-    public ResponseEntity<?> validateRequesterEmail(@RequestBody ServiceRequestRegisterPrefixValidate request) {
-        ServiceResponseValidateRequest response = model.validateRequesterEmail(request);
-        return new ResponseEntity<>(response, response.getHttpStatus());
+    public ResponseEntity<?> validateRequesterEmail(@RequestBody ServiceRequestRegisterResourceValidate request) {
+        // ServiceResponseRegisterResourceValidate response = model.validateRequesterEmail(request);
+        // return new ResponseEntity<>(response, response.getHttpStatus());
+        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
     }
 
     // TODO --- Resource Lifecycle Management
