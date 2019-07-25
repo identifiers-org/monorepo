de9d62a05640d55dec8fc8fdf70e0440101c109c
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 13:22:31 +0100

    ServiceResponseRegisterResource

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseRegisterResource.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2019-07-25 13:20
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseRegisterResource extends ServiceResponse<ServiceResponseRegisterResourcePayload> {
+}
