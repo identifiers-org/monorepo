c20a630b0acae3c4adbf90af152be1a4318ed95d
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-25 13:34:42 +0100

    ServiceResponseRegisterResourceSessionEvent

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseRegisterResourceSessionEvent.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2019-07-25 13:34
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseRegisterResourceSessionEvent extends ServiceResponse<ServiceResponseRegisterResourceSessionEventPayload> {
+}
