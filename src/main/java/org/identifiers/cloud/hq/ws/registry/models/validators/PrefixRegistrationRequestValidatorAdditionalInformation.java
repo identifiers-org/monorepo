83bf049c2f83d3f844ef756562a83a24aada7784
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-26 16:45:16 +0100

    ResourceRegistrationRequestValidatorAdditionalInformation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorAdditionalInformation.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/PrefixRegistrationRequestValidatorAdditionalInformation.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+// TODO I don't think it needs to be of "prototype" scope
 @Component
 @Scope("prototype")
 @Qualifier("prefixRegistrationRequestValidatorAdditionalInformation")
