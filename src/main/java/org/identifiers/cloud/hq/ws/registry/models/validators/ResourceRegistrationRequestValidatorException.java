1217b1f5874d827c8eb76b7a98b893b24e41dc58
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-26 11:09:35 +0100

    ResourceRegistrationRequestValidatorException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceRegistrationRequestValidatorException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-07-26 11:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceRegistrationRequestValidatorException extends RuntimeException {
+    public ResourceRegistrationRequestValidatorException(String message) {
+        super(message);
+    }
+}
