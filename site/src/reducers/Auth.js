d630c2fd135f020e33f532e9fcb5709eb9787a77
jferrer <jferrer@visualtis.com>
2019-07-26 11:15:24 +0100

    added actions for periodic auth renew

--- site/src/reducers/Auth.js
+++ site/src/reducers/Auth.js
@@ -6,13 +6,24 @@ import keycloakConf from '../config/keycloak.json';
 // Auth reducer
 const defaultState = {
   authenticated: undefined,
+  authRenewalIntervalHandler: undefined,
   keycloak: new Keycloak(keycloakConf)
 };
 
 const authReducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'SET_AUTHENTICATED':
-      return {authenticated: action.keycloak.authenticated, keycloak: action.keycloak}
+      return {authenticated: action.keycloak.authenticated, keycloak: action.keycloak};
+
+    case 'SET_AUTHRENEWALINTERVALHANDLER':
+
+      return {...state, authRenewalIntervalHandler: setInterval(action.authRenewalIntervalHandler, action.timeout)};
+
+    case 'RESET_AUTHRENEWALINTERVAL':
+      clearInterval(state.authRenewalIntervalHandler);
+      console.log('Auth renew interval reset');
+      return {...state, authRenewalIntervalHandler: setInterval(action.authRenewalIntervalHandler, action.timeout)};
+
 
     default:
       return state;
