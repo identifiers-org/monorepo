a2e9c1ce2a0c930252bfb848d430c9cea76e5281
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-26 16:36:05 +0100

    ResourceRegistrationRequestValidatorProviderCode

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceRegistrationRequestValidatorProviderCode.java
@@ -0,0 +1,28 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+import org.identifiers.cloud.hq.ws.registry.api.data.helpers.ApiAndDataModelsHelper;
+import org.identifiers.cloud.hq.ws.registry.api.requests.ServiceRequestRegisterResourcePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-07-26 13:20
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+@Qualifier("ResourceRegistrationRequestValidatorProviderCode")
+public class ResourceRegistrationRequestValidatorProviderCode implements ResourceRegistrationRequestValidator {
+    @Autowired
+    @Qualifier("PrefixRegistrationRequestValidatorProviderCode")
+    private PrefixRegistrationRequestValidator delegateValidator;
+
+    @Override
+    public boolean validate(ServiceRequestRegisterResourcePayload request) throws ResourceRegistrationRequestValidatorException {
+        return delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request));
+    }
+}
