559b2dec50c98d918520406ae9864c77790d8d55
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 00:28:09 +0100

    validation of requester

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
@@ -145,9 +145,8 @@ public class ResourceManagementApi {
 
     @PostMapping(value = "/validateRequester")
     public ResponseEntity<?> validateRequester(@RequestBody ServiceRequestRegisterResourceValidate request) {
-        // ServiceResponseRegisterResourceValidate response = model.validateRequester(request);
-        // return new ResponseEntity<>(response, response.getHttpStatus());
-        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+        ServiceResponseRegisterResourceValidate response = model.validateRequester(request);
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 
     @PostMapping(value = "/validateRequesterName")
