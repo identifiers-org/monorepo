c8e74d68de7abc2e7ce1da45dd25219cad3b48b6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 00:27:13 +0100

    validation of sample ID

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
@@ -133,9 +133,8 @@ public class ResourceManagementApi {
 
     @PostMapping(value = "/validateSampleId")
     public ResponseEntity<?> validateSampleId(@RequestBody ServiceRequestRegisterResourceValidate request) {
-        // ServiceResponseRegisterResourceValidate response = model.validateSampleId(request);
-        // return new ResponseEntity<>(response, response.getHttpStatus());
-        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+        ServiceResponseRegisterResourceValidate response = model.validateSampleId(request);
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 
     @PostMapping(value = "/validateAdditionalInformation")
