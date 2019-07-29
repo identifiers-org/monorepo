844fdfce4d9883b809052aeb44dd5eaa30b0c72b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 01:23:24 +0100

    ResourceRegistrationSession data model definition

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceRegistrationSession.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/models/ResourceRegistrationSession.java
@@ -1,5 +1,17 @@
 package org.identifiers.cloud.hq.ws.registry.data.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.models
@@ -11,7 +23,29 @@ package org.identifiers.cloud.hq.ws.registry.data.models;
  * This model represents a resource registration session, it groups together the different stages in the process of
  * approving or rejecting a resource registration request, and the evolution of its content.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {@Index(name = "idx_closed", columnList = "closed")})
 public class ResourceRegistrationSession {
+    @Id
+    @GeneratedValue
+    private long id;
 
-    // TODO
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private boolean closed = false;
+
+    // This is a reference to the latest version of the resource registration request that's being processed in the session.
+    @OneToOne(optional = false)
+    private ResourceRegistrationRequest resourceRegistrationRequest;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date created;
 }
