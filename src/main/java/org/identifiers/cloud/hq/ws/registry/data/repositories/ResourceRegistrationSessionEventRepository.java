aa76bb3230526caca474757f59a3fb8291c99804
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 08:40:14 +0100

    find resource registration session events by session ID

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionEventRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionEventRepository.java
@@ -3,6 +3,8 @@ package org.identifiers.cloud.hq.ws.registry.data.repositories;
 import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSessionEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
@@ -13,4 +15,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ResourceRegistrationSessionEventRepository extends JpaRepository<ResourceRegistrationSessionEvent, Long> {
     // TODO
+    List<ResourceRegistrationSessionEvent> findByResourceRegistrationSessionId(long id);
 }
