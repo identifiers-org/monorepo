b7062029d2f722ec0ea29d597312028928ab77d0
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 08:32:38 +0100

    ResourceRegistrationSessionEventRejectRepository

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionEventRejectRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSessionEventReject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2019-07-29 08:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ResourceRegistrationSessionEventRejectRepository extends JpaRepository<ResourceRegistrationSessionEventReject, Long> {
+    // TODO
+}
