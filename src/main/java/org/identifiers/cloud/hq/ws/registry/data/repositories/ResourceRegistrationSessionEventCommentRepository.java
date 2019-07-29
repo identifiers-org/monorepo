7b8bdaaccff93d7a66caad0c14d472f75518b354
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 08:30:39 +0100

    ResourceRegistrationSessionEventCommentRepository

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionEventCommentRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSessionEventComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2019-07-29 08:28
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ResourceRegistrationSessionEventCommentRepository extends JpaRepository<ResourceRegistrationSessionEventComment, Long> {
+    // TODO
+}
