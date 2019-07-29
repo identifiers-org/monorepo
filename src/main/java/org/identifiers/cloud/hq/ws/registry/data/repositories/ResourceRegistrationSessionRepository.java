b5d80ac7c3ad6fcb07904865a4486613cf5999f4
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 08:42:48 +0100

    find resource registration requests that are open

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionRepository.java
@@ -1,6 +1,8 @@
 package org.identifiers.cloud.hq.ws.registry.data.repositories;
 
 import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSession;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -13,4 +15,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ResourceRegistrationSessionRepository extends JpaRepository<ResourceRegistrationSession, Long> {
     // TODO
+    Page<ResourceRegistrationSession> findByClosedFalse(Pageable pageable);
 }
