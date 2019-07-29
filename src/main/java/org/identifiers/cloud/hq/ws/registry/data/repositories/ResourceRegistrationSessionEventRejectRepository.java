284122ea73cf71bf449301441b7a64eb252efc05
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 08:38:57 +0100

    find resource registration session rejected by session ID

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionEventRejectRepository.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionEventRejectRepository.java
@@ -13,4 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ResourceRegistrationSessionEventRejectRepository extends JpaRepository<ResourceRegistrationSessionEventReject, Long> {
     // TODO
+    ResourceRegistrationSessionEventReject findByResourceRegistrationSessionId(long id);
 }
