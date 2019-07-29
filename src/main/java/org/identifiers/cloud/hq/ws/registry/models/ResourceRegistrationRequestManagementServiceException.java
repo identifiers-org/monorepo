69b9c0f1c325f268a1e6e8d916be4b1ca44ad0db
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 01:44:42 +0100

    ResourceRegistrationRequestManagementServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationRequestManagementServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-07-29 01:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceRegistrationRequestManagementServiceException extends RuntimeException {
+    public ResourceRegistrationRequestManagementServiceException(String message) {
+        super(message);
+    }
+}
