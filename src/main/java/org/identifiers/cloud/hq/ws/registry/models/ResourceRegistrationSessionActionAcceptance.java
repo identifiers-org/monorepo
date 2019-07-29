028080876ec82ea674719c1b9bcb63682ce28f15
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 17:02:02 +0100

    composite action director

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationSessionActionAcceptance.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationSessionActionAcceptance.java
@@ -3,6 +3,9 @@ package org.identifiers.cloud.hq.ws.registry.models;
 import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models
@@ -18,6 +21,13 @@ public class ResourceRegistrationSessionActionAcceptance implements ResourceRegi
     // Related actions
     @Autowired
     private ResourceRegistrationSessionActionLogger actionLogger;
+
+    // Director
+    private List<ResourceRegistrationSessionAction> buildActionSequence() {
+        // TODO - Right now, we just log the closing of the resource registration session, but in the future there will
+        //  be notifications and other actions triggered by an accepted prefix registration request
+        return Arrays.asList(actionLogger);
+    }
     
     // TODO
     @Override
