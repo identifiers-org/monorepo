554c5c19508da97954382e16101c6f2278f51df8
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 11:49:18 +0100

    ResourceRegistrationSessionActionException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationSessionActionException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-07-29 11:49
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceRegistrationSessionActionException extends RuntimeException {
+    public ResourceRegistrationSessionActionException(String message) {
+        super(message);
+    }
+}
