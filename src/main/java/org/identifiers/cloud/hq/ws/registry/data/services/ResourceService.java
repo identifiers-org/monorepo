93619e88583fc55cc6204456bd2febc13b9f1691
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 19:52:03 +0100

    Resource Service appendResourceToNamespace(), skeleton, pending namespace retrieval

--- src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/ResourceService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/services/ResourceService.java
@@ -82,4 +82,43 @@ public class ResourceService {
         Resource registeredResource = repository.save(resource);
         return registeredResource;
     }
+
+    @Transactional
+    public Resource appendResourceToNamespace(Resource resource, String namespacePrefix) throws ResourceServiceException {
+        // TODO Resource instance validations via repository event handler
+        // Current model definitions only allow for locating a resource via its MIR ID or the ID
+        // It is not clear yet how to proceed as in: do we allow this method to be called with resources that have a
+        // MIR ID or not? Let's go for 'yes, we allow this to be called with resources with an existing MIR ID, although
+        // it may never happen, and we'll refine this in the future
+        // TODO maybe think about future refactoring for the following code
+        if (resource.getMirId() == null) {
+            // Request a MIR ID
+            try {
+                resource.setMirId(mirIdService.mintId());
+                log.info(String.format("Registering resource with name '%s', for namespace '%s', newly minted MIR ID '%s'",
+                        resource.getName(), resource.getNamespace().getPrefix(), resource.getMirId()));
+            } catch (MirIdServiceException e) {
+                throw new ResourceServiceException(String.format("Could not mint MIR ID for registering resource " +
+                                "with name '%s', for namespace '%s', due to '%s'",
+                        resource.getName(),
+                        resource.getNamespace().getPrefix(),
+                        e.getMessage()));
+            }
+        }
+        // Register the contact person
+        resource.setContactPerson(personService.registerPerson(resource.getContactPerson()));
+        // Register the location
+        resource.setLocation(locationService.registerLocation(resource.getLocation()));
+        // Register the institution
+        resource.setInstitution(institutionService.registerInstitution(resource.getInstitution()));
+        // TODO Retrieve the namespace by its prefix
+        // Register the resource within its namespace. Ok, why doing it this way? Because if this resource can't be
+        // registered as provider within its associated namespace, the Namespace service will throw an exception that
+        // will trigger a rollback in the database, as this method is transactional
+        namespaceService.registerProvider(resource.getNamespace(), resource);
+        // Register the resource
+        Resource registeredResource = repository.save(resource);
+        return registeredResource;
+
+    }
 }
