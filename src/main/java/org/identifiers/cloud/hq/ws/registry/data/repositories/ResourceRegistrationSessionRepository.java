e08d9eb7acf5d6ace27f844b652813efb978e5ff
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 07:44:16 +0100

    ResourceRegistrationSessionRepository

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/data/repositories/ResourceRegistrationSessionRepository.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.data.repositories;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.data.repositories
+ * Timestamp: 2019-07-29 07:43
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ResourceRegistrationSessionRepository extends JpaRepository<ResourceRegistrationSession, Long> {
+    // TODO
+}
