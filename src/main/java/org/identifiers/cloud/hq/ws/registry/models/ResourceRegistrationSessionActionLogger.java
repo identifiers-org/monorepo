142fc98dcfd046df89d4dd6c10df8475e5cda1b8
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-29 16:47:33 +0100

    wire in repositories

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationSessionActionLogger.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationSessionActionLogger.java
@@ -1,6 +1,9 @@
 package org.identifiers.cloud.hq.ws.registry.models;
 
 import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSession;
+import org.identifiers.cloud.hq.ws.registry.data.repositories.ResourceRegistrationSessionEventAcceptRepository;
+import org.identifiers.cloud.hq.ws.registry.data.repositories.ResourceRegistrationSessionEventRejectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Project: registry
@@ -13,6 +16,14 @@ import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSess
  * This action is about logging the fact that a resource registration session has been closed.
  */
 public class ResourceRegistrationSessionActionLogger implements ResourceRegistrationSessionAction {
+    // Repositories
+    @Autowired
+    private ResourceRegistrationSessionEventAcceptRepository resourceRegistrationSessionEventAcceptRepository;
+    @Autowired
+    private ResourceRegistrationSessionEventRejectRepository resourceRegistrationSessionEventRejectRepository;
+
+    
+    // TODO
     @Override
     public ResourceRegistrationSessionActionReport performAction(ResourceRegistrationSession session) throws ResourceRegistrationSessionActionException {
         return null;
