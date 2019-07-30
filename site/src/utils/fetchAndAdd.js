abdd52906e8b0a704cd318b46e2f0986540a3d41
jferrer <jferrer@visualtis.com>
2019-07-30 14:40:50 +0100

    add fetchandadd add id to subfields option

--- site/src/utils/fetchAndAdd.js
+++ site/src/utils/fetchAndAdd.js
@@ -1,10 +1,13 @@
 // Helper function for HATEOAS requests: fetches subobjects.
-export const fetchAndAdd = async function (parent, links, init) {
+export const fetchAndAdd = async function (parent, links, init, addId = false) {
   const resources = await Promise.all(links.map(link => fetch(link.url, init)));
   const jsonResources = await Promise.all(resources.map(res => res.ok ? res.json() : undefined));
 
   jsonResources.forEach((resource, index) => {
     parent[links[index].name] = resource;
+    if (addId) {
+      parent[links[index].name]['id'] = resource._links.self.href;
+    }
   });
 
   return parent;
