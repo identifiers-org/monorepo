1741609f7b6e735f12d24bb42a1fca6e3e993b59
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-07-30 11:54:31 +0100

    resource registration endpoint, commentResourceRegistrationRequest

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/ResourceManagementApi.java
@@ -48,9 +48,8 @@ public class ResourceManagementApi {
     // TODO Resource Registration Session, comment
     @PostMapping(value = "/commentResourceRegistrationRequest/{sessionId}")
     public ResponseEntity<?> commentResourceRegistrationRequest(@PathVariable long sessionId, @RequestBody ServiceRequestRegisterResourceSessionEvent request) {
-//        ServiceResponseRegisterResourceSessionEvent response = model.commentResourceRegistrationRequest(sessionId, request);
-//        return new ResponseEntity<>(response, response.getHttpStatus());
-        return new ResponseEntity<>("WORK IN PROGRESS", HttpStatus.NOT_IMPLEMENTED);
+        ServiceResponseRegisterResourceSessionEvent response = model.commentResourceRegistrationRequest(sessionId, request);
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 
     // TODO Resource Registration Session, reject
