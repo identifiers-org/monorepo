0f6e52c5f2ea0bbcfac70c824695721a712ff272
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 20:28:12 +0100

    NamespaceManagementApiModel, initDefaultResponse() helper

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/NamespaceManagementApiModel.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/NamespaceManagementApiModel.java
@@ -1,7 +1,10 @@
 package org.identifiers.cloud.hq.ws.registry.api.models;
 
 import lombok.extern.slf4j.Slf4j;
+import org.identifiers.cloud.hq.ws.registry.api.ApiCentral;
+import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponse;
 import org.identifiers.cloud.hq.ws.registry.models.NamespaceLifecycleManagementService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,6 +20,13 @@ import org.springframework.stereotype.Component;
 public class NamespaceManagementApiModel {
     // Services
     private NamespaceLifecycleManagementService namespaceLifecycleManagementService;
-    
+
+    // Helpers
+    private <T> void initDefaultResponse(ServiceResponse<T> response, T payload) {
+        response.setApiVersion(ApiCentral.apiVersion)
+                .setHttpStatus(HttpStatus.OK);
+        response.setPayload(payload);
+    }
+
     // TODO
 }
