d20af9a3790ba94356936b37341c0adf496cf167
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 19:05:12 +0100

    NamespaceLifecycleManagementSimpleStrategyContext

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/NamespaceLifecycleManagementSimpleStrategyContext.java
@@ -0,0 +1,24 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 19:03
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+public class NamespaceLifecycleManagementSimpleStrategyContext extends NamespaceLifecycleManagementContext {
+    // TODO
+}
