640ca6acde5a1b70393203d589e141308ec8542d
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 05:32:50 +0100

    ResourceLifecycleManagementOperationReport

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceLifecycleManagementOperationReport.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models.validators;
+
+import org.identifiers.cloud.hq.ws.registry.models.ActionReport;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.validators
+ * Timestamp: 2019-08-01 05:32
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceLifecycleManagementOperationReport extends ActionReport {
+    // Common report
+}
