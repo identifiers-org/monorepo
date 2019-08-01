4247d06eee7e9cbf7aecfbb9f02d8ac943f4e431
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 11:58:18 +0100

    ServiceRequestReactivateResource api request data model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestReactivateResource.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-08-01 11:49
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestReactivateResource extends ServiceRequest<ServiceRequestReactivateResourcePayload> {
+    // TODO
+}
