9944d6606b825e64d5506ecc637ffb02e4e7963f
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 05:34:56 +0100

    ResourceLifecycleManagementServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 05:34
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceLifecycleManagementServiceException extends RuntimeException {
+    public ResourceLifecycleManagementServiceException(String message) {
+        super(message);
+    }
+}
