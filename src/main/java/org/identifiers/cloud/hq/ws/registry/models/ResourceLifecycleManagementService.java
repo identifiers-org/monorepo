2251d67fb74f2921c2214b8f3ee1fd6024f84f40
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 05:09:54 +0100

    ResourceLifecycleManagementService, interface placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementService.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 05:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface ResourceLifecycleManagementService {
+}
