464298a32316cc316fdc4d72b1fb7ba498d316f4
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 20:58:58 +0100

    namespace management api controller, deactivation and re-activation of namespaces, endpoints implementation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/NamespaceManagementApiController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/NamespaceManagementApiController.java
@@ -1,7 +1,12 @@
 package org.identifiers.cloud.hq.ws.registry.api.controllers;
 
 import org.identifiers.cloud.hq.ws.registry.api.models.NamespaceManagementApiModel;
+import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseDeactivateNamespace;
+import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseReactivateNamespace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,16 @@ public class NamespaceManagementApiController {
     @Autowired
     private NamespaceManagementApiModel model;
 
-    
-    // TODO
+    // --- Namespace Lifecycle Management
+    @GetMapping(value = "/deactivateNamespace/{namespaceId}")
+    public ResponseEntity<?> deactivateNamespace(@PathVariable long namespaceId) {
+        ServiceResponseDeactivateNamespace response = model.deactivateNamespace(namespaceId);
+        return new ResponseEntity<>(response, response.getHttpStatus());
+    }
+
+    @GetMapping(value = "/reactivateNamespace/{namespaceId}")
+    public ResponseEntity<?> reactivateNamespace(@PathVariable long namespaceId) {
+        ServiceResponseReactivateNamespace response = model.reactivateNamespace(namespaceId);
+        return new ResponseEntity<>(response, response.getHttpStatus());
+    }
 }
