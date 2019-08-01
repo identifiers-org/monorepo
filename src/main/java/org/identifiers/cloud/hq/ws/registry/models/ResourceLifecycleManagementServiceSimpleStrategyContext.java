c1537842d56a5487ec2e5e3a65dab38d4af6f62b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 05:55:49 +0100

    ResourceLifecycleManagementServiceSimpleStrategyContext, skeleton

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementServiceSimpleStrategyContext.java
@@ -0,0 +1,22 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 05:55
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class ResourceLifecycleManagementServiceSimpleStrategyContext extends ResourceLifecycleManagementContext {
+    // TODO
+}
