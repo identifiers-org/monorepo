bca55698a9f6f2203b8ee813755960ec293400eb
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 11:00:50 +0100

    ServiceResponseDeactivateResourcePayload api data model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseDeactivateResourcePayload.java
@@ -0,0 +1,24 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2019-08-01 10:59
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class ServiceResponseDeactivateResourcePayload implements Serializable {
+    private String comment = "No comments on your resource deactivation request";
+}
