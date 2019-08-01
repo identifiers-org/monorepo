40faf934f3c491699b108552ec2311bc58d44096
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 12:02:02 +0100

    ServiceResponseReactivateResource api response body model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/responses/ServiceResponseReactivateResource.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.api.responses;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.responses
+ * Timestamp: 2019-08-01 12:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseReactivateResource extends ServiceResponse<ServiceResponseReactivateResourcePayload> {
+    // TODO
+}
