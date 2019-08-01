5af74de0ccfa0276f0631ebc86a52461d1e2784d
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 06:12:10 +0100

    algorithm skeleton for deactivation of resources

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementServiceSimpleStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementServiceSimpleStrategy.java
@@ -23,6 +23,9 @@ public class ResourceLifecycleManagementServiceSimpleStrategy implements Resourc
     @Override
     public ResourceLifecycleManagementOperationReport deactivateResource(Resource resource,
                                                                          ResourceLifecycleManagementContext context, String actor, String additionalInformation) throws ResourceLifecycleManagementServiceException {
+        // TODO Create default report
+        // TODO Check whether the given resource is active or not
+        // TODO Deprecate the resource
         return null;
     }
 
