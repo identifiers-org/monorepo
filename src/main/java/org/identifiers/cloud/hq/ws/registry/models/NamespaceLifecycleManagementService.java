71a2afedf3f86c9de7cfd03b645b9e6582a64081
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 17:57:44 +0100

    NamespaceLifecycleManagementService, interface placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/NamespaceLifecycleManagementService.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 17:57
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public interface NamespaceLifecycleManagementService {
+}
