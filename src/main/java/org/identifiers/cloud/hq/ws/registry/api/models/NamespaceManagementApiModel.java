f97c61e08b68627b281459469848e1fd7c438bb7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 20:19:50 +0100

    NamespaceManagementApiModel, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/NamespaceManagementApiModel.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2019-08-01 20:19
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class NamespaceManagementApiModel {
+    // TODO
+}
