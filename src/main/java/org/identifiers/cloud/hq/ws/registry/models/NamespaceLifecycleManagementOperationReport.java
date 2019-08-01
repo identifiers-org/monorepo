c3c6a6e7fab2660ad57917bd920d2dee81a91506
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 18:19:59 +0100

    NamespaceLifecycleManagementOperationReport

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/NamespaceLifecycleManagementOperationReport.java
@@ -0,0 +1,24 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.identifiers.cloud.hq.ws.registry.data.models.Namespace;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 18:19
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class NamespaceLifecycleManagementOperationReport extends ActionReport {
+    // Common report
+    Namespace namespace;
+}
--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/NamespaceLifecycleManagementService.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/NamespaceLifecycleManagementService.java
@@ -36,5 +36,5 @@ public interface NamespaceLifecycleManagementService {
      */
     NamespaceLifecycleManagementContext createEmptyContext();
 
-    
+
 }
