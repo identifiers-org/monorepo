7c8e15b469cce642653d78dbdd69e4786ee0cdd6
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 05:41:58 +0100

    ResourceReactivationContext

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceReactivationContext.java
@@ -0,0 +1,22 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 05:39
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class ResourceReactivationContext {
+    // Base class for the information needed by any final resource lifecycle management service strategy
+}
