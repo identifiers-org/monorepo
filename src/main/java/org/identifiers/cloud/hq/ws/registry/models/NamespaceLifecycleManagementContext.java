8331d3eef2fe7b577820ec7d6d0e4d33c5edc2ca
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 18:16:58 +0100

    NamespaceLifecycleManagementContext, skeleton

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/NamespaceLifecycleManagementContext.java
@@ -0,0 +1,22 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-01 18:16
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class NamespaceLifecycleManagementContext {
+    // TODO
+}
