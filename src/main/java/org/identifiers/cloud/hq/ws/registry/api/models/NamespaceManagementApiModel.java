b5fbef188bf79d4a61c8f1ead6ab8535aaee9883
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-01 20:36:50 +0100

    NamespaceManagementApiModel, createNamespaceDeactivationDefaultResponse() helper

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/NamespaceManagementApiModel.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/NamespaceManagementApiModel.java
@@ -3,6 +3,8 @@ package org.identifiers.cloud.hq.ws.registry.api.models;
 import lombok.extern.slf4j.Slf4j;
 import org.identifiers.cloud.hq.ws.registry.api.ApiCentral;
 import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponse;
+import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseDeactivateNamespace;
+import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponseDeactivateNamespacePayload;
 import org.identifiers.cloud.hq.ws.registry.models.NamespaceLifecycleManagementService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -26,6 +28,12 @@ public class NamespaceManagementApiModel {
         response.setApiVersion(ApiCentral.apiVersion)
                 .setHttpStatus(HttpStatus.OK);
         response.setPayload(payload);
+    }
+
+    private ServiceResponseDeactivateNamespace createNamespaceDeactivationDefaultResponse() {
+        ServiceResponseDeactivateNamespace response = new ServiceResponseDeactivateNamespace();
+        initDefaultResponse(response, new ServiceResponseDeactivateNamespacePayload());
+        return response;
     }
 
     // TODO
