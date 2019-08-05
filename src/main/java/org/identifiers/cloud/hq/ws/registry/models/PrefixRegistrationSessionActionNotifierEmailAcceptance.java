40775bf9dc46af2e4a54f98eedb5ac76ff20d0d7
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-05 11:39:23 +0100

    prefix registration notifier via e-mail, skeleton

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionNotifierEmailAcceptance.java
@@ -0,0 +1,41 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import lombok.extern.slf4j.Slf4j;
+import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-05 11:13
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Slf4j
+@Component
+@Qualifier("PrefixRegistrationSessionActionNotifierEmailAcceptance")
+public class PrefixRegistrationSessionActionNotifierEmailAcceptance implements PrefixRegistrationSessionAction {
+    private static final int MAIL_REQUEST_RETRY_MAX_ATTEMPTS = 12;
+    private static final int MAIL_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    // Helper
+    @Retryable(maxAttempts = MAIL_REQUEST_RETRY_MAX_ATTEMPTS,
+            backoff = @Backoff(delay = MAIL_REQUEST_RETRY_BACK_OFF_PERIOD))
+    private void doSendEmail() {
+        // TODO
+    }
+
+    @Override
+    public PrefixRegistrationSessionActionReport performAction(PrefixRegistrationSession session) throws PrefixRegistrationSessionActionException {
+        return null;
+    }
+}
