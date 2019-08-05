aa2da75767836ea6ea9a0ac8c665469ae9564e3e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-05 11:54:25 +0100

    annotations on PrefixRegistrationSessionActionNotifierEmailCuratorStart

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionNotifierEmailCuratorStart.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionNotifierEmailCuratorStart.java
@@ -1,10 +1,13 @@
 package org.identifiers.cloud.hq.ws.registry.models;
 
+import lombok.extern.slf4j.Slf4j;
 import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
 
 /**
  * Project: registry
@@ -14,6 +17,9 @@ import org.springframework.retry.annotation.Retryable;
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
+@Component
+@Scope("prototype")
+@Slf4j
 public class PrefixRegistrationSessionActionNotifierEmailCuratorStart implements PrefixRegistrationSessionAction {
     private static final int MAIL_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     private static final int MAIL_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
