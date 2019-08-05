a4f74e7a5d41dfc9f35a81643683cb0585fd2d69
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-05 11:19:03 +0100

    email sending agent, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/helpers/EmailSendAgent.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.hq.ws.registry.models.helpers;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models.helpers
+ * Timestamp: 2019-08-05 11:14
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This implements an e-mail sending agent
+ */
+@Component
+public class EmailSendAgent {
+    // TODO
+}
