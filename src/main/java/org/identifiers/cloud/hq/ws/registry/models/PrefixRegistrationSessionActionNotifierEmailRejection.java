ac7f7575f8d76e0263cf7004784f5f0f53fc5cbd
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-05 11:53:15 +0100

    PrefixRegistrationSessionActionNotifierEmailRejection skeleton

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionNotifierEmailRejection.java
@@ -0,0 +1,37 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-05 11:13
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationSessionActionNotifierEmailRejection implements PrefixRegistrationSessionAction {
+    private static final int MAIL_REQUEST_RETRY_MAX_ATTEMPTS = 12;
+    private static final int MAIL_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
+
+    // Configuration
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    // Helper
+    @Retryable(maxAttempts = MAIL_REQUEST_RETRY_MAX_ATTEMPTS,
+            backoff = @Backoff(delay = MAIL_REQUEST_RETRY_BACK_OFF_PERIOD))
+    private void doSendEmail() {
+        // TODO
+    }
+
+    @Override
+    public PrefixRegistrationSessionActionReport performAction(PrefixRegistrationSession session) throws PrefixRegistrationSessionActionException {
+        return null;
+    }
+}
