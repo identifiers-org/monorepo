1dbca1160d35b542aac9c915073489a76526e89c
jferrer <jferrer@visualtis.com>
2019-08-05 11:49:52 +0100

    added rrr page

--- site/src/components/pages/ResourceRegistrationRequestPage.js
+++ site/src/components/pages/ResourceRegistrationRequestPage.js
@@ -28,11 +28,30 @@ class ResourceRegistrationRequestPage extends React.Component  {
   }
 }
 
-// Redux Mappings.
+// Mapping
 const mapStateToProps = (state) => ({
+  namespacePrefix: state.resourceRegistrationRequestForm.namespacePrefix,
+  institutionName: state.resourceRegistrationRequestForm.institutionName,
+  institutionDescription: state.resourceRegistrationRequestForm.institutionDescription,
+  institutionHomeUrl: state.resourceRegistrationRequestForm.institutionHomeUrl,
+  institutionLocation: state.resourceRegistrationRequestForm.institutionLocation,
+  institutionIsProvider: state.resourceRegistrationRequestForm.institutionIsProvider,
+  providerName: state.resourceRegistrationRequestForm.providerName,
+  providerDescription: state.resourceRegistrationRequestForm.providerDescription,
+  providerCode: state.resourceRegistrationRequestForm.providerCode,
+  providerHomeUrl: state.resourceRegistrationRequestForm.providerHomeUrl,
+  providerUrlPattern: state.resourceRegistrationRequestForm.providerUrlPattern,
+  providerLocation: state.resourceRegistrationRequestForm.providerLocation,
+  requesterName: state.resourceRegistrationRequestForm.requesterName,
+  requesterEmail: state.resourceRegistrationRequestForm.requesterEmail,
+  locationList: state.locationList
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  setValue: (id, value) => dispatch(setRegistrationRequestFieldField('PREFIX', id, 'value', value)),
+  validate: (id) => dispatch(setRegistrationRequestFieldField('PREFIX', id, 'requestedValidate', true)),
+  setValidation: (id, validation) => dispatch(setValidation('PREFIX', id, validation)),
+  reset: (id) => dispatch(reset('PREFIX', id))
 });
 
 export default connect (mapStateToProps, mapDispatchToProps)(ResourceRegistrationRequestPage);
