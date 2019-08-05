6fa804a3188c683d4f50bb9ee3d3f9a045dbfe7e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-05 11:53:00 +0100

    PrefixRegistrationSessionActionNotifierEmailCuratorStart skeleton

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionNotifierEmailCuratorStart.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/PrefixRegistrationSessionActionNotifierEmailCuratorStart.java
@@ -1,26 +1,20 @@
 package org.identifiers.cloud.hq.ws.registry.models;
 
-import lombok.extern.slf4j.Slf4j;
 import org.identifiers.cloud.hq.ws.registry.data.models.PrefixRegistrationSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
-import org.springframework.stereotype.Component;
 
 /**
  * Project: registry
  * Package: org.identifiers.cloud.hq.ws.registry.models
- * Timestamp: 2019-08-05 11:13
+ * Timestamp: 2019-08-05 11:46
  *
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * ---
  */
-@Slf4j
-@Component
-@Qualifier("PrefixRegistrationSessionActionNotifierEmailAcceptance")
-public class PrefixRegistrationSessionActionNotifierEmailAcceptance implements PrefixRegistrationSessionAction {
+public class PrefixRegistrationSessionActionNotifierEmailCuratorStart implements PrefixRegistrationSessionAction {
     private static final int MAIL_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     private static final int MAIL_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
 
