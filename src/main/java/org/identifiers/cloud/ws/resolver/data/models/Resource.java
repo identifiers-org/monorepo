4445f891e4507db61eda959043ff922bda7f1643
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-06 09:20:22 +0100

    updated resource model to hold deprecation / deactivation information

--- src/main/java/org/identifiers/cloud/ws/resolver/data/models/Resource.java
+++ src/main/java/org/identifiers/cloud/ws/resolver/data/models/Resource.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Project: resolver
@@ -39,5 +40,7 @@ public class Resource implements Serializable {
     private String resourceHomeUrl;
     private Institution institution;
     private Location location;
-
+    // Deprecation / deactivation
+    private boolean deprecated;
+    private Date deprecationDate;
 }
