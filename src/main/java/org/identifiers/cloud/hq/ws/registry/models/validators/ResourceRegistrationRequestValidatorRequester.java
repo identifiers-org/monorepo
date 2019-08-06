9a88aa300154e32c4256045d6c140f6c36933b18
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-06 05:08:47 +0100

    delegate exception transformation

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceRegistrationRequestValidatorRequester.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/validators/ResourceRegistrationRequestValidatorRequester.java
@@ -23,6 +23,10 @@ public class ResourceRegistrationRequestValidatorRequester implements ResourceRe
 
     @Override
     public boolean validate(ServiceRequestRegisterResourcePayload request) throws ResourceRegistrationRequestValidatorException {
-        return delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request));
+        try {
+            return delegateValidator.validate(ApiAndDataModelsHelper.getFrom(request));
+        } catch (PrefixRegistrationRequestValidatorException e) {
+            throw new ResourceRegistrationRequestValidatorException(e.getMessage());
+        }
     }
 }
