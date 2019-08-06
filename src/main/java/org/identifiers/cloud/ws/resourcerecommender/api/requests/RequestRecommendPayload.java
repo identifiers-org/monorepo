ca728b5822d1e26db1cb70c53a8df88f46f89547
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-06 10:32:03 +0100

    refactoring

--- src/main/java/org/identifiers/cloud/ws/resourcerecommender/api/requests/RequestRecommendPayload.java
+++ src/main/java/org/identifiers/cloud/ws/resourcerecommender/api/requests/RequestRecommendPayload.java
@@ -1,6 +1,11 @@
 package org.identifiers.cloud.ws.resourcerecommender.api.requests;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.identifiers.cloud.ws.resourcerecommender.api.data.models.ResolvedResource;
 
 import java.io.Serializable;
@@ -13,16 +18,12 @@ import java.util.List;
  * Timestamp: 2018-03-06 15:26
  * ---
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestRecommendPayload implements Serializable {
     private List<ResolvedResource> resolvedResources;
-
-    public List<ResolvedResource> getResolvedResources() {
-        return resolvedResources;
-    }
-
-    public RequestRecommendPayload setResolvedResources(List<ResolvedResource> resolvedResources) {
-        this.resolvedResources = resolvedResources;
-        return this;
-    }
 }
