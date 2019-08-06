2037dc5aa565e5328ae26ffc99c34cd85997e439
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-06 04:09:59 +0100

    curator e-mail notifier for new resource registration request, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceRegistrationSessionActionNotifierEmailCuratorStart.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+import org.identifiers.cloud.hq.ws.registry.data.models.ResourceRegistrationSession;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-06 04:08
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ResourceRegistrationSessionActionNotifierEmailCuratorStart implements ResourceRegistrationSessionAction {
+    @Override
+    public ResourceRegistrationSessionActionReport performAction(ResourceRegistrationSession session) throws ResourceRegistrationSessionActionException {
+        return null;
+    }
+}
