57f5378d49113dc65fe16b4394416293197d42b0
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-13 15:44:39 +0100

    Missing repository component, bug fix

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementServiceSimpleStrategy.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/ResourceLifecycleManagementServiceSimpleStrategy.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.identifiers.cloud.hq.ws.registry.data.models.Resource;
 import org.identifiers.cloud.hq.ws.registry.data.repositories.ResourceRepository;
 import org.identifiers.cloud.hq.ws.registry.models.validators.ResourceLifecycleManagementOperationReport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
