a815e9c1ddc45ce3f72d7a6a8f30d3850229bf0e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-14 17:24:47 +0100

    Organization ROR data model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/rororg/Organization.java
@@ -0,0 +1,42 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.rororg;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.rororg
+ * Timestamp: 2019-08-14 17:15
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Organization implements Serializable {
+    private String id;
+    private String name;
+    private List<String> types = new ArrayList<>();
+    private List<String> aliases = new ArrayList<>();
+    private List<String> acronyms = new ArrayList<>();
+    @JsonAlias({"wikipedia_url"})
+    private String wikipediaUrl;
+    // TODO Country
+    // TODO ISNI
+    // TODO OrgRef
+    // TODO Wikidata
+    // TODO GRID
+}
