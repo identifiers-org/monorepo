5f871083687b9fa35dd93824f8841ec4f83779e2
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-14 17:26:36 +0100

    ROR data model for country

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/rororg/Country.java
@@ -0,0 +1,32 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.rororg;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.rororg
+ * Timestamp: 2019-08-14 17:25
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * ror.org "country" data model
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Country {
+    @JsonAlias({"country_name"})
+    private String countryName;
+    @JsonAlias({"country_code"})
+    private String countryCode;
+}
