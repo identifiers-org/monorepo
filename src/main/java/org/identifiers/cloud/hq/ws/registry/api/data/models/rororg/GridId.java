75b7f10cac8dd62176344db6c0c11fdfc15f2913
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-14 17:30:18 +0100

    ROR data model for GRID

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/data/models/rororg/GridId.java
@@ -0,0 +1,29 @@
+package org.identifiers.cloud.hq.ws.registry.api.data.models.rororg;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.data.models.rororg
+ * Timestamp: 2019-08-14 17:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * ROR data model for GRID link from an organization
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GridId {
+    private String preferred;
+    private String all;
+}
