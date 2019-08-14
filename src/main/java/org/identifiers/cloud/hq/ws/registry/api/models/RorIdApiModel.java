9e1ce1aac737d9c78b9171d69aadb5add7185fee
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-14 17:14:57 +0100

    RorIdApiModel, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/RorIdApiModel.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2019-08-14 17:14
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class RorIdApiModel {
+    // TODO
+}
