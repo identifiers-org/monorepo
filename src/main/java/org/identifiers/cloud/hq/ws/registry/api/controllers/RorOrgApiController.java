eb11b45b4a5fbcb0a4415847d5417fb78710c62b
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-14 17:12:55 +0100

    ROR ID API controller for interoperability, placeholdrer

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/RorOrgApiController.java
@@ -0,0 +1,20 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2019-08-14 17:10
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This Controller handles requests for working with ROR IDs (https://ror.org), in an interoperable way
+ */
+@RestController
+@RequestMapping("rorIdApi")
+public class RorOrgApiController {
+    // TODO
+}
