8ab0f4cc1a5e76c9244adf899ac192ae74d78d3a
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-15 04:22:38 +0100

    compute ISNI information

--- src/main/java/org/identifiers/cloud/hq/ws/registry/models/rororg/Organization.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/rororg/Organization.java
@@ -1,13 +1,18 @@
 package org.identifiers.cloud.hq.ws.registry.models.rororg;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +31,8 @@ import java.util.Map;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Accessors(chain = true)
-@JsonIgnoreProperties(ignoreUnknown = true, value = {"KEY_EXTERNAL_ID_ISNI", "KEY_EXTERNAL_ID_ORGREF", "KEY_EXTERNAL_ID_WIKIDATA", "KEY_EXTERNAL_ID_GRID"})
+@JsonIgnoreProperties(ignoreUnknown = true, value = {"KEY_EXTERNAL_ID_ISNI", "KEY_EXTERNAL_ID_ORGREF", "KEY_EXTERNAL_ID_WIKIDATA", "KEY_EXTERNAL_ID_GRID", "isni", "orgRef", "wikidata", "grid"})
+@Slf4j
 public class Organization implements Serializable {
     // Unmapped property keys
     private static final String KEY_EXTERNAL_ID_ISNI = "ISNI";
@@ -46,5 +52,30 @@ public class Organization implements Serializable {
     private Country country;
     // TODO Wire in external IDs, although I may not use them
     @JsonAlias({"external_ids"})
-    private Map<String, String> externalIds;
+    private Map<String, Object> externalIds;
+    // Made up
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private OrganizationIsniInformation isni;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private OrganizationOrgRefInformation orgRef;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private OrganizationWikidataInformation wikidata;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private OrganizationGridIdInfo grid;
+
+    // Tricky getters
+    @JsonIgnore
+    public OrganizationIsniInformation getIsni() {
+        if (isni == null) {
+            if (externalIds.get(KEY_EXTERNAL_ID_ISNI) != null) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    isni = objectMapper.readValue(objectMapper.writeValueAsString(externalIds.get(KEY_EXTERNAL_ID_ISNI)), OrganizationIsniInformation.class);
+                } catch (IOException e) {
+                    log.error(String.format("Could NOT build ISNI from existing information in Organization with ROR ID '%s'", id));
+                }
+            }
+        }
+        return isni;
+    }
 }
