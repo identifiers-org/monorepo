bde95493c301aedcfc7818c970f95d0f2abe4019
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-15 02:29:47 +0100

    ServiceRequestRorApiGetInstitution

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/requests/ServiceRequestRorApiGetInstitution.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.hq.ws.registry.api.requests;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.requests
+ * Timestamp: 2019-08-15 02:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestRorApiGetInstitution extends ServiceRequest<ServiceRequestRorApiGetInstitutionPayload> {
+}
