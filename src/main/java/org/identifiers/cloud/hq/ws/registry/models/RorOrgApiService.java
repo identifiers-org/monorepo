a0ad9c50fa79f4f3cbab04ba25a10da81a1ce1cc
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-15 01:18:09 +0100

    RorOrgApiService contract placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/RorOrgApiService.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-15 01:17
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This interface represents the contract met by any ROR API broker / service
+ */
+public interface RorOrgApiService {
+    // TODO
+}
