4cb017f821249ee46c69f3cf72f2bffc887e1537
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-17 06:41:05 +0100

    schema.org api controller, placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/SchemaOrgApiController.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.hq.ws.registry.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.controllers
+ * Timestamp: 2019-08-17 06:40
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@RestController
+@RequestMapping("schemaOrgApi")
+public class SchemaOrgApiController {
+    // TODO
+}
