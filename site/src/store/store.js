4fc363d26d9c4f3ded895abf764f60e4cd3b6d1a
Javier Ferrer <javilol@gmail.com>
2019-08-19 17:00:27 +0100

    added dependencies to store

--- site/src/store/store.js
+++ site/src/store/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+// Reducers.
 import configReducer from '../reducers/ConfigReducer';
 import namespaceListReducer from '../reducers/NamespaceList';
 import resolvedResourcesReducer from '../reducers/ResolvedResources';
+import schemaOrgMetadataReducer from '../reducers/SchemaOrgMetadataReducer';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
