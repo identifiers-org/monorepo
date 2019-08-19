60845a92daee0eac807b6bcb323bebb0e44f8cef
Javier Ferrer <javilol@gmail.com>
2019-08-19 17:01:04 +0100

    converted index to async registry style

--- site/src/index.js
+++ site/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+// Actions.
+import { getConfigFromDevopsApi } from './actions/Config';
+import { getSchemaOrgMetadataFromRegistry } from './actions/SchemaOrgMetadata';
+
+// Components.
 import AppRouter from './routers/AppRouter';
 import store from './store/store';
 
-import { getConfigFromDevopsApi } from './actions/Config';
+// Utils.
+import { appendSchemaOrg } from './utils/schemaOrg';
 
 import './styles/styles.scss';
 
@@ -18,12 +24,35 @@ const jsx = (
 );
 
 
-// Get initial data.
-// Configuration from devops endpoint, which will be residing in the same url as the app.
-const configUrlPort = process.env.NODE_ENV === 'development' ? 9090 : window.location.port;
-const configUrl = `${window.location.protocol}//${window.location.hostname}:${configUrlPort}`;
+// ==================== APP Initialization ====================
+(async () => {
+  // Get initial data.
+  // Configuration from devops endpoint, which will be residing in the same url as the app.
+  const configUrlPort = process.env.NODE_ENV === 'development' ? 9090 : window.location.port;
+  const configUrl = `${window.location.protocol}//${window.location.hostname}:${configUrlPort}`;
 
-store.dispatch(getConfigFromDevopsApi(configUrl));
+  store.dispatch(getConfigFromDevopsApi(configUrl));
 
-// Render app.
-ReactDOM.render(jsx, document.getElementById("app"));
+  // Get Schema.org Metadata and append it to the document's head.
+  await store.dispatch(getSchemaOrgMetadataFromRegistry());
+  appendSchemaOrg(store.getState().schemaOrgMetadata);
+
+
+
+  // Render app.
+  ReactDOM.render(jsx, document.getElementById("app"));
+
+  // Run ebi framework stuff.
+  ebiFrameworkPopulateBlackBar();
+  ebiFrameworkActivateBlackBar();
+  ebiFrameworkExternalLinks();
+  ebiFrameworkManageGlobalSearch();
+  ebiFrameworkSearchNullError();
+  ebiFrameworkHideGlobalNav();
+  ebiFrameworkAssignImageByMetaTags();
+  ebiFrameworkInsertEMBLdropdown();
+  ebiFrameworkUpdateFoot();
+  ebiFrameworkUpdateFooterMeta();
+  ebiFrameworkIncludeAnnouncements();
+  ebiFrameworkRunDataProtectionBanner('1.3');
+})()
