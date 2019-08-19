37d5972b3bbcfd2289d80e3a50f8ae328b51cdc7
Javier Ferrer <javilol@gmail.com>
2019-08-19 15:23:18 +0100

    fixed actions

---
+++ site/src/actions/SchemaOrgMetadata.js
@@ -0,0 +1,27 @@
+// Config.
+import { config } from '../config/Config';
+
+
+//
+// SchemaOrgMetadata actions.
+//
+
+// Get Schema.org Metadata list from registry. Will dispatch setSchemaOrgMetadata.
+export const getSchemaOrgMetadataFromRegistry = (id) => {
+  return async (dispatch) => {
+    let requestUrl = !id ? `${config.registryApi}/${config.schemaOrgPlatformEndpoint}` : `${config.registryApi}/${config.schemaOrgNamespaceEndpoint}/${id}`;
+
+    const response = await fetch(requestUrl);
+    const json = await response.json();
+
+    dispatch(setLocationList(json));
+  };
+};
+
+
+export const setLocationList = (schemaOrgMetadata) => {
+  return {
+    type: 'SET_SCHEMAORGMETADATA',
+    schemaOrgMetadata
+  }
+}
