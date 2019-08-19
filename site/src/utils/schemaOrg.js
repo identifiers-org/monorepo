7aa197aeb7e439703cabbf9408affdb9e630d56c
Javier Ferrer <javilol@gmail.com>
2019-08-19 16:59:40 +0100

    metadata injection utility now checks for a previous jsonld

--- site/src/utils/schemaOrg.js
+++ site/src/utils/schemaOrg.js
@@ -1,6 +1,8 @@
 export function appendSchemaOrg(contents) {
+
   const script = document.createElement('script');
 
+  script.id = 'schemaorg-metadata';
   script.type = 'application/ld+json';
   script.innerHTML = JSON.stringify(contents, null, 2);
 
