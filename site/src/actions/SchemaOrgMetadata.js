69d1e637bf6b5fa8fef2a878765ea1088f3b5fcf
Javier Ferrer <javilol@gmail.com>
2019-08-19 17:48:13 +0100

    added schema.org metadata by prefix action

--- site/src/actions/SchemaOrgMetadata.js
+++ site/src/actions/SchemaOrgMetadata.js
@@ -21,6 +21,18 @@ export const getSchemaOrgMetadataFromRegistry = (namespaceId) => {
   };
 };
 
+// Get Schema.org Metadata list from registry using a prefix. Will dispatch setSchemaOrgMetadata.
+export const getSchemaOrgMetadataByPrefixFromRegistry = (prefix) => {
+  return async (dispatch) => {
+    let requestUrl = `${config.registryApi}/${config.schemaOrgNamespacePrefixEndpoint}/${prefix}`;
+
+    const response = await fetch(requestUrl);
+    const json = await response.json();
+
+    dispatch(setSchemaOrgMetadata(json));
+  };
+};
+
 
 export const setSchemaOrgMetadata = (schemaOrgMetadata) => {
   // Inject metadata in DOM.
