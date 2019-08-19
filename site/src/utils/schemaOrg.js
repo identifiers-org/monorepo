df24c68213a2cee8c1e1b45a2bd317e140013c1b
Javier Ferrer <javilol@gmail.com>
2019-08-19 16:34:15 +0100

    first look for an already existing metadata script

--- site/src/utils/schemaOrg.js
+++ site/src/utils/schemaOrg.js
@@ -1,6 +1,7 @@
 export function appendSchemaOrg(contents) {
-  const script = document.createElement('script');
+  const script = document.getElementById('schemaorg-metadata') || document.createElement('script');
 
+  script.id = 'schemaorg-metadata';
   script.type = 'application/ld+json';
   script.innerHTML = JSON.stringify(contents, null, 2);
 
