6f2c335817beb6f79f7a34fd3e611481e9cb69ad
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-19 10:25:08 +0100

    SchemaOrgMetadataProvider placeholder

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/SchemaOrgMetadataProvider.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-19 10:23
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This contract is met by any provider of Schema.org metadata from the registry data
+ */
+public interface SchemaOrgMetadataProvider {
+    // TODO
+}
