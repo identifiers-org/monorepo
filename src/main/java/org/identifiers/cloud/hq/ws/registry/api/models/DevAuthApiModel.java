0e5b150f8b126b39811dff6912dc5339f3a4e6b9
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-21 01:18:22 +0100

    Development API for playing with OAuth2 JWT, API model

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/DevAuthApiModel.java
@@ -0,0 +1,36 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.api.models
+ * Timestamp: 2019-08-20 14:50
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+@Slf4j
+public class DevAuthApiModel {
+    // TODO
+    public ResponseEntity<?> getPrincipal() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return new ResponseEntity<>(authentication.getPrincipal(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof Jwt) {
+            Jwt jwtToken = (Jwt) authentication.getPrincipal();
+            return new ResponseEntity<>(jwtToken.getClaims().getOrDefault("preferred_username", "UNKNOWN_USER"), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("NOT JWT", HttpStatus.OK);
+    }
+}
