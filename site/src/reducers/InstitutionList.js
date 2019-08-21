f7c412360d06f5d3c0075b9b10f742e747b1ee30
jferrer <jferrer@visualtis.com>
2019-08-21 16:24:45 +0100

    added SET_INSTITUTION (single) and sort institutions in reducer

--- site/src/reducers/InstitutionList.js
+++ site/src/reducers/InstitutionList.js
@@ -7,7 +7,10 @@ const institutionListReducer = (state = defaultState, action) => {
 
   // Set store institution list to the supplied one.
   case 'SET_INSTITUTIONLIST':
-    return action.institutionList
+    return action.institutionList.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+
+  case 'SET_INSTITUTION':
+    return [...state.filter(institution => institution.id !== action.institution.id), action.institution].sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
 
   default:
     return state;
