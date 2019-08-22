7bb05eda958f6700ad6ef9d8a9835e22eea92812
jferrer <jferrer@visualtis.com>
2019-08-22 17:22:54 +0100

    fixed get institution by rorid

--- site/src/actions/CurationDashboardPage/CurationInstitutionList.js
+++ site/src/actions/CurationDashboardPage/CurationInstitutionList.js
@@ -51,24 +51,23 @@ export const getCurationInstitutionListFromRegistry = (params) => {
 
 
 // Get institution using a ROR ID.
-export const getInstitutionForRORIDFromRegistry = (rorId) => {
-  return async (dispatch) => {
-    let requestURL = new URL(`${config.registryApi}/${config.rorIdEndpoint}/getInstitutionForRorId`);
-    const init = {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        apiVersion: '1.0',
-        payload: {rorId}
-      })
-    };
-
-    const response = await fetch(requestURL, init);
-
-    return response.status !== 200 ? null : await response.json();
+export const getInstitutionForRORIDFromRegistry = (rorId) => async () => {
+  let requestURL = new URL(`${config.registryApi}/${config.rorIdEndpoint}/getInstitutionForRorId`);
+  const init = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      apiVersion: '1.0',
+      payload: {rorId}
+    })
   };
+
+  const response = await fetch(requestURL, init);
+  const data = await response.json();
+
+  return response.status === 200 ? data : null;
 };
 
 
