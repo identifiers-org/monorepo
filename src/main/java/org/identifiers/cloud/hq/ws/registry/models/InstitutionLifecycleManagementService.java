00a25b63de980e01c25c9ea6bbcb7bb7a04a47f2
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-22 11:20:58 +0100

    InstitutionLifecycleManagementService

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/InstitutionLifecycleManagementService.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-22 11:20
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This is the contract for any strategy implementing lifecycle management for institutions
+ */
+public interface InstitutionLifecycleManagementService {
+    // TODO
+}
