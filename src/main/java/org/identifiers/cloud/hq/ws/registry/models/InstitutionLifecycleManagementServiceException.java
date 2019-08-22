96f93eba2dfbb62ca8d7bb1ade0ae9e561d0938e
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-08-22 11:21:38 +0100

    InstitutionLifecycleManagementServiceException

---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/models/InstitutionLifecycleManagementServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.hq.ws.registry.models;
+
+/**
+ * Project: registry
+ * Package: org.identifiers.cloud.hq.ws.registry.models
+ * Timestamp: 2019-08-22 11:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class InstitutionLifecycleManagementServiceException extends RuntimeException {
+    public InstitutionLifecycleManagementServiceException(String message) {
+        super(message);
+    }
+}
