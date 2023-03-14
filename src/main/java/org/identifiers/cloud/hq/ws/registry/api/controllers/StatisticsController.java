667a3978359fc33a7dbd7ca21d45003aaf84e4b8
Renato Juaçaba Neto <renatocjn@gmail.com>
2023-03-14 14:51:32 +0000

    retry for statistics endpoit

--- src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/StatisticsController.java
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/controllers/StatisticsController.java
@@ -2,8 +2,8 @@ package org.identifiers.cloud.hq.ws.registry.api.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.identifiers.cloud.hq.ws.registry.api.data.models.NamespaceStatistics;
+import org.identifiers.cloud.hq.ws.registry.api.models.StatisticsModel;
 import org.identifiers.cloud.hq.ws.registry.api.responses.ServiceResponse;
-import org.identifiers.cloud.hq.ws.registry.data.repositories.NamespaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 /***
  * Relevant matomo documentation:
@@ -30,54 +28,30 @@ public class StatisticsController {
     @Value("${org.identifiers.matomo.enabled}")
     boolean matomoIsEnabled;
 
-    @Value("${org.identifiers.matomo.baseUrl}")
-    String matomoBaseUrl;
-
-    @Value("${org.identifiers.matomo.authToken}")
-    String matomoTokenAuth;
-
     @Autowired
-    RestTemplate restTemplate;
-
-    @Autowired
-    NamespaceRepository namespaceRepository;
-
-    private String getMatomoRequestUrl(String prefix) {
-        String matomoRequestUrl = UriComponentsBuilder.fromHttpUrl(matomoBaseUrl.trim())
-                .queryParam("module", "API")
-                .queryParam("method", "VisitsSummary.get")
-                .queryParam("idSite", "1")
-                .queryParam("period", "month")
-                .queryParam("date", "today")
-                .queryParam("format", "json")
-                .queryParam("token_auth", matomoTokenAuth)
-                .queryParam("segment", String.format("dimension6==%s", prefix))
-                .build().toString();
-        log.debug("Matomo query URL: {}", matomoRequestUrl);
-        return matomoRequestUrl;
-    }
+    StatisticsModel model;
 
     @GetMapping("/namespace/{prefix}")
-    public ResponseEntity<ServiceResponse<NamespaceStatistics>> getNamespaceStatistic2(@PathVariable String prefix) {
+    public ResponseEntity<ServiceResponse<NamespaceStatistics>> getNamespaceStatistic(@PathVariable String prefix) {
         log.debug("Request for statistics of namespace {}", prefix);
         if (!matomoIsEnabled) {
             log.debug("Matomo disabled, answering with no content");
             return ResponseEntity.noContent().build();
         }
-        if (namespaceRepository.findByPrefix(prefix) == null) {
+        if (!model.doesNamespaceExists(prefix)) {
             return ResponseEntity.notFound().build();
         }
 
         try {
-            String matomoRequestUrl = getMatomoRequestUrl(prefix);
-            NamespaceStatistics stats = restTemplate.getForObject(matomoRequestUrl, NamespaceStatistics.class);
+            NamespaceStatistics stats = model.getMatomoStatisticsFor(prefix);
+            log.debug("Found statistics for {}: {}", prefix, stats);
             ServiceResponse<NamespaceStatistics> response = ServiceResponse.getBaseResponse(stats);
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (RestClientException e) {
             ServiceResponse response = ServiceResponse.getBaseResponse();
             log.error("Error on matomo get", e);
             response.setErrorMessage(e.getMessage());
-            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
---
+++ src/main/java/org/identifiers/cloud/hq/ws/registry/api/models/StatisticsModel.java
@@ -0,0 +1,54 @@
+package org.identifiers.cloud.hq.ws.registry.api.models;
+
+import lombok.extern.slf4j.Slf4j;
+import org.identifiers.cloud.hq.ws.registry.api.data.models.NamespaceStatistics;
+import org.identifiers.cloud.hq.ws.registry.data.repositories.NamespaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@Slf4j
+public class StatisticsModel {
+    @Value("${org.identifiers.matomo.baseUrl}")
+    String matomoBaseUrl;
+
+    @Value("${org.identifiers.matomo.authToken}")
+    String matomoTokenAuth;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    NamespaceRepository namespaceRepository;
+
+    public boolean doesNamespaceExists(String prefix) {
+        return namespaceRepository.findByPrefix(prefix) != null;
+    }
+
+    private String getMatomoRequestUrl(String prefix) {
+        String matomoRequestUrl = UriComponentsBuilder.fromHttpUrl(matomoBaseUrl.trim())
+                .queryParam("module", "API")
+                .queryParam("method", "VisitsSummary.get")
+                .queryParam("idSite", "1")
+                .queryParam("period", "month")
+                .queryParam("date", "today")
+                .queryParam("format", "json")
+                .queryParam("token_auth", matomoTokenAuth)
+                .queryParam("segment", String.format("dimension6==%s", prefix))
+                .build().toString();
+        log.debug("Matomo query URL: {}", matomoRequestUrl);
+        return matomoRequestUrl;
+    }
+
+    @Retryable(value = RestClientException.class, maxAttempts = 2, backoff = @Backoff(delay = 100))
+    public NamespaceStatistics getMatomoStatisticsFor(String prefix) {
+        String matomoRequestUrl = getMatomoRequestUrl(prefix);
+        return restTemplate.getForObject(matomoRequestUrl, NamespaceStatistics.class);
+    }
+}
