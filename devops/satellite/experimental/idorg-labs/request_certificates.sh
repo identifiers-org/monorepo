9db7ce4895c92161a3ba40769b2c9e07c88f536a
Renato Caminha Juacaba Neto <rjuacaba@ebi.ac.uk>
2025-01-27 15:55:06 +0000

    Cleaning up devops

--- devops/satellite/experimental/idorg-labs/request_certificates.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcloud beta compute ssl-certificates create idorg-labs-satellite-api-resolver --domains resolver.api.labs.identifiers.org
-gcloud beta compute ssl-certificates create idorg-labs-satellite-api-metadata --domains metadata.api.labs.identifiers.org
-gcloud beta compute ssl-certificates create idorg-labs-satellite-web --domains labs.identifiers.org
-gcloud beta compute ssl-certificates create idorg-labs-satellite-sparql --domains sparql.api.labs.identifiers.org
