7017e39604fe0e6bb825f651dad3f2de475e3611
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2025-02-06 09:48:33 +0000

    Give up parsing in shared library

--- webservices/commons/src/main/java/org/identifiers/cloud/commons/compactidparsing/helpers/CompactIdParsingHelper.java
+++
@@ -1,7 +0,0 @@
-package org.identifiers.cloud.commons.compactidparsing.helpers;
-
-import org.identifiers.cloud.commons.compactidparsing.ParsedCompactIdentifier;
-
-public interface CompactIdParsingHelper {
-    ParsedCompactIdentifier parseCompactIdRequest(String rawCompactIdentifier);
-}
