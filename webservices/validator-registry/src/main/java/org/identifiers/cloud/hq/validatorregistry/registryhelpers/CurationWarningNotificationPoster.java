38c6c464e7bfa5f4ae377ed2c7cca20c43c8a09f
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2025-03-19 15:23:28 +0000

    small adjustments and validator version bump

--- webservices/validator-registry/src/main/java/org/identifiers/cloud/hq/validatorregistry/registryhelpers/CurationWarningNotificationPoster.java
+++ webservices/validator-registry/src/main/java/org/identifiers/cloud/hq/validatorregistry/registryhelpers/CurationWarningNotificationPoster.java
@@ -7,6 +7,7 @@ import org.identifiers.cloud.commons.messages.requests.ServiceRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.net.URI;
 import java.util.List;
@@ -21,13 +22,21 @@ public class CurationWarningNotificationPoster {
     URI notificationEndpoint;
 
     public void post(List<CurationWarningNotification> notifications) {
+        if (CollectionUtils.isEmpty(notifications)) {
+            log.info("Zero notifications to post");
+            return;
+        }
         log.info("Posting {} notifications to {}", notifications.size(), notificationEndpoint);
 
         var serviceRequest = ServiceRequest.of(notifications);
         try {
-            restTemplateInternalAuthEnabled.postForEntity(notificationEndpoint, serviceRequest, Void.class);
+            var response = restTemplateInternalAuthEnabled.postForEntity(notificationEndpoint, serviceRequest, Void.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                log.error("Unsuccessful response code from registry: {}", response.getStatusCode());
+            }
         } catch (Exception e) {
-            log.error("Failed to send notifications to {}", notificationEndpoint, e);
+            log.error("Failed to send notifications to {}: {}", notificationEndpoint, e.getMessage());
+            log.debug("Stacktrace", e);
         }
 
         log.debug("Finished posting notifications");
