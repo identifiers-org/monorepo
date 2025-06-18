dfa0e949671379461fa8243b9af4d29225f1930b
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2025-06-18 10:24:09 +0100

    Add survey to home page

--- webservices/satellite-webspa/site/src/components/pages/HomePage.js
+++ webservices/satellite-webspa/site/src/components/pages/HomePage.js
@@ -43,6 +43,18 @@ class HomePage extends React.Component {
                 Registry
               </a>.
             </p>
+            <div className="alert alert-info mt-3">
+              <h4 className="text-info-emphasis">
+                <i className="icon icon-common icon-bullhorn me-2"></i>
+                We need your help!
+              </h4>
+              <p className="mb-0 text-justify text-muted">
+                Has identifiers.org saved you time or effort?
+                Please take 15 minutes to fill in a survey and help EMBL-EBI make the case for why open data
+                resources are critical to life science research.
+                <a href="https://www.surveymonkey.com/r/QGFMBH8"> Click here to fill in the survey </a>
+              </p>
+            </div>
         </div>
 
         <div className="col col-12 col-xl-6 bg-light py-5 d-flex align-items-center justify-content-center">
